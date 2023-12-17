adf5e6a8ad2b5536736f5cfe6691dfa6b1ac0924
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-12-17 10:47:41 +0100

    prepare production

--- plenoirf/production/__init__.py
+++ plenoirf/production/__init__.py
@@ -1,1 +1,17 @@
 from . import example
+
+import os
+import magnetic_deflection
+
+def init(production_dir):
+    pass
+
+
+def make_jobs(production_dir):
+    lock = magnetic_deflection.allsky.production.Production(
+        os.path.join(path, "lock")
+    )
+
+
+def run_job(job):
+    pass
