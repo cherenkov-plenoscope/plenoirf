d32418ed7ca19d0bba65c7a1b5d8b175fdecf42b
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2023-12-19 11:16:18 +0100

    adopt tests from acr package

---
+++ plenoirf/producing/tests/test_random_draw_event.py
@@ -0,0 +1,34 @@
+import magnetic_deflection
+import atmospheric_cherenkov_response
+import plenoirf
+import numpy as np
+
+
+def test_dummy():
+    NUM_EVENTS = 128
+    RUN_ID = 1337
+
+    pointing_range = (
+        atmospheric_cherenkov_response.pointing_range.PointingRange_from_cone()
+    )
+
+    allsky_deflection = magnetic_deflection.allsky.testing.AllSkyDummy()
+    assert allsky_deflection.config["particle"]["key"] == "electron"
+    assert allsky_deflection.config["site"]["key"] == "chile"
+
+    prng = np.random.Generator(np.random.PCG64(RUN_ID))
+
+    events = plenoirf.producing.random.draw_primaries_and_pointings(
+        prng=prng,
+        run_id=RUN_ID,
+        num_events=NUM_EVENTS,
+        field_of_view_half_angle_rad=np.deg2rad(6.5),
+        pointing_range=pointing_range,
+        site_particle_magnetic_deflection=allsky_deflection,
+    )
+
+    cps = events["corsika_primary_steering"]
+    assert len(cps["primaries"]) == NUM_EVENTS
+
+    pdirs = events["primary_directions"]
+    assert len(pdirs) == NUM_EVENTS
