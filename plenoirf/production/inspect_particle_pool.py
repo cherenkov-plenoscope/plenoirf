a531c399830c2e16415932e61e6de677828c33a1
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-01-03 13:58:07 +0100

    refactor

--- plenoirf/production/inspect_particle_pool.py
+++ plenoirf/production/inspect_particle_pool.py
@@ -6,8 +6,9 @@ from .. import bookkeeping
 
 
 def run_job(job, logger):
-    bin_radius_m = job["config"]["ground_grid"]["geometry"]["bin_width_m"] / 2
+    aperture_radius_m = guess_aperture_radius_m(job=job)
 
+    logger.info("inspect_particle_pool, init corsika particle zoo")
     corsika_particle_zoo = cpw.particles.identification.Zoo(
         media_refractive_indices={
             "water": 1.33,
@@ -17,51 +18,53 @@ def run_job(job, logger):
         }
     )
 
+    logger.info("inspect_particle_pool, read corsika's particle output")
+
     with cpw.particles.ParticleEventTapeReader(
         path=job["paths"]["tmp"]["particle_pools_tar"]
-    ) as parrun:
-        for event_idx, event in enumerate(parrun):
-            corsika_evth, parreader = event
+    ) as particle_run:
+        for event in particle_run:
+            corsika_evth, particle_reader = event
 
-            uid = nail_down_uid(
-                corsika_evth=corsika_evth, job=job, event_idx=event_idx
-            )
+            uid = make_uid_from_evth(corsika_evth=corsika_evth)
+
+            ppp = init_particlepool_record(uid=uid)
 
             core = record_by_uid(
                 dynamicsizerecarray=job["event_table"]["core"],
                 uid=uid,
             )
 
-            ppp = {spt.IDX: uid}
-            ppp["num_water_cherenkov"] = 0
-            ppp["num_air_cherenkov"] = 0
-            ppp["num_unknown"] = 0
-
             if core:
-                aaa = {spt.IDX: uid}
-                aaa["num_air_cherenkov_on_aperture"] = 0
+                aaa = init_particlepoolonaperture_record(uid=uid)
 
-            for parblock in parreader:
-                cer = mask_cherenkov_emission(
-                    corsika_particles=parblock,
+            for particle_block in particle_reader:
+                cherenkov_emission_mask = mask_cherenkov_emission(
+                    corsika_particles=particle_block,
                     corsika_particle_zoo=corsika_particle_zoo,
                 )
-                ppp["num_water_cherenkov"] += np.sum(cer["media"]["water"])
-                ppp["num_air_cherenkov"] += np.sum(cer["media"]["air"])
-                ppp["num_unknown"] += np.sum(cer["unknown"])
+
+                ppp = update_particlepool_record(
+                    rec=ppp,
+                    cherenkov_emission_mask=cherenkov_emission_mask,
+                )
 
                 if core:
-                    subparblock = parblock[cer["media"]["air"]]
-                    subparblock_r_m = (
-                        distances_to_point_on_observation_level_m(
-                            corsika_particles=subparblock,
-                            x_m=core["core_x_m"],
-                            y_m=core["core_y_m"],
-                        )
-                    )
-                    mask_on_aperture = subparblock_r_m <= bin_radius_m
-                    aaa["num_air_cherenkov_on_aperture"] += np.sum(
-                        mask_on_aperture
+                    """
+                    If the core was drawn by the ground grid algorithm,
+                    we want to know if particles emit Cherenkov light right
+                    in front of the instrument's aperture.
+                    """
+                    particles_emitting_in_air = particle_block[
+                        cherenkov_emission_mask["media"]["air"]
+                    ]
+
+                    aaa = update_particlepoolonaperture_record(
+                        rec=aaa,
+                        particles_emitting_in_air=particles_emitting_in_air,
+                        aperture_x_m=core["core_x_m"],
+                        aperture_y_m=core["core_y_m"],
+                        aperture_radius_m=aperture_radius_m,
                     )
 
             job["event_table"]["particlepool"].append_record(ppp)
@@ -70,11 +73,55 @@ def run_job(job, logger):
     return job
 
 
-def nail_down_uid(corsika_evth, job, event_idx):
+def guess_aperture_radius_m(job):
+    grid_bin_width = job["config"]["ground_grid"]["geometry"]["bin_width_m"]
+    grid_bin_diagonal = np.sqrt(3) * grid_bin_width
+    aperture_radius_m = (1 / 2) * grid_bin_diagonal
+    return aperture_radius_m
+
+
+def init_particlepool_record(uid):
+    ppp = {spt.IDX: uid}
+    ppp["num_water_cherenkov"] = 0
+    ppp["num_air_cherenkov"] = 0
+    ppp["num_unknown"] = 0
+    return ppp
+
+
+def init_particlepoolonaperture_record(uid):
+    aaa = {spt.IDX: uid}
+    aaa["num_air_cherenkov_on_aperture"] = 0
+    return aaa
+
+
+def update_particlepool_record(rec, cherenkov_emission_mask):
+    cer = cherenkov_emission_mask
+    rec["num_water_cherenkov"] += np.sum(cer["media"]["water"])
+    rec["num_air_cherenkov"] += np.sum(cer["media"]["air"])
+    rec["num_unknown"] += np.sum(cer["unknown"])
+    return rec
+
+
+def update_particlepoolonaperture_record(
+    rec,
+    particles_emitting_in_air,
+    aperture_x_m,
+    aperture_y_m,
+    aperture_radius_m,
+):
+    distance_m = distances_to_point_on_observation_level_m(
+        corsika_particles=particles_emitting_in_air,
+        x_m=aperture_x_m,
+        y_m=aperture_y_m,
+    )
+    mask_on_aperture = distance_m <= aperture_radius_m
+    rec["num_air_cherenkov_on_aperture"] += np.sum(mask_on_aperture)
+    return rec
+
+
+def make_uid_from_evth(corsika_evth):
     run_id = int(corsika_evth[cpw.I.EVTH.RUN_NUMBER])
-    assert run_id == job["run_id"]
-    event_id = event_idx + 1
-    assert event_id == corsika_evth[cpw.I.EVTH.EVENT_NUMBER]
+    event_id = int(corsika_evth[cpw.I.EVTH.EVENT_NUMBER])
     uid = bookkeeping.uid.make_uid(run_id=run_id, event_id=event_id)
     return uid
 
@@ -86,9 +133,33 @@ def record_by_uid(dynamicsizerecarray, uid):
             for name in dynamicsizerecarray._recarray.dtype.names:
                 out[name] = dynamicsizerecarray._recarray[name][i]
             return out
+    return None
 
 
 def mask_cherenkov_emission(corsika_particles, corsika_particle_zoo):
+    """
+    Returns binary masks indicating if a particle will emit Cherenkov light
+    in a certain medium.
+
+    Parameters
+    ----------
+    corsika_particles : numpy.ndarray, shape=(N, 7), dtype=np.float32
+        Block of 'N' particles written by corsika.
+        See corsika_primary.particles.
+    corsika_particle_zoo : corsika_primary.particles.identification.Zoo
+        The zoo does not only identify particles, it is also primed with
+        media e.g. water and air for which it stores the threshold
+        velocities the particles must pass to emit Cherenkov light.
+
+    Returns
+    -------
+    masks : dict of binary masks
+        -unknown : 1 if the zoo does not know the particle type.
+        -media
+            - e.g. water: 1 if the particle will emit Cherenkov light in water.
+            - e.g. air ...
+            - ...
+    """
     num_particles = corsika_particles.shape[0]
     media = corsika_particle_zoo.media_cherenkov_threshold_lorentz_factor
     out = {}
