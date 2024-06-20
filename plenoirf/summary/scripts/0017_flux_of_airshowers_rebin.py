d06d90a46b1bacc6f0c00756819d0b737c84bb07
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-06-20 12:59:53 +0200

    update 0017

--- plenoirf/summary/scripts/0017_flux_of_airshowers_rebin.py
+++ plenoirf/summary/scripts/0017_flux_of_airshowers_rebin.py
@@ -11,27 +11,22 @@ Rebin the diff. flux of cosmic-rays dFdE into the energy-binning used
 for the diff. sensitivity.
 """
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
+os.makedirs(paths["out_dir"], exist_ok=True)
 
-irf_config = irf.summary.read_instrument_response_config(run_dir=pa["run_dir"])
-sum_config = irf.summary.read_summary_config(summary_dir=pa["summary_dir"])
-seb.matplotlib.rcParams.update(sum_config["plot"]["matplotlib"])
+seb.matplotlib.rcParams.update(res.analysis["plot"]["matplotlib"])
 
-os.makedirs(pa["out_dir"], exist_ok=True)
-
-SITES = irf_config["config"]["sites"]
-PARTICLES = irf_config["config"]["particles"]
-COSMIC_RAYS = irf.utils.filter_particles_with_electric_charge(PARTICLES)
+COSMIC_RAYS = res.COSMIC_RAYS
 
 # load
 # ----
 airshower_fluxes = json_utils.tree.read(
-    os.path.join(pa["summary_dir"], "0015_flux_of_airshowers")
+    os.path.join(paths["analysis_dir"], "0015_flux_of_airshowers")
 )
 
 energy_binning = json_utils.read(
-    os.path.join(pa["summary_dir"], "0005_common_binning", "energy.json")
+    os.path.join(paths["analysis_dir"], "0005_common_binning", "energy.json")
 )
 
 # prepare
@@ -47,64 +42,68 @@ for E in energy_bin["edges"]:
 # ----
 diff_flux = {}
 diff_flux_au = {}
-for sk in SITES:
-    diff_flux[sk] = {}
-    diff_flux_au[sk] = {}
-    sk_dir = os.path.join(pa["out_dir"], sk)
-    os.makedirs(sk_dir, exist_ok=True)
-    for pk in COSMIC_RAYS:
-        fine_dFdE = airshower_fluxes[sk][pk]["differential_flux"]["values"]
-        fine_dFdE_au = airshower_fluxes[sk][pk]["differential_flux"][
-            "absolute_uncertainty"
-        ]
-        dFdE = np.zeros(energy_bin["num_bins"])
-        dFdE_au = np.zeros(energy_bin["num_bins"])
 
-        for ebin in range(energy_bin["num_bins"]):
-            fe_start = fine_energy_bin_matches[ebin]
-            fe_stop = fine_energy_bin_matches[ebin + 1]
-            dFdE[ebin] = np.mean(fine_dFdE[fe_start:fe_stop])
-            dFdE_au[ebin] = np.mean(fine_dFdE_au[fe_start:fe_stop])
+for pk in COSMIC_RAYS:
+    fine_dFdE = airshower_fluxes[pk]["differential_flux"]["values"]
+    fine_dFdE_au = airshower_fluxes[pk]["differential_flux"][
+        "absolute_uncertainty"
+    ]
+    dFdE = np.zeros(energy_bin["num_bins"])
+    dFdE_au = np.zeros(energy_bin["num_bins"])
 
-        diff_flux[sk][pk] = dFdE
-        diff_flux_au[sk][pk] = dFdE_au
+    for ebin in range(energy_bin["num_bins"]):
+        fe_start = fine_energy_bin_matches[ebin]
+        fe_stop = fine_energy_bin_matches[ebin + 1]
+        dFdE[ebin] = np.mean(fine_dFdE[fe_start:fe_stop])
+        dFdE_au[ebin] = np.mean(fine_dFdE_au[fe_start:fe_stop])
 
-        json_utils.write(
-            os.path.join(pa["out_dir"], sk, pk + ".json"),
-            {
-                "energy_binning_key": energy_bin["key"],
-                "differential_flux": dFdE,
-                "absolute_uncertainty": dFdE_au,
-                "unit": "m$^{-2}$ sr$^{-1}$ s$^{-1}$ (GeV)$^{-1}$",
-            },
-        )
+    diff_flux[pk] = dFdE
+    diff_flux_au[pk] = dFdE_au
+
+    json_utils.write(
+        os.path.join(paths["out_dir"], pk + ".json"),
+        {
+            "energy_binning_key": energy_bin["key"],
+            "differential_flux": dFdE,
+            "absolute_uncertainty": dFdE_au,
+            "unit": "m$^{-2}$ sr$^{-1}$ s$^{-1}$ (GeV)$^{-1}$",
+        },
+    )
 
 # plot
 # ----
-for sk in SITES:
-    fig = seb.figure(irf.summary.figure.FIGURE_STYLE)
-    ax = seb.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
-    for pk in COSMIC_RAYS:
-        ax.plot(
-            fine_energy_bin["centers"],
-            airshower_fluxes[sk][pk]["differential_flux"]["values"],
-            color=sum_config["plot"]["particle_colors"][pk],
-        )
-        seb.ax_add_histogram(
-            ax=ax,
-            bin_edges=energy_bin["edges"],
-            bincounts=diff_flux[sk][pk],
-            bincounts_upper=diff_flux[sk][pk] + diff_flux_au[sk][pk],
-            bincounts_lower=diff_flux[sk][pk] - diff_flux_au[sk][pk],
-            linecolor=sum_config["plot"]["particle_colors"][pk],
-            face_color=sum_config["plot"]["particle_colors"][pk],
-            face_alpha=0.2,
-        )
-    ax.set_ylabel(
-        "differential flux /\nm$^{-2}$ sr$^{-1}$ s$^{-1}$ (GeV)$^{-1}$"
+
+fig = seb.figure(irf.summary.figure.FIGURE_STYLE)
+ax = seb.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
+for pk in COSMIC_RAYS:
+    ax.plot(
+        fine_energy_bin["centers"],
+        airshower_fluxes[pk]["differential_flux"]["values"],
+        color=res.analysis["plot"]["particle_colors"][pk],
     )
-    ax.set_xlabel("energy / GeV")
-    ax.set_ylim([1e-6, 1e2])
-    ax.loglog()
-    fig.savefig(os.path.join(pa["out_dir"], sk + ".jpg"))
-    seb.close(fig)
+    seb.ax_add_histogram(
+        ax=ax,
+        bin_edges=energy_bin["edges"],
+        bincounts=diff_flux[pk],
+        bincounts_upper=diff_flux[pk] + diff_flux_au[pk],
+        bincounts_lower=diff_flux[pk] - diff_flux_au[pk],
+        linecolor=res.analysis["plot"]["particle_colors"][pk],
+        face_color=res.analysis["plot"]["particle_colors"][pk],
+        face_alpha=0.2,
+    )
+ax.set_ylabel("differential flux /\nm$^{-2}$ sr$^{-1}$ s$^{-1}$ (GeV)$^{-1}$")
+ax.set_xlabel("energy / GeV")
+ax.set_ylim([1e-6, 1e2])
+ax.loglog()
+ax.text(
+    0.1,
+    0.1,
+    f"site: {res.SITE['name']}",
+    horizontalalignment="center",
+    # verticalalignment="center",
+    transform=ax.transAxes,
+)
+fig.savefig(
+    os.path.join(paths["out_dir"], "airshower_differential_flux_rebinned.jpg")
+)
+seb.close(fig)
