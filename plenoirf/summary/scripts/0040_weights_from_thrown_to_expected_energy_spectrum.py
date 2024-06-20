471de28372e406d06d120c46ea3d00ba17c2d876
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-06-20 13:18:46 +0200

    update 0040

--- plenoirf/summary/scripts/0040_weights_from_thrown_to_expected_energy_spectrum.py
+++ plenoirf/summary/scripts/0040_weights_from_thrown_to_expected_energy_spectrum.py
@@ -7,16 +7,13 @@ import numpy as np
 import sebastians_matplotlib_addons as seb
 import json_utils
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
 
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["plenoirf_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
-seb.matplotlib.rcParams.update(sum_config["plot"]["matplotlib"])
-
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
 os.makedirs(paths["out_dir"], exist_ok=True)
+
+seb.matplotlib.rcParams.update(res.analysis["plot"]["matplotlib"])
+
 
 energy_binning = json_utils.read(
     os.path.join(paths["analysis_dir"], "0005_common_binning", "energy.json")
@@ -24,10 +21,9 @@ energy_binning = json_utils.read(
 energy_bin = energy_binning["trigger_acceptance"]
 fine_energy_bin = energy_binning["interpolation"]
 
-PARTICLES = irf_config["config"]["particles"]
-SITES = irf_config["config"]["sites"]
+PARTICLES = res.PARTICLES
 
-particle_colors = sum_config["plot"]["particle_colors"]
+particle_colors = res.analysis["plot"]["particle_colors"]
 
 # AIRSHOWER RATES
 # ===============
@@ -51,23 +47,19 @@ gamma_reference_source = json_utils.read(
     )
 )
 
-for sk in SITES:
-    _airshower_differential_fluxes[sk]["gamma"] = {}
-    _airshower_differential_fluxes[sk]["gamma"][
-        "differential_flux"
-    ] = gamma_reference_source["differential_flux"]
-
+_airshower_differential_fluxes["gamma"] = {}
+_airshower_differential_fluxes["gamma"][
+    "differential_flux"
+] = gamma_reference_source["differential_flux"]
 
 airshower_rates["rates"] = {}
-for sk in SITES:
-    airshower_rates["rates"][sk] = {}
-    for pk in PARTICLES:
-        airshower_rates["rates"][sk][pk] = (
-            airshower_rates["energy_bin_centers"]
-            * _airshower_differential_fluxes[sk][pk]["differential_flux"][
-                "values"
-            ]
-        )
+
+airshower_rates["rates"] = {}
+for pk in PARTICLES:
+    airshower_rates["rates"][pk] = (
+        airshower_rates["energy_bin_centers"]
+        * _airshower_differential_fluxes[pk]["differential_flux"]["values"]
+    )
 
 # Read features
 # =============
@@ -81,81 +73,86 @@ thrown_spectrum["rates"] = {}
 
 energy_ranges = {}
 
-for sk in SITES:
-    tables[sk] = {}
-    thrown_spectrum["rates"][sk] = {}
-    energy_ranges[sk] = {}
-    for pk in PARTICLES:
-        thrown_spectrum["rates"][sk][pk] = {}
-        energy_ranges[sk][pk] = {}
+tables = {}
+thrown_spectrum["rates"] = {}
+energy_ranges = {}
+for pk in PARTICLES:
+    thrown_spectrum["rates"][pk] = {}
+    energy_ranges[pk] = {}
 
-        _table = snt.read(
-            path=os.path.join(
-                paths["plenoirf_dir"],
-                "event_table",
-                sk,
-                pk,
-                "event_table.tar",
+    _table = snt.read(
+        path=os.path.join(
+            paths["plenoirf_dir"],
+            "response",
+            res.instrument_key,
+            res.site_key,
+            pk,
+            "event_table.tar",
+        )
+    )
+
+    thrown_spectrum["rates"][pk] = np.histogram(
+        _table["primary"]["energy_GeV"],
+        bins=thrown_spectrum["energy_bin_edges"],
+    )[0]
+    energy_ranges[pk]["min"] = np.min(_table["primary"]["energy_GeV"])
+    energy_ranges[pk]["max"] = np.max(_table["primary"]["energy_GeV"])
+
+for pk in PARTICLES:
+    particle_dir = os.path.join(paths["out_dir"], pk)
+    os.makedirs(particle_dir, exist_ok=True)
+
+    w_energy = np.geomspace(
+        energy_ranges[pk]["min"],
+        energy_ranges[pk]["max"],
+        fine_energy_bin["num_bins"],
+    )
+    w_weight = irf.analysis.reweight.reweight(
+        initial_energies=thrown_spectrum["energy_bin_centers"],
+        initial_rates=thrown_spectrum["rates"][pk],
+        target_energies=airshower_rates["energy_bin_centers"],
+        target_rates=airshower_rates["rates"][pk],
+        event_energies=w_energy,
+    )
+
+    json_utils.write(
+        os.path.join(particle_dir, "weights_vs_energy.json"),
+        {
+            "comment": (
+                "Weights vs. energy to transform from thrown "
+                "energy-spectrum to expected energy-spectrum of "
+                "air-showers. In contrast to the energy-spectrum of "
+                "cosmic-rays, this already includes the "
+                "geomagnetic-cutoff."
             ),
-            structure=irf.table.STRUCTURE,
-        )
-
-        thrown_spectrum["rates"][sk][pk] = np.histogram(
-            _table["primary"]["energy_GeV"],
-            bins=thrown_spectrum["energy_bin_edges"],
-        )[0]
-        energy_ranges[sk][pk]["min"] = np.min(_table["primary"]["energy_GeV"])
-        energy_ranges[sk][pk]["max"] = np.max(_table["primary"]["energy_GeV"])
-
-for sk in SITES:
-    for pk in PARTICLES:
-        site_particle_dir = os.path.join(paths["out_dir"], sk, pk)
-        os.makedirs(site_particle_dir, exist_ok=True)
-
-        w_energy = np.geomspace(
-            energy_ranges[sk][pk]["min"],
-            energy_ranges[sk][pk]["max"],
-            fine_energy_bin["num_bins"],
-        )
-        w_weight = irf.analysis.reweight.reweight(
-            initial_energies=thrown_spectrum["energy_bin_centers"],
-            initial_rates=thrown_spectrum["rates"][sk][pk],
-            target_energies=airshower_rates["energy_bin_centers"],
-            target_rates=airshower_rates["rates"][sk][pk],
-            event_energies=w_energy,
-        )
-
-        json_utils.write(
-            os.path.join(site_particle_dir, "weights_vs_energy.json"),
-            {
-                "comment": (
-                    "Weights vs. energy to transform from thrown "
-                    "energy-spectrum to expected energy-spectrum of "
-                    "air-showers. In contrast to the energy-spectrum of "
-                    "cosmic-rays, this already includes the "
-                    "geomagnetic-cutoff."
-                ),
-                "energy_GeV": w_energy,
-                "unit": "1",
-                "mean": w_weight,
-            },
-        )
+            "energy_GeV": w_energy,
+            "unit": "1",
+            "mean": w_weight,
+        },
+    )
 
 weights = json_utils.tree.read(paths["out_dir"])
 
-for sk in SITES:
-    fig = seb.figure(seb.FIGURE_16_9)
-    ax = seb.add_axes(fig=fig, span=(0.1, 0.1, 0.8, 0.8))
-    for pk in PARTICLES:
-        ax.plot(
-            weights[sk][pk]["weights_vs_energy"]["energy_GeV"],
-            weights[sk][pk]["weights_vs_energy"]["mean"],
-            color=particle_colors[pk],
-        )
-    ax.loglog()
-    ax.set_xlabel("energy / GeV")
-    ax.set_ylabel("relative re-weights/ 1")
-    ax.set_xlim([1e-1, 1e4])
-    ax.set_ylim([1e-6, 1.0])
-    fig.savefig(os.path.join(paths["out_dir"], "{:s}_weights.jpg".format(sk)))
-    seb.close(fig)
+fig = seb.figure(seb.FIGURE_16_9)
+ax = seb.add_axes(fig=fig, span=(0.1, 0.1, 0.8, 0.8))
+for pk in PARTICLES:
+    ax.plot(
+        weights[pk]["weights_vs_energy"]["energy_GeV"],
+        weights[pk]["weights_vs_energy"]["mean"],
+        color=particle_colors[pk],
+    )
+ax.loglog()
+ax.set_xlabel("energy / GeV")
+ax.set_ylabel("relative re-weights/ 1")
+ax.set_xlim([1e-1, 1e4])
+ax.set_ylim([1e-6, 1.0])
+ax.text(
+    0.1,
+    0.1,
+    f"site: {res.SITE['name']}",
+    horizontalalignment="center",
+    # verticalalignment="center",
+    transform=ax.transAxes,
+)
+fig.savefig(os.path.join(paths["out_dir"], "weights.jpg"))
+seb.close(fig)
