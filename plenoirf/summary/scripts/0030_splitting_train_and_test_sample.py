09e2f4fc044454b108d734f2830d4cd8cad65a91
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-06-20 13:09:05 +0200

    update 0030

--- plenoirf/summary/scripts/0030_splitting_train_and_test_sample.py
+++ plenoirf/summary/scripts/0030_splitting_train_and_test_sample.py
@@ -6,50 +6,37 @@ import os
 import sklearn
 import json_utils
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
 
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["run_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
+os.makedirs(paths["out_dir"], exist_ok=True)
 
-SITES = irf_config["config"]["sites"]
-PARTICLES = irf_config["config"]["particles"]
-summary_random_seed = sum_config["random_seed"]
-test_size = sum_config["train_and_test"]["test_size"]
+for pk in res.PARTICLES:
+    event_table = snt.read(
+        path=os.path.join(
+            paths["plenoirf_dir"],
+            "response",
+            res.instrument_key,
+            res.site_key,
+            pk,
+            "event_table.tar",
+        )
+    )
 
+    train_idxs, test_idxs = sklearn.model_selection.train_test_split(
+        event_table["primary"][snt.IDX],
+        test_size=res.analysis["train_and_test"]["test_size"],
+        random_state=res.analysis["random_seed"],
+    )
 
-for sk in SITES:
-    for pk in PARTICLES:
-        site_dir = os.path.join(paths["out_dir"], sk)
-        os.makedirs(site_dir, exist_ok=True)
-
-        event_table = snt.read(
-            path=os.path.join(
-                paths["run_dir"],
-                "event_table",
-                sk,
-                pk,
-                "event_table.tar",
+    json_utils.write(
+        os.path.join(paths["out_dir"], pk + ".json"),
+        {
+            "comment": (
+                "Split into train-sample and test-sample to "
+                "validate machine-learning."
             ),
-            structure=irf.table.STRUCTURE,
-        )
-
-        train_idxs, test_idxs = sklearn.model_selection.train_test_split(
-            event_table["primary"][snt.IDX],
-            test_size=test_size,
-            random_state=summary_random_seed,
-        )
-
-        json_utils.write(
-            os.path.join(site_dir, pk + ".json"),
-            {
-                "comment": (
-                    "Split into train-sample and test-sample to "
-                    "validate machine-learning."
-                ),
-                "train": train_idxs,
-                "test": test_idxs,
-            },
-        )
+            "train": train_idxs,
+            "test": test_idxs,
+        },
+    )
