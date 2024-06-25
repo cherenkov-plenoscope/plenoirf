465df8178b6aa73217845b2af31697506fa379e7
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-06-25 15:53:32 +0200

    first try to estimate instrument response

--- plenoirf/summary/scripts/0100_trigger_acceptance_for_cosmic_particles.py
+++ plenoirf/summary/scripts/0100_trigger_acceptance_for_cosmic_particles.py
@@ -7,170 +7,172 @@ import sparse_numeric_table as snt
 import os
 import json_utils
 
-
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
-
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["plenoirf_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
-
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
 os.makedirs(paths["out_dir"], exist_ok=True)
 
-PARTICLES = irf_config["config"]["particles"]
-SITES = irf_config["config"]["sites"]
-MAX_SOURCE_ANGLE_DEG = sum_config["gamma_ray_source_direction"][
+MAX_SOURCE_ANGLE_DEG = res.analysis["gamma_ray_source_direction"][
     "max_angle_relative_to_pointing_deg"
 ]
-pointing_azimuth_deg = irf_config["config"]["plenoscope_pointing"][
-    "azimuth_deg"
-]
-pointing_zenith_deg = irf_config["config"]["plenoscope_pointing"]["zenith_deg"]
 
 energy_bin = json_utils.read(
     os.path.join(paths["analysis_dir"], "0005_common_binning", "energy.json")
 )["trigger_acceptance"]
 
+trigger_thresholds = res.analysis["trigger"][res.site_key][
+    "ratescan_thresholds_pe"
+]
+trigger_modus = res.analysis["trigger"][res.site_key]["modus"]
 
-for sk in SITES:
-    trigger_thresholds = sum_config["trigger"][sk]["ratescan_thresholds_pe"]
-    trigger_modus = sum_config["trigger"][sk]["modus"]
+for pk in res.PARTICLES:
+    pk_dir = os.path.join(paths["out_dir"], pk)
+    os.makedirs(pk_dir, exist_ok=True)
 
-    for pk in PARTICLES:
-        site_particle_dir = os.path.join(paths["out_dir"], sk, pk)
+    diffuse_particle_table = res.read_event_table(particle_key=pk)
 
-        os.makedirs(site_particle_dir, exist_ok=True)
+    _diff = snt.cut_on_common_indices(
+        table=diffuse_particle_table,
+        level_keys=["primary", "instrument_pointing"],
+    )
 
-        diffuse_particle_table = snt.read(
-            path=os.path.join(
-                paths["plenoirf_dir"],
-                "event_table",
-                sk,
-                pk,
-                "event_table.tar",
+    # point source
+    # ------------
+    idx_possible_onregion = (
+        irf.analysis.cuts.cut_primary_direction_within_angle(
+            primary_table=_diff["primary"],
+            radial_angle_deg=MAX_SOURCE_ANGLE_DEG,
+            azimuth_deg=np.rad2deg(
+                _diff["instrument_pointing"]["azimuth_rad"]
             ),
-            structure=irf.table.STRUCTURE,
+            zenith_deg=np.rad2deg(_diff["instrument_pointing"]["zenith_rad"]),
         )
+    )
 
-        # point source
-        # ------------
-        idx_possible_onregion = (
-            irf.analysis.cuts.cut_primary_direction_within_angle(
-                primary_table=diffuse_particle_table["primary"],
-                radial_angle_deg=MAX_SOURCE_ANGLE_DEG,
-                azimuth_deg=pointing_azimuth_deg,
-                zenith_deg=pointing_zenith_deg,
-            )
+    point_particle_table = snt.cut_table_on_indices(
+        table=diffuse_particle_table,
+        common_indices=idx_possible_onregion,
+    )
+
+    has_groundgrid_result = snt.make_mask_of_right_in_left(
+        left_indices=point_particle_table["primary"][snt.IDX],
+        right_indices=point_particle_table["groundgrid_result"][snt.IDX],
+    )
+
+    energy_GeV = point_particle_table["primary"]["energy_GeV"]
+    quantity_scatter = point_particle_table["groundgrid"][
+        "area_thrown_m2"
+    ] * np.cos(point_particle_table["primary"]["zenith_rad"])
+    num_grid_cells_above_lose_threshold = np.zeros(shape=len(energy_GeV))
+    num_grid_cells_above_lose_threshold[
+        has_groundgrid_result
+    ] = point_particle_table["groundgrid_result"]["num_bins_above_threshold"]
+    total_num_grid_cells = point_particle_table["groundgrid"][
+        "num_bins_thrown"
+    ]
+
+    value = []
+    absolute_uncertainty = []
+    for threshold in trigger_thresholds:
+        print(pk, threshold)
+        idx_detected = irf.analysis.light_field_trigger_modi.make_indices(
+            trigger_table=point_particle_table["trigger"],
+            threshold=threshold,
+            modus=trigger_modus,
         )
-
-        point_particle_table = snt.cut_table_on_indices(
-            table=diffuse_particle_table,
-            common_indices=idx_possible_onregion,
+        mask_detected = snt.make_mask_of_right_in_left(
+            left_indices=point_particle_table["primary"][snt.IDX],
+            right_indices=idx_detected,
         )
-
-        energy_GeV = point_particle_table["primary"]["energy_GeV"]
-        quantity_scatter = point_particle_table["grid"]["area_thrown_m2"]
-        num_grid_cells_above_lose_threshold = point_particle_table["grid"][
-            "num_bins_above_threshold"
-        ]
-        total_num_grid_cells = point_particle_table["grid"]["num_bins_thrown"]
-
-        value = []
-        absolute_uncertainty = []
-        for threshold in trigger_thresholds:
-            idx_detected = irf.analysis.light_field_trigger_modi.make_indices(
-                trigger_table=point_particle_table["trigger"],
-                threshold=threshold,
-                modus=trigger_modus,
-            )
-            mask_detected = snt.make_mask_of_right_in_left(
-                left_indices=point_particle_table["primary"][snt.IDX],
-                right_indices=idx_detected,
-            )
-            (
-                _q_eff,
-                _q_eff_au,
-            ) = atmospheric_cherenkov_response.analysis.effective_quantity_for_grid(
-                energy_bin_edges_GeV=energy_bin["edges"],
-                energy_GeV=energy_GeV,
-                mask_detected=mask_detected,
-                quantity_scatter=quantity_scatter,
-                num_grid_cells_above_lose_threshold=(
-                    num_grid_cells_above_lose_threshold
-                ),
-                total_num_grid_cells=total_num_grid_cells,
-            )
-            value.append(_q_eff)
-            absolute_uncertainty.append(_q_eff_au)
-
-        json_utils.write(
-            os.path.join(site_particle_dir, "point.json"),
-            {
-                "comment": (
-                    "Effective area for a point source. "
-                    "VS trigger-ratescan-thresholds VS energy-bins"
-                ),
-                "energy_bin_edges_GeV": energy_bin["edges"],
-                "trigger": sum_config["trigger"],
-                "unit": "m$^{2}$",
-                "mean": value,
-                "absolute_uncertainty": absolute_uncertainty,
-            },
+        (
+            _q_eff,
+            _q_eff_au,
+        ) = atmospheric_cherenkov_response.analysis.effective_quantity_for_grid(
+            energy_bin_edges_GeV=energy_bin["edges"],
+            energy_GeV=energy_GeV,
+            mask_detected=mask_detected,
+            quantity_scatter=quantity_scatter,
+            num_grid_cells_above_lose_threshold=(
+                num_grid_cells_above_lose_threshold
+            ),
+            total_num_grid_cells=total_num_grid_cells,
         )
+        value.append(_q_eff)
+        absolute_uncertainty.append(_q_eff_au)
 
-        # diffuse source
-        # --------------
-        energy_GeV = diffuse_particle_table["primary"]["energy_GeV"]
-        quantity_scatter = (
-            diffuse_particle_table["grid"]["area_thrown_m2"]
-            * diffuse_particle_table["primary"]["solid_angle_thrown_sr"]
+    json_utils.write(
+        os.path.join(pk_dir, "point.json"),
+        {
+            "comment": (
+                "Effective area for a point source. "
+                "VS trigger-ratescan-thresholds VS energy-bins"
+            ),
+            "energy_bin_edges_GeV": energy_bin["edges"],
+            "unit": "m$^{2}$",
+            "mean": value,
+            "absolute_uncertainty": absolute_uncertainty,
+        },
+    )
+
+    # diffuse source
+    # --------------
+
+    has_groundgrid_result = snt.make_mask_of_right_in_left(
+        left_indices=diffuse_particle_table["primary"][snt.IDX],
+        right_indices=diffuse_particle_table["groundgrid_result"][snt.IDX],
+    )
+
+    energy_GeV = diffuse_particle_table["primary"]["energy_GeV"]
+    quantity_scatter = (
+        diffuse_particle_table["groundgrid"]["area_thrown_m2"]
+        * np.cos(diffuse_particle_table["primary"]["zenith_rad"])
+        * diffuse_particle_table["primary"]["solid_angle_thrown_sr"]
+    )
+    num_grid_cells_above_lose_threshold = np.zeros(shape=len(energy_GeV))
+    num_grid_cells_above_lose_threshold[
+        has_groundgrid_result
+    ] = diffuse_particle_table["groundgrid_result"]["num_bins_above_threshold"]
+    total_num_grid_cells = diffuse_particle_table["groundgrid"][
+        "num_bins_thrown"
+    ]
+
+    value = []
+    absolute_uncertainty = []
+    for threshold in trigger_thresholds:
+        idx_detected = irf.analysis.light_field_trigger_modi.make_indices(
+            trigger_table=diffuse_particle_table["trigger"],
+            threshold=threshold,
+            modus=trigger_modus,
         )
-        num_grid_cells_above_lose_threshold = diffuse_particle_table["grid"][
-            "num_bins_above_threshold"
-        ]
-        total_num_grid_cells = diffuse_particle_table["grid"][
-            "num_bins_thrown"
-        ]
-
-        value = []
-        absolute_uncertainty = []
-        for threshold in trigger_thresholds:
-            idx_detected = irf.analysis.light_field_trigger_modi.make_indices(
-                trigger_table=diffuse_particle_table["trigger"],
-                threshold=threshold,
-                modus=trigger_modus,
-            )
-            mask_detected = snt.make_mask_of_right_in_left(
-                left_indices=diffuse_particle_table["primary"][snt.IDX],
-                right_indices=idx_detected,
-            )
-            (
-                _q_eff,
-                _q_eff_au,
-            ) = atmospheric_cherenkov_response.analysis.effective_quantity_for_grid(
-                energy_bin_edges_GeV=energy_bin["edges"],
-                energy_GeV=energy_GeV,
-                mask_detected=mask_detected,
-                quantity_scatter=quantity_scatter,
-                num_grid_cells_above_lose_threshold=(
-                    num_grid_cells_above_lose_threshold
-                ),
-                total_num_grid_cells=total_num_grid_cells,
-            )
-            value.append(_q_eff)
-            absolute_uncertainty.append(_q_eff_au)
-
-        json_utils.write(
-            os.path.join(site_particle_dir, "diffuse.json"),
-            {
-                "comment": (
-                    "Effective acceptance (area x solid angle) "
-                    "for a diffuse source. "
-                    "VS trigger-ratescan-thresholds VS energy-bins"
-                ),
-                "unit": "m$^{2}$ sr",
-                "mean": value,
-                "absolute_uncertainty": absolute_uncertainty,
-            },
+        mask_detected = snt.make_mask_of_right_in_left(
+            left_indices=diffuse_particle_table["primary"][snt.IDX],
+            right_indices=idx_detected,
         )
+        (
+            _q_eff,
+            _q_eff_au,
+        ) = atmospheric_cherenkov_response.analysis.effective_quantity_for_grid(
+            energy_bin_edges_GeV=energy_bin["edges"],
+            energy_GeV=energy_GeV,
+            mask_detected=mask_detected,
+            quantity_scatter=quantity_scatter,
+            num_grid_cells_above_lose_threshold=(
+                num_grid_cells_above_lose_threshold
+            ),
+            total_num_grid_cells=total_num_grid_cells,
+        )
+        value.append(_q_eff)
+        absolute_uncertainty.append(_q_eff_au)
+
+    json_utils.write(
+        os.path.join(pk_dir, "diffuse.json"),
+        {
+            "comment": (
+                "Effective acceptance (area x solid angle) "
+                "for a diffuse source. "
+                "VS trigger-ratescan-thresholds VS energy-bins"
+            ),
+            "unit": "m$^{2}$ sr",
+            "mean": value,
+            "absolute_uncertainty": absolute_uncertainty,
+        },
+    )
--- plenoirf/summary/scripts/0101_trigger_acceptance_for_cosmic_particles_plot.py
+++ plenoirf/summary/scripts/0101_trigger_acceptance_for_cosmic_particles_plot.py
@@ -6,19 +6,10 @@ import os
 import sebastians_matplotlib_addons as seb
 import json_utils
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
-
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["plenoirf_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
-seb.matplotlib.rcParams.update(sum_config["plot"]["matplotlib"])
-
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
 os.makedirs(paths["out_dir"], exist_ok=True)
-
-SITES = irf_config["config"]["sites"]
-PARTICLES = irf_config["config"]["particles"]
+seb.matplotlib.rcParams.update(res.analysis["plot"]["matplotlib"])
 
 cr = json_utils.tree.read(
     os.path.join(
@@ -30,83 +21,65 @@ energy_bin = json_utils.read(
     os.path.join(paths["analysis_dir"], "0005_common_binning", "energy.json")
 )["trigger_acceptance"]
 
-particle_colors = sum_config["plot"]["particle_colors"]
+particle_colors = res.analysis["plot"]["particle_colors"]
 
-for sk in SITES:
-    trigger_thresholds = np.array(
-        sum_config["trigger"][sk]["ratescan_thresholds_pe"]
-    )
-    analysis_trigger_threshold = sum_config["trigger"][sk]["threshold_pe"]
+trigger_thresholds = np.array(
+    res.analysis["trigger"][res.site_key]["ratescan_thresholds_pe"]
+)
+analysis_trigger_threshold = res.analysis["trigger"][res.site_key][
+    "threshold_pe"
+]
 
-    for source_key in irf.summary.figure.SOURCES:
-        for tt in range(len(trigger_thresholds)):
-            fig = seb.figure(irf.summary.figure.FIGURE_STYLE)
-            ax = seb.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
+for source_key in irf.summary.figure.SOURCES:
+    for tt in range(len(trigger_thresholds)):
+        fig = seb.figure(irf.summary.figure.FIGURE_STYLE)
+        ax = seb.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
 
-            text_y = 0
-            for pk in PARTICLES:
-                Q = np.array(cr[sk][pk][source_key]["mean"][tt])
-                Q_au = np.array(
-                    cr[sk][pk][source_key]["absolute_uncertainty"][tt]
-                )
+        text_y = 0
+        for pk in res.PARTICLES:
+            Q = np.array(cr[pk][source_key]["mean"][tt])
+            Q_au = np.array(cr[pk][source_key]["absolute_uncertainty"][tt])
 
-                seb.ax_add_histogram(
-                    ax=ax,
-                    bin_edges=energy_bin["edges"],
-                    bincounts=Q,
-                    linestyle="-",
-                    linecolor=particle_colors[pk],
-                    bincounts_upper=Q + Q_au,
-                    bincounts_lower=Q - Q_au,
-                    face_color=particle_colors[pk],
-                    face_alpha=0.25,
-                )
-
-                ax.text(
-                    0.9,
-                    0.1 + text_y,
-                    pk,
-                    color=particle_colors[pk],
-                    transform=ax.transAxes,
-                )
-                text_y += 0.06
-
-            ax.set_xlabel("energy / GeV")
-            ax.set_ylabel(
-                "{:s} / {:s}".format(
-                    irf.summary.figure.SOURCES[source_key]["label"],
-                    irf.summary.figure.SOURCES[source_key]["unit"],
-                )
+            seb.ax_add_histogram(
+                ax=ax,
+                bin_edges=energy_bin["edges"],
+                bincounts=Q,
+                linestyle="-",
+                linecolor=particle_colors[pk],
+                bincounts_upper=Q + Q_au,
+                bincounts_lower=Q - Q_au,
+                face_color=particle_colors[pk],
+                face_alpha=0.25,
             )
-            ax.set_ylim(
-                irf.summary.figure.SOURCES[source_key]["limits"][
-                    "passed_trigger"
-                ]
-            )
-            ax.loglog()
-            ax.set_xlim(energy_bin["limits"])
 
-            if trigger_thresholds[tt] == analysis_trigger_threshold:
-                fig.savefig(
-                    os.path.join(
-                        paths["out_dir"],
-                        "{:s}_{:s}.jpg".format(
-                            sk,
-                            source_key,
-                        ),
-                    )
-                )
-            ax.set_title(
-                "trigger-threshold: {:d} p.e.".format(trigger_thresholds[tt])
+            ax.text(
+                0.9,
+                0.1 + text_y,
+                pk,
+                color=particle_colors[pk],
+                transform=ax.transAxes,
             )
-            fig.savefig(
-                os.path.join(
-                    paths["out_dir"],
-                    "{:s}_{:s}_{:06d}.jpg".format(
-                        sk,
-                        source_key,
-                        tt,
-                    ),
-                )
+            text_y += 0.06
+
+        ax.set_xlabel("energy / GeV")
+        ax.set_ylabel(
+            "{:s} / {:s}".format(
+                irf.summary.figure.SOURCES[source_key]["label"],
+                irf.summary.figure.SOURCES[source_key]["unit"],
             )
-            seb.close(fig)
+        )
+        ax.set_ylim(
+            irf.summary.figure.SOURCES[source_key]["limits"]["passed_trigger"]
+        )
+        ax.loglog()
+        ax.set_xlim(energy_bin["limits"])
+
+        if trigger_thresholds[tt] == analysis_trigger_threshold:
+            fig.savefig(os.path.join(paths["out_dir"], f"{source_key:s}.jpg"))
+        ax.set_title(
+            "trigger-threshold: {:d} p.e.".format(trigger_thresholds[tt])
+        )
+        fig.savefig(
+            os.path.join(paths["out_dir"], f"{source_key:s}_{tt:06d}.jpg")
+        )
+        seb.close(fig)
