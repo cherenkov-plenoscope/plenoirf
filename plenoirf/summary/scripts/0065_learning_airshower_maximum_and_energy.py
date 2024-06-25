ad86f7daa7820b7874bd079d8649b55b0c5ce1b6
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-06-25 13:11:28 +0200

    update 0065

--- plenoirf/summary/scripts/0065_learning_airshower_maximum_and_energy.py
+++ plenoirf/summary/scripts/0065_learning_airshower_maximum_and_energy.py
@@ -13,15 +13,9 @@ from sklearn import ensemble
 from sklearn import model_selection
 from sklearn import utils
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
-
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
 os.makedirs(paths["out_dir"], exist_ok=True)
-
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["plenoirf_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
 
 train_test = json_utils.tree.read(
     os.path.join(
@@ -42,10 +36,9 @@ passing_trajectory = json_utils.tree.read(
     os.path.join(paths["analysis_dir"], "0059_passing_trajectory_quality")
 )
 
-random_seed = sum_config["random_seed"]
+random_seed = res.analysis["random_seed"]
 
-SITES = irf_config["config"]["sites"]
-PARTICLES = irf_config["config"]["particles"]
+PARTICLES = res.PARTICLES
 NON_GAMMA_PARTICLES = dict(PARTICLES)
 NON_GAMMA_PARTICLES.pop("gamma")
 
@@ -79,7 +72,7 @@ min_number_samples = 100
 
 
 def read_event_frame(
-    site_key,
+    res,
     particle_key,
     run_dir,
     transformed_features_dir,
@@ -88,43 +81,28 @@ def read_event_frame(
     train_test,
     level_keys,
 ):
-    sk = site_key
     pk = particle_key
 
-    airshower_table = snt.read(
-        path=os.path.join(
-            run_dir,
-            "event_table",
-            sk,
-            pk,
-            "event_table.tar",
-        ),
-        structure=irf.table.STRUCTURE,
-    )
+    airshower_table = res.read_event_table(particle_key=pk)
 
     airshower_table["transformed_features"] = snt.read(
-        path=os.path.join(
+        os.path.join(
             transformed_features_dir,
-            sk,
             pk,
             "transformed_features.tar",
-        ),
-        structure=irf.features.TRANSFORMED_FEATURE_STRUCTURE,
+        )
     )["transformed_features"]
 
-    EXT_STRUCTRURE = dict(irf.table.STRUCTURE)
-    EXT_STRUCTRURE[
-        "transformed_features"
-    ] = irf.features.TRANSFORMED_FEATURE_STRUCTURE["transformed_features"]
+    EXT_STRUCTRURE = irf.features.init_all_features_structure()
 
     out = {}
     for kk in ["test", "train"]:
         idxs_valid_kk = snt.intersection(
             [
-                passing_trigger[sk][pk]["idx"],
-                passing_quality[sk][pk]["idx"],
-                passing_trajectory[sk][pk]["idx"],
-                train_test[sk][pk][kk],
+                passing_trigger[pk]["idx"],
+                passing_quality[pk]["idx"],
+                passing_trajectory[pk]["idx"],
+                train_test[pk][kk],
             ]
         )
         table_kk = snt.cut_and_sort_table_on_indices(
@@ -178,105 +156,96 @@ def make_x_y_arrays(event_frame):
     y = np.array(
         [
             np.log10(f["primary/energy_GeV"].values),
-            np.log10(f["cherenkovpool/maximum_asl_m"].values),
+            np.log10(f["cherenkovpool/z_emission_p50_m"].values),
         ]
     ).T
     return x, y
 
 
 train_test_gamma_energy = {}
-for sk in SITES:
-    train_test_gamma_energy[sk] = {}
-    for pk in PARTICLES:
-        if pk == "gamma":
-            train_test_gamma_energy[sk][pk] = {}
-            train_test_gamma_energy[sk][pk]["train"] = train_test[sk][pk][
-                "train"
-            ]
-            train_test_gamma_energy[sk][pk]["test"] = train_test[sk][pk][
-                "test"
-            ]
-        else:
-            train_test_gamma_energy[sk][pk] = {}
-            train_test_gamma_energy[sk][pk]["train"] = []
-            train_test_gamma_energy[sk][pk]["test"] = np.concatenate(
-                [train_test[sk][pk]["train"], train_test[sk][pk]["test"]]
-            )
-
-
-for sk in SITES:
-    particle_frames = {}
-    for pk in PARTICLES:
-        particle_frames[pk] = read_event_frame(
-            site_key=sk,
-            particle_key=pk,
-            run_dir=paths["plenoirf_dir"],
-            transformed_features_dir=transformed_features_dir,
-            passing_trigger=passing_trigger,
-            passing_quality=passing_quality,
-            train_test=train_test_gamma_energy,
-            level_keys=level_keys,
+for pk in PARTICLES:
+    if pk == "gamma":
+        train_test_gamma_energy[pk] = {}
+        train_test_gamma_energy[pk]["train"] = train_test[pk]["train"]
+        train_test_gamma_energy[pk]["test"] = train_test[pk]["test"]
+    else:
+        train_test_gamma_energy[pk] = {}
+        train_test_gamma_energy[pk]["train"] = []
+        train_test_gamma_energy[pk]["test"] = np.concatenate(
+            [train_test[pk]["train"], train_test[pk]["test"]]
         )
 
-    # prepare sets
-    # ------------
-    MA = {}
+
+particle_frames = {}
+for pk in PARTICLES:
+    particle_frames[pk] = read_event_frame(
+        res=res,
+        particle_key=pk,
+        run_dir=paths["plenoirf_dir"],
+        transformed_features_dir=transformed_features_dir,
+        passing_trigger=passing_trigger,
+        passing_quality=passing_quality,
+        train_test=train_test_gamma_energy,
+        level_keys=level_keys,
+    )
+
+# prepare sets
+# ------------
+MA = {}
+for pk in PARTICLES:
+    MA[pk] = {}
+    for mk in ["test", "train"]:
+        MA[pk][mk] = {}
+        MA[pk][mk]["x"], MA[pk][mk]["y"] = make_x_y_arrays(
+            event_frame=particle_frames[pk][mk]
+        )
+
+# train model on gamma only
+# -------------------------
+num_features = MA["gamma"]["train"]["x"].shape[1]
+models = {}
+
+models["MultiLayerPerceptron"] = sklearn.neural_network.MLPRegressor(
+    solver="lbfgs",
+    alpha=1e-2,
+    hidden_layer_sizes=(num_features, num_features, num_features),
+    random_state=random_seed,
+    verbose=False,
+    max_iter=5000,
+    learning_rate_init=0.1,
+)
+models["RandomForest"] = sklearn.ensemble.RandomForestRegressor(
+    random_state=random_seed,
+    n_estimators=10,
+)
+
+_X_shuffle, _y_shuffle = sklearn.utils.shuffle(
+    MA["gamma"]["train"]["x"],
+    MA["gamma"]["train"]["y"],
+    random_state=random_seed,
+)
+
+for mk in models:
+    models[mk].fit(_X_shuffle, _y_shuffle)
+
+    model_path = os.path.join(paths["out_dir"], mk + ".pkl")
+    with open(model_path, "wb") as fout:
+        fout.write(pickle.dumps(models[mk]))
+
     for pk in PARTICLES:
-        MA[pk] = {}
-        for mk in ["test", "train"]:
-            MA[pk][mk] = {}
-            MA[pk][mk]["x"], MA[pk][mk]["y"] = make_x_y_arrays(
-                event_frame=particle_frames[pk][mk]
-            )
+        _y_score = models[mk].predict(MA[pk]["test"]["x"])
 
-    # train model on gamma only
-    # -------------------------
-    num_features = MA["gamma"]["train"]["x"].shape[1]
-    models = {}
+        for tk in targets:
+            y_true = 10 ** MA[pk]["test"]["y"][:, targets[tk]["idx"]]
+            y_score = 10 ** _y_score[:, targets[tk]["idx"]]
 
-    models["MultiLayerPerceptron"] = sklearn.neural_network.MLPRegressor(
-        solver="lbfgs",
-        alpha=1e-2,
-        hidden_layer_sizes=(num_features, num_features, num_features),
-        random_state=random_seed,
-        verbose=False,
-        max_iter=5000,
-        learning_rate_init=0.1,
-    )
-    models["RandomForest"] = sklearn.ensemble.RandomForestRegressor(
-        random_state=random_seed,
-        n_estimators=10,
-    )
+            out = {}
+            out["comment"] = "Reconstructed from the test-set."
+            out["learner"] = mk
+            out[tk] = y_score
+            out["unit"] = targets[tk]["unit"]
+            out["idx"] = np.array(particle_frames[pk]["test"]["idx"])
 
-    _X_shuffle, _y_shuffle = sklearn.utils.shuffle(
-        MA["gamma"]["train"]["x"],
-        MA["gamma"]["train"]["y"],
-        random_state=random_seed,
-    )
-
-    for mk in models:
-        models[mk].fit(_X_shuffle, _y_shuffle)
-
-        model_path = os.path.join(paths["out_dir"], mk + ".pkl")
-        with open(model_path, "wb") as fout:
-            fout.write(pickle.dumps(models[mk]))
-
-        for pk in PARTICLES:
-            _y_score = models[mk].predict(MA[pk]["test"]["x"])
-
-            for tk in targets:
-                y_true = 10 ** MA[pk]["test"]["y"][:, targets[tk]["idx"]]
-                y_score = 10 ** _y_score[:, targets[tk]["idx"]]
-
-                out = {}
-                out["comment"] = "Reconstructed from the test-set."
-                out["learner"] = mk
-                out[tk] = y_score
-                out["unit"] = targets[tk]["unit"]
-                out["idx"] = np.array(particle_frames[pk]["test"]["idx"])
-
-                site_particle_dir = os.path.join(paths["out_dir"], sk, pk)
-                os.makedirs(site_particle_dir, exist_ok=True)
-                json_utils.write(
-                    os.path.join(site_particle_dir, tk + ".json"), out
-                )
+            pk_dir = os.path.join(paths["out_dir"], pk)
+            os.makedirs(pk_dir, exist_ok=True)
+            json_utils.write(os.path.join(pk_dir, tk + ".json"), out)
