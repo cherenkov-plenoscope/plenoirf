b0c7d06f000cd370fad508173d5f6682819e453a
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-06-25 13:51:56 +0200

    update 0074 and 0075

--- plenoirf/summary/scripts/0074_trigger_probability_vs_cherenkov_density_on_ground.py
+++ plenoirf/summary/scripts/0074_trigger_probability_vs_cherenkov_density_on_ground.py
@@ -5,22 +5,11 @@ from os.path import join as opj
 import numpy as np
 import sparse_numeric_table as snt
 import plenoirf as irf
-import sebastians_matplotlib_addons as seb
 import json_utils
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
-
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["plenoirf_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
-seb.matplotlib.rcParams.update(sum_config["plot"]["matplotlib"])
-
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
 os.makedirs(paths["out_dir"], exist_ok=True)
-
-SITES = irf_config["config"]["sites"]
-PARTICLES = irf_config["config"]["particles"]
 
 trigger_modi = {}
 trigger_modi["passing_trigger"] = json_utils.tree.read(
@@ -35,63 +24,60 @@ trigger_modi[
     )
 )
 
-grid_bin_area_m2 = irf_config["grid_geometry"]["bin_area"]
+grid_bin_area_m2 = res.config["ground_grid"]["geometry"]["bin_width_m"] ** 2.0
 density_bin_edges_per_m2 = np.geomspace(1e-3, 1e4, 7 * 5 + 1)
 
+for pk in res.PARTICLES:
+    pk_dir = opj(paths["out_dir"], pk)
+    os.makedirs(pk_dir, exist_ok=True)
 
-for sk in SITES:
-    for pk in PARTICLES:
-        site_particle_dir = opj(paths["out_dir"], sk, pk)
-        os.makedirs(site_particle_dir, exist_ok=True)
+    event_table = res.read_event_table(particle_key=pk)
+    event_table = snt.cut_and_sort_table_on_indices(
+        table=event_table,
+        common_indices=event_table["trigger"][snt.IDX],
+        level_keys=["trigger", "cherenkovsizepart", "instrument_pointing"],
+    )
+    df = snt.make_rectangular_DataFrame(table=event_table)
 
-        event_table = snt.read(
-            path=os.path.join(
-                paths["plenoirf_dir"],
-                "event_table",
-                sk,
-                pk,
-                "event_table.tar",
-            ),
-            structure=irf.table.STRUCTURE,
+    for tm in trigger_modi:
+        mask_pasttrigger = snt.make_mask_of_right_in_left(
+            left_indices=df[snt.IDX].values,
+            right_indices=trigger_modi[tm][pk]["idx"],
+        ).astype(float)
+
+        projected_area_m2 = (
+            np.cos(df["instrument_pointing/zenith_rad"]) * grid_bin_area_m2
         )
 
-        for tm in trigger_modi:
-            mask_pasttrigger = snt.make_mask_of_right_in_left(
-                left_indices=event_table["trigger"][snt.IDX],
-                right_indices=trigger_modi[tm][sk][pk]["idx"],
-            )
+        num_thrown = np.histogram(
+            df["cherenkovsizepart/num_photons"] / projected_area_m2,
+            bins=density_bin_edges_per_m2,
+        )[0]
 
-            num_thrown = np.histogram(
-                event_table["cherenkovsizepart"]["num_photons"]
-                / grid_bin_area_m2,
-                bins=density_bin_edges_per_m2,
-            )[0]
+        num_pasttrigger = np.histogram(
+            df["cherenkovsizepart/num_photons"] / projected_area_m2,
+            bins=density_bin_edges_per_m2,
+            weights=mask_pasttrigger,
+        )[0]
 
-            num_pasttrigger = np.histogram(
-                event_table["cherenkovsizepart"]["num_photons"]
-                / grid_bin_area_m2,
-                bins=density_bin_edges_per_m2,
-                weights=mask_pasttrigger,
-            )[0]
+        trigger_probability = irf.utils._divide_silent(
+            numerator=num_pasttrigger,
+            denominator=num_thrown,
+            default=np.nan,
+        )
 
-            trigger_probability = irf.utils._divide_silent(
-                numerator=num_pasttrigger,
-                denominator=num_thrown,
-                default=np.nan,
-            )
+        trigger_probability_unc = irf.utils._divide_silent(
+            numerator=np.sqrt(num_pasttrigger),
+            denominator=num_pasttrigger,
+            default=np.nan,
+        )
 
-            trigger_probability_unc = irf.utils._divide_silent(
-                numerator=np.sqrt(num_pasttrigger),
-                denominator=num_pasttrigger,
-                default=np.nan,
-            )
-
-            json_utils.write(
-                os.path.join(site_particle_dir, tm + ".json"),
-                {
-                    "Cherenkov_density_bin_edges_per_m2": density_bin_edges_per_m2,
-                    "unit": "1",
-                    "mean": trigger_probability,
-                    "relative_uncertainty": trigger_probability_unc,
-                },
-            )
+        json_utils.write(
+            os.path.join(pk_dir, tm + ".json"),
+            {
+                "Cherenkov_density_bin_edges_per_m2": density_bin_edges_per_m2,
+                "unit": "1",
+                "mean": trigger_probability,
+                "relative_uncertainty": trigger_probability_unc,
+            },
+        )
--- plenoirf/summary/scripts/0075_trigger_probability_vs_cherenkov_density_on_ground_plot.py
+++ plenoirf/summary/scripts/0075_trigger_probability_vs_cherenkov_density_on_ground_plot.py
@@ -7,16 +7,10 @@ from os.path import join as opj
 import sebastians_matplotlib_addons as seb
 import json_utils
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
-
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["plenoirf_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
-seb.matplotlib.rcParams.update(sum_config["plot"]["matplotlib"])
-
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
 os.makedirs(paths["out_dir"], exist_ok=True)
+seb.matplotlib.rcParams.update(res.analysis["plot"]["matplotlib"])
 
 trigger_vs_size = json_utils.tree.read(
     os.path.join(
@@ -26,58 +20,52 @@ trigger_vs_size = json_utils.tree.read(
 )
 
 trigger_modi = {
-    "passing_trigger": "trigger-probability",
-    "passing_trigger_if_only_accepting_not_rejecting": "trigger-probability\nif only accepting not rejecting",
+    "passing_trigger": "trigger probability",
+    "passing_trigger_if_only_accepting_not_rejecting": "trigger probability\nif only accepting not rejecting",
 }
 
-particle_colors = sum_config["plot"]["particle_colors"]
+particle_colors = res.analysis["plot"]["particle_colors"]
 key = "trigger_probability_vs_cherenkov_size_in_grid_bin"
 
-for sk in irf_config["config"]["sites"]:
-    for tm in trigger_modi:
-        # all particles together
-        # ----------------------
-        fig = seb.figure(irf.summary.figure.FIGURE_STYLE)
-        ax = seb.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
+for tm in trigger_modi:
+    # all particles together
+    # ----------------------
+    fig = seb.figure(irf.summary.figure.FIGURE_STYLE)
+    ax = seb.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
 
-        text_y = 0
-        for pk in irf_config["config"]["particles"]:
-            density_bin_edges = trigger_vs_size[sk][pk][tm][
-                "Cherenkov_density_bin_edges_per_m2"
-            ]
+    text_y = 0
+    for pk in res.PARTICLES:
+        density_bin_edges = trigger_vs_size[pk][tm][
+            "Cherenkov_density_bin_edges_per_m2"
+        ]
 
-            prob = trigger_vs_size[sk][pk][tm]["mean"]
-            prob_unc = trigger_vs_size[sk][pk][tm]["relative_uncertainty"]
+        prob = trigger_vs_size[pk][tm]["mean"]
+        prob_unc = trigger_vs_size[pk][tm]["relative_uncertainty"]
 
-            seb.ax_add_histogram(
-                ax=ax,
-                bin_edges=density_bin_edges,
-                bincounts=prob,
-                linestyle="-",
-                linecolor=particle_colors[pk],
-                bincounts_upper=prob * (1 + prob_unc),
-                bincounts_lower=prob * (1 - prob_unc),
-                face_color=particle_colors[pk],
-                face_alpha=0.25,
-            )
-            ax.text(
-                0.85,
-                0.1 + text_y,
-                pk,
-                color=particle_colors[pk],
-                transform=ax.transAxes,
-            )
-            text_y += 0.06
-        ax.semilogx()
-        ax.semilogy()
-        ax.set_xlim([np.min(density_bin_edges), np.max(density_bin_edges)])
-        ax.set_ylim([1e-6, 1.5e-0])
-        ax.set_xlabel("density of Cherenkov-photons at plenoscope / m$^{-2}$")
-        ax.set_ylabel("{:s} / 1".format(trigger_modi[tm]))
-        fig.savefig(
-            opj(
-                paths["out_dir"],
-                sk + "_" + tm + ".jpg",
-            )
+        seb.ax_add_histogram(
+            ax=ax,
+            bin_edges=density_bin_edges,
+            bincounts=prob,
+            linestyle="-",
+            linecolor=particle_colors[pk],
+            bincounts_upper=prob * (1 + prob_unc),
+            bincounts_lower=prob * (1 - prob_unc),
+            face_color=particle_colors[pk],
+            face_alpha=0.25,
         )
-        seb.close(fig)
+        ax.text(
+            0.85,
+            0.1 + text_y,
+            pk,
+            color=particle_colors[pk],
+            transform=ax.transAxes,
+        )
+        text_y += 0.06
+    ax.semilogx()
+    ax.semilogy()
+    ax.set_xlim([np.min(density_bin_edges), np.max(density_bin_edges)])
+    ax.set_ylim([1e-6, 1.5e-0])
+    ax.set_xlabel("density of Cherenkov photons at plenoscope / m$^{-2}$")
+    ax.set_ylabel("{:s} / 1".format(trigger_modi[tm]))
+    fig.savefig(opj(paths["out_dir"], f"{tm}.jpg"))
+    seb.close(fig)
