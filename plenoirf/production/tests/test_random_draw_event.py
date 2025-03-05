dbc29b65e48af8dc08f984741a173b1735f0b5e3
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-03-05 13:44:58 +0100

    fix DummySkyMap use

--- plenoirf/production/tests/test_random_draw_event.py
+++ plenoirf/production/tests/test_random_draw_event.py
@@ -1,6 +1,7 @@
 import magnetic_deflection
 import atmospheric_cherenkov_response
 import plenoirf
+import solid_angle_utils
 import numpy as np
 
 
@@ -18,6 +19,15 @@ def test_dummy():
 
     prng = np.random.Generator(np.random.PCG64(RUN_ID))
 
+    energy_distribution = {
+        "energy_start_GeV": 1.0, "energy_stop_GeV": 10.0, "power_slope": -1.5
+    }
+
+    scatter_solid_angle_vs_energy = {
+        "energy_GeV": [1.0, 10.0],
+        "solid_angle_sr": [solid_angle_utils.squaredeg2sr(4.0), solid_angle_utils.squaredeg2sr(16.0)]
+    }
+
     (
         events,
         debug,
@@ -27,9 +37,9 @@ def test_dummy():
         num_events=NUM_EVENTS,
         field_of_view_half_angle_rad=np.deg2rad(6.5),
         pointing_range=pointing_range,
-        energy_range={"energy_start_GeV": 1.0, "energy_stop_GeV": 10.0},
+        energy_distribution=energy_distribution,
         site_particle_magnetic_deflection_skymap=skymap_deflection,
-        skymap_query_mode="cone",
+        scatter_solid_angle_vs_energy=scatter_solid_angle_vs_energy,
     )
 
     cps = events["corsika_primary_steering"]
