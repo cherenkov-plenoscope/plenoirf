01aa29d47eadbffa4a27887aacf4af3d40e73cd7
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-03-11 17:56:39 +0100

    reduce bit precision in event table.

--- plenoirf/event_table/structure.py
+++ plenoirf/event_table/structure.py
@@ -74,71 +74,71 @@ def level_structure_to_dtype(level_structure, include_index=True):
 def init_primary_level_structure():
     t = collections.OrderedDict()
     t["particle_id"] = {
-        "dtype": "<i8",
+        "dtype": "<i4",
         "comment": "CORSIKA particle-id to identify the primary particle.",
     }
     t["energy_GeV"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "Energy of primary particle.",
     }
     t["theta_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "Direction of primary particle's momentum. "
         "CORSIKA's zenith distance angle of the primary particle's momentum.",
     }
     t["phi_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "Direction of primary particle's momentum. "
         "CORSIKA's azimuth angle w.r.t. to magnetic north of the "
         "primary particle's momentum.",
     }
     t["azimuth_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "Pointing direction of the primary particle w.r.t. "
         "magnetic north.",
     }
     t["zenith_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "Pointing direction of the primary particle.",
     }
     t["depth_g_per_cm2"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "Starting depth of CORSIKA simulation.",
     }
 
     t["momentum_x_GeV_per_c"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "Taken from CORSIKA's eventheader (EVTH).",
     }
-    t["momentum_y_GeV_per_c"] = {"dtype": "<f8", "comment": ""}
-    t["momentum_z_GeV_per_c"] = {"dtype": "<f8", "comment": ""}
+    t["momentum_y_GeV_per_c"] = {"dtype": "<f4", "comment": ""}
+    t["momentum_z_GeV_per_c"] = {"dtype": "<f4", "comment": ""}
 
     t["starting_height_asl_m"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "The simulation of the primary particle " "starts here.",
     }
     t["starting_x_m"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "See starting_height_asl_m.",
     }
     t["starting_y_m"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "See starting_height_asl_m.",
     }
 
     t["first_interaction_height_asl_m"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "See CORSIKA, this is not very "
         "meaningfull for e.g. electrons.",
     }
 
     t["solid_angle_thrown_sr"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "The size of the solid angle from which the direction "
         "of the primary particle is drawn from.",
     }
     t["inner_atmopsheric_magnetic_cutoff"] = {
-        "dtype": "<i8",
+        "dtype": "<i1",
         "comment": "A boolean flag (0, 1). If 1, the tables for magnetic "
         "deflection were not able to predict the direction of the "
         "primary particle. This is a strong indicator for there is "
@@ -146,7 +146,7 @@ def init_primary_level_structure():
         "given direction.",
     }
     t["draw_primary_direction_method"] = {
-        "dtype": "<i8",
+        "dtype": "<i1",
         "comment": ""
         "1: METHOD_SKYMAP_VALID (i.e. cutoff == False), "
         "3: METHOD_SKYMAP_CUTOFF_FALLBACK_FULL_SKY (i.e. cutoff == True), "
@@ -163,12 +163,12 @@ METHOD_VIEWCONE = 10
 def init_instrument_pointing_level_structure():
     t = collections.OrderedDict()
     t["azimuth_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "Azimuth direction of the instrument's optical axis "
         "w.r.t. magnetic north.",
     }
     t["zenith_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "Zenith direction of the instrument's optical axis "
         "w.r.t. magnetic north.",
     }
@@ -197,7 +197,7 @@ def init_cherenkovpool_level_structure():
         for percentile in [16, 50, 84]:
             tkey = "{:s}_p{:02d}_{:s}".format(key, percentile, keys[key])
             t[tkey] = {
-                "dtype": "<f8",
+                "dtype": "<f4",
                 "comment": "percentile {:d}".format(percentile),
             }
     return t
@@ -205,21 +205,21 @@ def init_cherenkovpool_level_structure():
 
 def init_groundgrid_level_structure():
     t = collections.OrderedDict()
-    t["bin_width_m"] = {"dtype": "<f8", "comment": ""}
-    t["num_bins_each_axis"] = {"dtype": "<i8", "comment": ""}
+    t["bin_width_m"] = {"dtype": "<f4", "comment": ""}
+    t["num_bins_each_axis"] = {"dtype": "<i4", "comment": ""}
 
     t["center_x_m"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "This is random_shift_x_m + cherenkov_pool_median_x_m.",
     }
-    t["center_y_m"] = {"dtype": "<f8", "comment": "See center_x_m."}
+    t["center_y_m"] = {"dtype": "<f4", "comment": "See center_x_m."}
 
     t["num_bins_thrown"] = {
-        "dtype": "<i8",
+        "dtype": "<i4",
         "comment": "The number of all grid-bins which can collect "
         "Cherenkov-photons.",
     }
-    t["area_thrown_m2"] = {"dtype": "<f8", "comment": ""}
+    t["area_thrown_m2"] = {"dtype": "<f4", "comment": ""}
 
     return t
 
@@ -233,13 +233,13 @@ def patch_groundgrid(groundgrid, groundgrid_result):
         shape=groundgrid.shape[0],
         dtype=[
             (snt.IDX, snt.IDX_DTYPE),
-            ("bin_width_m", "<f8"),
-            ("num_bins_each_axis", "<i8"),
-            ("center_x_m", "<f8"),
-            ("center_y_m", "<f8"),
-            ("num_bins_thrown", "<i8"),
-            ("area_thrown_m2", "<f8"),
-            ("num_bins_above_threshold", "<i8"),
+            ("bin_width_m", "<f4"),
+            ("num_bins_each_axis", "<i4"),
+            ("center_x_m", "<f4"),
+            ("center_y_m", "<f4"),
+            ("num_bins_thrown", "<i4"),
+            ("area_thrown_m2", "<f4"),
+            ("num_bins_above_threshold", "<i4"),
         ],
     )
 
@@ -275,13 +275,13 @@ def init_groundgrid_result_level_structure():
     t = collections.OrderedDict()
     # arguments to init GroundGrid
 
-    t["bin_idx_x"] = {"dtype": "<i8", "comment": ""}
-    t["bin_idx_y"] = {"dtype": "<i8", "comment": ""}
-    t["bin_num_photons"] = {"dtype": "<f8", "comment": ""}
-    t["core_x_m"] = {"dtype": "<f8", "comment": ""}
-    t["core_y_m"] = {"dtype": "<f8", "comment": ""}
+    t["bin_idx_x"] = {"dtype": "<i4", "comment": ""}
+    t["bin_idx_y"] = {"dtype": "<i4", "comment": ""}
+    t["bin_num_photons"] = {"dtype": "<f4", "comment": ""}
+    t["core_x_m"] = {"dtype": "<f4", "comment": ""}
+    t["core_y_m"] = {"dtype": "<f4", "comment": ""}
 
-    t["num_bins_above_threshold"] = {"dtype": "<i8", "comment": ""}
+    t["num_bins_above_threshold"] = {"dtype": "<i4", "comment": ""}
 
     # compare scatter
     num_scatter_bins = 16
@@ -325,7 +325,7 @@ def init_particlepool_level_structure():
 def init_particlepoolonaperture_level_structure():
     t = collections.OrderedDict()
     t["num_air_cherenkov_on_aperture"] = {
-        "dtype": "<i8",
+        "dtype": "<i4",
         "comment": "Same as 'num_air_cherenkov' but also run through the "
         "instrument's aperture for Cherenkov-light.",
     }
@@ -344,11 +344,11 @@ def init_instrument_level_structure():
 
 def init_trigger_level_structure(num_foci=12):
     t = collections.OrderedDict()
-    t["num_cherenkov_pe"] = {"dtype": "<i8", "comment": ""}
-    t["response_pe"] = {"dtype": "<i8", "comment": ""}
+    t["num_cherenkov_pe"] = {"dtype": "<i4", "comment": ""}
+    t["response_pe"] = {"dtype": "<i4", "comment": ""}
     for nn in range(num_foci):
         key = "focus_{:02d}_response_pe".format(nn)
-        t[key] = {"dtype": "<i8", "comment": ""}
+        t[key] = {"dtype": "<i4", "comment": ""}
     return t
 
 
@@ -358,17 +358,17 @@ def init_pasttrigger_level_structure():
 
 def init_cherenkovclassification_level_structure():
     t = collections.OrderedDict()
-    t["num_true_positives"] = {"dtype": "<i8", "comment": ""}
-    t["num_false_negatives"] = {"dtype": "<i8", "comment": ""}
-    t["num_false_positives"] = {"dtype": "<i8", "comment": ""}
-    t["num_true_negatives"] = {"dtype": "<i8", "comment": ""}
+    t["num_true_positives"] = {"dtype": "<i4", "comment": ""}
+    t["num_false_negatives"] = {"dtype": "<i4", "comment": ""}
+    t["num_false_positives"] = {"dtype": "<i4", "comment": ""}
+    t["num_true_negatives"] = {"dtype": "<i4", "comment": ""}
     return t
 
 
 def init_features_level_structure():
     t = collections.OrderedDict()
     t["num_photons"] = {
-        "dtype": "<i8",
+        "dtype": "<i4",
         "comment": "The number of photon-eqivalents that are identified to be dense cluster(s) of Cherenkov-photons",
         "transformation": {
             "function": "log(x)",
@@ -379,7 +379,7 @@ def init_features_level_structure():
         "unit": "1",
     }
     t["paxel_intensity_peakness_std_over_mean"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "A measure for the intensity distribution on the aperture-plane. The larger the value, the less evenly the intensity is distributed on the plane.",
         "transformation": {
             "function": "log(x)",
@@ -390,7 +390,7 @@ def init_features_level_structure():
         "unit": "1",
     }
     t["paxel_intensity_peakness_max_over_mean"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "A measure for the intensity distribution on the aperture-plane. The larger the value, the more the intensity is concentrated in a small area on the aperture-plane.",
         "transformation": {
             "function": "log(x)",
@@ -403,7 +403,7 @@ def init_features_level_structure():
 
     paxel_intensity_median_str = "Median intersection-positions in {:s} of reconstructed Cherenkov-photons on the aperture-plane"
     t["paxel_intensity_median_x"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": paxel_intensity_median_str.format("x"),
         "transformation": {
             "function": "x",
@@ -414,7 +414,7 @@ def init_features_level_structure():
         "unit": "m",
     }
     t["paxel_intensity_median_y"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": paxel_intensity_median_str.format("y"),
         "transformation": {
             "function": "x",
@@ -427,7 +427,7 @@ def init_features_level_structure():
 
     _watershed_str = "A measure for the areal distribution of reconstructed Cherenkov-photons on the aperture-plane."
     t["aperture_num_islands_watershed_rel_thr_2"] = {
-        "dtype": "<i8",
+        "dtype": "<i2",
         "comment": _watershed_str,
         "transformation": {
             "function": "x",
@@ -438,7 +438,7 @@ def init_features_level_structure():
         "unit": "1",
     }
     t["aperture_num_islands_watershed_rel_thr_4"] = {
-        "dtype": "<i8",
+        "dtype": "<i2",
         "comment": _watershed_str,
         "transformation": {
             "function": "x",
@@ -449,7 +449,7 @@ def init_features_level_structure():
         "unit": "1",
     }
     t["aperture_num_islands_watershed_rel_thr_8"] = {
-        "dtype": "<i8",
+        "dtype": "<i2",
         "comment": _watershed_str,
         "transformation": {
             "function": "x",
@@ -462,7 +462,7 @@ def init_features_level_structure():
 
     _light_front_c_str = "Incident-direction in {:s} of reconstructed Cherenkov-photon-plane passing through the aperture-plane."
     t["light_front_cx"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": _light_front_c_str.format("x"),
         "transformation": {
             "function": "x",
@@ -473,7 +473,7 @@ def init_features_level_structure():
         "unit": "rad",
     }
     t["light_front_cy"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": _light_front_c_str.format("y"),
         "transformation": {
             "function": "x",
@@ -486,7 +486,7 @@ def init_features_level_structure():
 
     _image_infinity_c_mean_str = "Mean incident-direction in {:s} of reconstructed Cherenkov-photons in the image focussed to infinity."
     t["image_infinity_cx_mean"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": _image_infinity_c_mean_str.format("x"),
         "transformation": {
             "function": "x",
@@ -497,7 +497,7 @@ def init_features_level_structure():
         "unit": "rad",
     }
     t["image_infinity_cy_mean"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": _image_infinity_c_mean_str.format("y"),
         "transformation": {
             "function": "x",
@@ -508,7 +508,7 @@ def init_features_level_structure():
         "unit": "rad",
     }
     t["image_infinity_cx_std"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "",
         "transformation": {
             "function": "log(x)",
@@ -519,7 +519,7 @@ def init_features_level_structure():
         "unit": "rad",
     }
     t["image_infinity_cy_std"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "",
         "transformation": {
             "function": "log(x)",
@@ -530,7 +530,7 @@ def init_features_level_structure():
         "unit": "rad",
     }
     t["image_infinity_num_photons_on_edge_field_of_view"] = {
-        "dtype": "<i8",
+        "dtype": "<i4",
         "comment": "Number of photon-eqivalents on the edge of the field-of-view in an image focused on infinity.",
         "transformation": {
             "function": "x",
@@ -541,7 +541,7 @@ def init_features_level_structure():
         "unit": "p.e.",
     }
     t["image_smallest_ellipse_object_distance"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "The object-distance in front of the aperture where the refocused image of the airshower yields the Hillas-ellipse with the smallest solid angle. See also 'image_smallest_ellipse_solid_angle'.",
         "transformation": {
             "function": "log(x)",
@@ -552,7 +552,7 @@ def init_features_level_structure():
         "unit": "m",
     }
     t["image_smallest_ellipse_solid_angle"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "The solid angle of the smallest Hillas-ellipse in all refocused images. See also 'image_smallest_ellipse_object_distance'.",
         "transformation": {
             "function": "log(x)",
@@ -563,7 +563,7 @@ def init_features_level_structure():
         "unit": "sr",
     }
     t["image_smallest_ellipse_half_depth"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "The range in object-distance for the Hillas-ellipse to double its solid angle when refocusing starts at the smallest ellipse.",
         "transformation": {
             "function": "log(x)",
@@ -576,7 +576,7 @@ def init_features_level_structure():
 
     image_half_depth_shift_c_str = "How much the mean intensity in the image shifts in {:s} when refocussing from smallest to double solid angle of ellipse."
     t["image_half_depth_shift_cx"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": image_half_depth_shift_c_str.format("cx"),
         "transformation": {
             "function": "x",
@@ -587,7 +587,7 @@ def init_features_level_structure():
         "unit": "rad",
     }
     t["image_half_depth_shift_cy"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": image_half_depth_shift_c_str.format("cy"),
         "transformation": {
             "function": "x",
@@ -598,7 +598,7 @@ def init_features_level_structure():
         "unit": "rad",
     }
     t["image_smallest_ellipse_num_photons_on_edge_field_of_view"] = {
-        "dtype": "<i8",
+        "dtype": "<i4",
         "comment": "Number of photon-eqivalents on the edge of the field-of-view in an image focused to the smallest Hillas-ellipse.",
         "transformation": {
             "function": "x",
@@ -609,7 +609,7 @@ def init_features_level_structure():
         "unit": "p.e.",
     }
     t["image_num_islands"] = {
-        "dtype": "<i8",
+        "dtype": "<i2",
         "comment": "The number of individual dense clusters of reconstructed Cherenkov-photons in the image-space.",
         "transformation": {
             "function": "x",
@@ -629,24 +629,24 @@ def init_reconstructed_trajectory_level_structure():
         "Primary particle's core-position w.r.t. principal aperture-plane."
     )
     t["x_m"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": xy_comment,
         "unit": "m",
     }
     t["y_m"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": xy_comment,
         "unit": "m",
     }
 
     cxy_comment = "Primary particle's direction w.r.t. pointing."
     t["cx_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": cxy_comment,
         "unit": "rad",
     }
     t["cy_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": cxy_comment,
         "unit": "rad",
     }
@@ -656,39 +656,39 @@ def init_reconstructed_trajectory_level_structure():
         + "pointing according to fuzzy-estimator."
     )
     t["fuzzy_cx_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": cxy_comment_fuzzy,
         "unit": "rad",
     }
     t["fuzzy_cy_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": cxy_comment_fuzzy,
         "unit": "rad",
     }
 
     t["fuzzy_main_axis_support_cx_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "",
         "unit": "rad",
     }
     t["fuzzy_main_axis_support_cy_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "",
         "unit": "rad",
     }
     t["fuzzy_main_axis_support_uncertainty_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "",
         "unit": "rad",
     }
 
     t["fuzzy_main_axis_azimuth_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "",
         "unit": "rad",
     }
     t["fuzzy_main_axis_azimuth_uncertainty_rad"] = {
-        "dtype": "<f8",
+        "dtype": "<f4",
         "comment": "",
         "unit": "rad",
     }
