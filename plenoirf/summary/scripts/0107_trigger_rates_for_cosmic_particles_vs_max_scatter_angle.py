8408a84aa6cc22767c5dfd1a3f376110fa89a200
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-03-21 14:01:07 +0100

    runs again but creates rubbish.

--- plenoirf/summary/scripts/0107_trigger_rates_for_cosmic_particles_vs_max_scatter_angle.py
+++ plenoirf/summary/scripts/0107_trigger_rates_for_cosmic_particles_vs_max_scatter_angle.py
@@ -2,24 +2,15 @@
 import sys
 import numpy as np
 import plenoirf as irf
-import propagate_uncertainties as pru
-import sparse_numeric_table as snt
-import cosmic_fluxes
 import os
+import sebastians_matplotlib_addons as sebplt
 import json_utils
+import propagate_uncertainties as pru
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
 
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["plenoirf_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
-
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
 os.makedirs(paths["out_dir"], exist_ok=True)
-COSMIC_RAYS = irf_config["config"]["particles"]
-_ = COSMIC_RAYS.pop("gamma")
-SITES = irf_config["config"]["sites"]
 
 acceptance = json_utils.tree.read(
     os.path.join(
@@ -54,59 +45,55 @@ dKdE / s^{-1} m^{-2} (GeV)^{-1}
 
 source_key = "diffuse"
 
-for sk in SITES:
-    sk_dir = os.path.join(paths["out_dir"], sk)
-    os.makedirs(sk_dir, exist_ok=True)
+# cosmic-rays
+# -----------
+for ck in res.COSMIC_RAYS:
+    ck_dir = os.path.join(paths["out_dir"], ck)
+    os.makedirs(ck_dir, exist_ok=True)
 
-    # cosmic-rays
-    # -----------
-    for ck in COSMIC_RAYS:
-        sk_ck_dir = os.path.join(sk_dir, ck)
-        os.makedirs(sk_ck_dir, exist_ok=True)
+    Q = acceptance[ck][source_key]["mean"]
+    Q_au = acceptance[ck][source_key]["absolute_uncertainty"]
 
-        Q = acceptance[sk][ck][source_key]["mean"]
-        Q_au = acceptance[sk][ck][source_key]["absolute_uncertainty"]
+    num_max_scatter_angles = Q.shape[0]
 
-        num_max_scatter_angles = Q.shape[0]
+    R = np.zeros(num_max_scatter_angles)
+    R_au = np.zeros(R.shape)
+    dRdE = np.zeros(shape=(num_max_scatter_angles, energy_bin["num_bins"]))
+    dRdE_au = np.zeros(shape=dRdE.shape)
 
-        R = np.zeros(num_max_scatter_angles)
-        R_au = np.zeros(R.shape)
-        dRdE = np.zeros(shape=(num_max_scatter_angles, energy_bin["num_bins"]))
-        dRdE_au = np.zeros(shape=dRdE.shape)
+    cosmic_dFdE = airshower_fluxes[ck]["differential_flux"]
+    cosmic_dFdE_au = airshower_fluxes[ck]["absolute_uncertainty"]
 
-        cosmic_dFdE = airshower_fluxes[sk][ck]["differential_flux"]
-        cosmic_dFdE_au = airshower_fluxes[sk][ck]["absolute_uncertainty"]
-
-        for sc in range(num_max_scatter_angles):
-            for eb in range(energy_bin["num_bins"]):
-                dRdE[sc, eb], dRdE_au[sc, eb] = pru.multiply(
-                    x=cosmic_dFdE[eb],
-                    x_au=cosmic_dFdE_au[eb],
-                    y=Q[sc, eb],
-                    y_au=Q_au[sc, eb],
-                )
-
-            R[sc], R_au[sc] = irf.utils.integrate_rate_where_known(
-                dRdE=dRdE[sc, :],
-                dRdE_au=dRdE_au[sc, :],
-                E_edges=energy_bin["edges"],
+    for sc in range(num_max_scatter_angles):
+        for eb in range(energy_bin["num_bins"]):
+            dRdE[sc, eb], dRdE_au[sc, eb] = pru.multiply(
+                x=cosmic_dFdE[eb],
+                x_au=cosmic_dFdE_au[eb],
+                y=Q[sc, eb],
+                y_au=Q_au[sc, eb],
             )
 
-        json_utils.write(
-            os.path.join(sk_ck_dir, "differential.json"),
-            {
-                "comment": "Differential rate VS max. scatter angle VS energy",
-                "unit": "s$^{-1} (GeV)$^{-1}$",
-                "dRdE": dRdE,
-                "dRdE_au": dRdE_au,
-            },
+        R[sc], R_au[sc] = irf.utils.integrate_rate_where_known(
+            dRdE=dRdE[sc, :],
+            dRdE_au=dRdE_au[sc, :],
+            E_edges=energy_bin["edges"],
         )
-        json_utils.write(
-            os.path.join(sk_ck_dir, "integral.json"),
-            {
-                "comment": "Intrgral rate VS max. scatter angle.",
-                "unit": "s$^{-1}$",
-                "R": R,
-                "R_au": R_au,
-            },
-        )
+
+    json_utils.write(
+        os.path.join(ck_dir, "differential.json"),
+        {
+            "comment": "Differential rate VS max. scatter angle VS energy",
+            "unit": "s$^{-1} (GeV)$^{-1}$",
+            "dRdE": dRdE,
+            "dRdE_au": dRdE_au,
+        },
+    )
+    json_utils.write(
+        os.path.join(ck_dir, "integral.json"),
+        {
+            "comment": "Intrgral rate VS max. scatter angle.",
+            "unit": "s$^{-1}$",
+            "R": R,
+            "R_au": R_au,
+        },
+    )
--- plenoirf/summary/scripts/0108_trigger_rates_for_cosmic_particles_vs_max_scatter_angle_plot.py
+++ plenoirf/summary/scripts/0108_trigger_rates_for_cosmic_particles_vs_max_scatter_angle_plot.py
@@ -3,26 +3,16 @@ import sys
 import numpy as np
 import plenoirf as irf
 import os
-import copy
-import propagate_uncertainties as pu
 import sebastians_matplotlib_addons as sebplt
 import json_utils
+import propagate_uncertainties as pru
+import copy
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
 
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["plenoirf_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
-sebplt.matplotlib.rcParams.update(sum_config["plot"]["matplotlib"])
-
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
 os.makedirs(paths["out_dir"], exist_ok=True)
-
-PARTICLES = irf_config["config"]["particles"]
-COSMIC_RAYS = irf.utils.filter_particles_with_electric_charge(PARTICLES)
-SITES = irf_config["config"]["sites"]
-PLT = sum_config["plot"]
+sebplt.matplotlib.rcParams.update(res.analysis["plot"]["matplotlib"])
 
 energy_bin = json_utils.read(
     os.path.join(paths["analysis_dir"], "0005_common_binning", "energy.json")
@@ -40,197 +30,185 @@ rates = json_utils.tree.read(
 )
 
 MAX_SCATTER_SOLID_ANGLE_SR = 0.0
-for pk in COSMIC_RAYS:
+for pk in res.COSMIC_RAYS:
     MAX_SCATTER_SOLID_ANGLE_SR = np.max(
         [MAX_SCATTER_SOLID_ANGLE_SR, scatter_bin[pk]["stop"]]
     )
 
-for sk in SITES:
-    fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
-    ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
-    for pk in COSMIC_RAYS:
-        R = rates[sk][pk]["integral"]["R"]
-        R_au = rates[sk][pk]["integral"]["R_au"]
+fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
+ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
+for pk in res.COSMIC_RAYS:
+    R = rates[pk]["integral"]["R"]
+    R_au = rates[pk]["integral"]["R_au"]
 
-        sebplt.ax_add_histogram(
-            ax=ax,
-            bin_edges=1e3 * scatter_bin[pk]["edges"],
-            bincounts=1e-3 * (R),
-            bincounts_lower=1e-3 * (R - R_au),
-            bincounts_upper=1e-3 * (R + R_au),
-            linecolor=PLT["particle_colors"][pk],
-            face_color=PLT["particle_colors"][pk],
-            face_alpha=0.2,
+    sebplt.ax_add_histogram(
+        ax=ax,
+        bin_edges=1e3 * scatter_bin[pk]["edges"],
+        bincounts=1e-3 * (R),
+        bincounts_lower=1e-3 * (R - R_au),
+        bincounts_upper=1e-3 * (R + R_au),
+        linecolor=res.PARTICLE_COLORS[pk],
+        face_color=res.PARTICLE_COLORS[pk],
+        face_alpha=0.2,
+    )
+fig.text(
+    x=0.8,
+    y=0.05,
+    s=r"1msr = 3.3(1$^\circ)^2$",
+    color="grey",
+)
+ax.set_ylabel("trigger rate / 1k s$^{-1}$")
+ax.set_xlabel("scatter solid angle / msr")
+ax.set_xlim(1e3 * scatter_bin[pk]["limits"])
+ax.set_ylim([0, 1e2])
+fig.savefig(os.path.join(paths["out_dir"], "trigger-rate_vs_scatter.jpg"))
+sebplt.close(fig)
+
+
+fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
+ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
+for pk in res.COSMIC_RAYS:
+    R = rates[pk]["integral"]["R"]
+    R_au = rates[pk]["integral"]["R_au"]
+
+    dRdS = np.zeros(scatter_bin[pk]["num_bins"] - 1)
+    dRdS_au = np.zeros(dRdS.shape)
+    for sc in range(scatter_bin[pk]["num_bins"] - 1):
+        dR, dR_au = pru.add(
+            x=R[sc + 1],
+            x_au=R_au[sc + 1],
+            y=-R[sc],
+            y_au=R_au[sc],
         )
-    fig.text(
-        x=0.8,
-        y=0.05,
-        s=r"1msr = 3.3(1$^\circ)^2$",
-        color="grey",
-    )
-    ax.set_ylabel("trigger-rate / 1k s$^{-1}$")
-    ax.set_xlabel("scatter solid angle / msr")
-    ax.set_xlim(1e3 * scatter_bin[pk]["limits"])
-    ax.set_ylim([0, 1e2])
-    fig.savefig(
-        os.path.join(paths["out_dir"], sk + "_trigger-rate_vs_scatter.jpg")
-    )
-    sebplt.close(fig)
-
-
-for sk in SITES:
-    fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
-    ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
-    for pk in COSMIC_RAYS:
-        R = rates[sk][pk]["integral"]["R"]
-        R_au = rates[sk][pk]["integral"]["R_au"]
-
-        dRdS = np.zeros(scatter_bin[pk]["num_bins"] - 1)
-        dRdS_au = np.zeros(dRdS.shape)
-        for sc in range(scatter_bin[pk]["num_bins"] - 1):
-            dR, dR_au = pu.add(
-                x=R[sc + 1],
-                x_au=R_au[sc + 1],
-                y=-R[sc],
-                y_au=R_au[sc],
-            )
-            _Rmean, _Rmean_au = pu.add(
-                x=R[sc + 1],
-                x_au=R_au[sc + 1],
-                y=R[sc],
-                y_au=R_au[sc],
-            )
-            Rmean, Rmean_au = pu.multiply(
-                x=0.5,
-                x_au=0.0,
-                y=_Rmean,
-                y_au=_Rmean_au,
-            )
-            dS = 1e3 * scatter_bin[pk]["widths"][sc]
-            _dRdS, _dRdS_au = pu.divide(x=dR, x_au=dR_au, y=dS, y_au=0.0)
-            dRdS[sc], dRdS_au[sc] = pu.divide(
-                x=_dRdS,
-                x_au=_dRdS_au,
-                y=Rmean,
-                y_au=Rmean_au,
-            )
-
-        sebplt.ax_add_histogram(
-            ax=ax,
-            bin_edges=1e3 * scatter_bin[pk]["edges"][0:-1],
-            bincounts=dRdS,
-            bincounts_lower=(dRdS - dRdS_au),
-            bincounts_upper=(dRdS + dRdS_au),
-            linecolor=PLT["particle_colors"][pk],
-            face_color=PLT["particle_colors"][pk],
-            face_alpha=0.2,
+        _Rmean, _Rmean_au = pru.add(
+            x=R[sc + 1],
+            x_au=R_au[sc + 1],
+            y=R[sc],
+            y_au=R_au[sc],
+        )
+        Rmean, Rmean_au = pru.multiply(
+            x=0.5,
+            x_au=0.0,
+            y=_Rmean,
+            y_au=_Rmean_au,
+        )
+        dS = 1e3 * scatter_bin[pk]["widths"][sc]
+        _dRdS, _dRdS_au = pru.divide(x=dR, x_au=dR_au, y=dS, y_au=0.0)
+        dRdS[sc], dRdS_au[sc] = pru.divide(
+            x=_dRdS,
+            x_au=_dRdS_au,
+            y=Rmean,
+            y_au=Rmean_au,
         )
 
-    fig.text(
-        x=0.8,
-        y=0.05,
-        s=r"1msr = 3.3(1$^\circ)^2$",
-        color="grey",
+    sebplt.ax_add_histogram(
+        ax=ax,
+        bin_edges=1e3 * scatter_bin[pk]["edges"][0:-1],
+        bincounts=dRdS,
+        bincounts_lower=(dRdS - dRdS_au),
+        bincounts_upper=(dRdS + dRdS_au),
+        linecolor=res.PARTICLE_COLORS[pk],
+        face_color=res.PARTICLE_COLORS[pk],
+        face_alpha=0.2,
     )
-    ax.set_ylabel(
-        (
-            "R: trigger-rate / s$^{-1}$\n"
-            "S: scatter solid angle / msr\n"
-            "dR/dS R$^{-1}$ / (msr)$^{-1}$"
-        )
+
+fig.text(
+    x=0.8,
+    y=0.05,
+    s=r"1msr = 3.3(1$^\circ)^2$",
+    color="grey",
+)
+ax.set_ylabel(
+    (
+        "R: trigger-rate / s$^{-1}$\n"
+        "S: scatter solid angle / msr\n"
+        "dR/dS R$^{-1}$ / (msr)$^{-1}$"
     )
-    ax.set_xlabel("scatter solid angle / msr")
-    ax.set_xlim(1e3 * scatter_bin[pk]["limits"])
-    ax.set_ylim([1e-4, 1e0])
-    ax.semilogy()
-    fig.savefig(
-        os.path.join(
-            paths["out_dir"], sk + "_diff-trigger-rate_vs_scatter.jpg"
-        )
-    )
-    sebplt.close(fig)
+)
+ax.set_xlabel("scatter solid angle / msr")
+ax.set_xlim(1e3 * scatter_bin[pk]["limits"])
+ax.set_ylim([1e-4, 1e0])
+ax.semilogy()
+fig.savefig(os.path.join(paths["out_dir"], "diff-trigger-rate_vs_scatter.jpg"))
+sebplt.close(fig)
 
 
 AXSPAN = copy.deepcopy(irf.summary.figure.AX_SPAN)
 AXSPAN = [AXSPAN[0], AXSPAN[1], AXSPAN[2], AXSPAN[3]]
 
-for sk in SITES:
-    for pk in COSMIC_RAYS:
-        print("plot 2D", sk, pk)
+for pk in res.COSMIC_RAYS:
+    print("plot 2D", pk)
 
-        dRdE = rates[sk][pk]["differential"]["dRdE"]
-        dRdE_au = rates[sk][pk]["differential"]["dRdE_au"]
+    dRdE = rates[pk]["differential"]["dRdE"]
+    dRdE_au = rates[pk]["differential"]["dRdE_au"]
 
-        # integrate along energy to get rate
-        # ----------------------------------
-        R = np.zeros(shape=dRdE.shape)
-        for sc in range(scatter_bin[pk]["num_bins"]):
-            R[sc, :] = dRdE[sc, :] * energy_bin["widths"]
+    # integrate along energy to get rate
+    # ----------------------------------
+    R = np.zeros(shape=dRdE.shape)
+    for sc in range(scatter_bin[pk]["num_bins"]):
+        R[sc, :] = dRdE[sc, :] * energy_bin["widths"]
 
-        # differentiate w.r.t. scatter
-        # ----------------------------
-        dRdS = np.zeros(shape=(R.shape[0] - 1, R.shape[1]))
-        for eb in range(energy_bin["num_bins"]):
-            for sc in range(scatter_bin[pk]["num_bins"] - 1):
-                dR = R[sc + 1, eb] - R[sc, eb]
-                Rmean = 0.5 * (R[sc + 1, eb] + R[sc, eb])
-                dS = 1e3 * scatter_bin[pk]["widths"][sc]
+    # differentiate w.r.t. scatter
+    # ----------------------------
+    dRdS = np.zeros(shape=(R.shape[0] - 1, R.shape[1]))
+    for eb in range(energy_bin["num_bins"]):
+        for sc in range(scatter_bin[pk]["num_bins"] - 1):
+            dR = R[sc + 1, eb] - R[sc, eb]
+            Rmean = 0.5 * (R[sc + 1, eb] + R[sc, eb])
+            dS = 1e3 * scatter_bin[pk]["widths"][sc]
 
-                with np.errstate(divide="ignore", invalid="ignore"):
-                    dRdS[sc, eb] = (dR / dS) / Rmean
+            with np.errstate(divide="ignore", invalid="ignore"):
+                dRdS[sc, eb] = (dR / dS) / Rmean
 
-        dRdS[np.isnan(dRdS)] = 0.0
+    dRdS[np.isnan(dRdS)] = 0.0
 
-        fig = sebplt.figure(style=irf.summary.figure.FIGURE_STYLE)
-        ax = sebplt.add_axes(fig=fig, span=[AXSPAN[0], AXSPAN[1], 0.55, 0.7])
+    fig = sebplt.figure(style=irf.summary.figure.FIGURE_STYLE)
+    ax = sebplt.add_axes(fig=fig, span=[AXSPAN[0], AXSPAN[1], 0.55, 0.7])
 
-        ax_cb = sebplt.add_axes(
-            fig=fig,
-            span=[0.8, AXSPAN[1], 0.02, 0.7],
-            # style=sebplt.AXES_BLANK,
-        )
+    ax_cb = sebplt.add_axes(
+        fig=fig,
+        span=[0.8, AXSPAN[1], 0.02, 0.7],
+        # style=sebplt.AXES_BLANK,
+    )
 
-        ax.set_xlim(energy_bin["limits"])
-        ax.set_ylim([0, 1e3 * MAX_SCATTER_SOLID_ANGLE_SR])
-        ax.semilogx()
+    ax.set_xlim(energy_bin["limits"])
+    ax.set_ylim([0, 1e3 * MAX_SCATTER_SOLID_ANGLE_SR])
+    ax.semilogx()
 
-        ax.set_xlabel("energy / GeV")
-        ax.set_ylabel("scatter solid angle / msr")
+    ax.set_xlabel("energy / GeV")
+    ax.set_ylabel("scatter solid angle / msr")
 
-        fig.text(
-            x=0.8,
-            y=0.05,
-            s=r"1msr = 3.3(1$^\circ)^2$",
-            color="grey",
-        )
-        pcm_ratio = ax.pcolormesh(
-            energy_bin["edges"],
-            1e3 * scatter_bin[pk]["edges"][0:-1],
-            dRdS,
-            norm=sebplt.plt_colors.LogNorm(),
-            cmap="terrain_r",
-            vmin=1e-4,
-            vmax=1e0,
-        )
+    fig.text(
+        x=0.8,
+        y=0.05,
+        s=r"1msr = 3.3(1$^\circ)^2$",
+        color="grey",
+    )
+    pcm_ratio = ax.pcolormesh(
+        energy_bin["edges"],
+        1e3 * scatter_bin[pk]["edges"][0:-1],
+        dRdS,
+        norm=sebplt.plt_colors.LogNorm(vmin=1e-4, vmax=1e0),
+        cmap="terrain_r",
+    )
 
-        sebplt.plt.colorbar(
-            pcm_ratio,
-            cax=ax_cb,
-            label=(
-                "R: trigger-rate / s$^{-1}$\n"
-                "S: scatter solid angle / msr\n"
-                "dR/dS R$^{-1}$ / (msr)$^{-1}$"
+    sebplt.plt.colorbar(
+        pcm_ratio,
+        cax=ax_cb,
+        label=(
+            "R: trigger-rate / s$^{-1}$\n"
+            "S: scatter solid angle / msr\n"
+            "dR/dS R$^{-1}$ / (msr)$^{-1}$"
+        ),
+    )
+    sebplt.ax_add_grid(ax=ax)
+
+    fig.savefig(
+        os.path.join(
+            paths["out_dir"],
+            "{:s}_diff-trigger-rate_vs_scatter_vs_energy.jpg".format(
+                pk,
             ),
         )
-        sebplt.ax_add_grid(ax=ax)
-
-        fig.savefig(
-            os.path.join(
-                paths["out_dir"],
-                "{:s}_{:s}_diff-trigger-rate_vs_scatter_vs_energy.jpg".format(
-                    sk,
-                    pk,
-                ),
-            )
-        )
-        sebplt.close(fig)
+    )
+    sebplt.close(fig)
