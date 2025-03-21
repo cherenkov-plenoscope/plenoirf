4ec1ae8bb5365fc44f83c824bc9696c636040ce7
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-03-21 16:28:12 +0100

    prepare pointing dependency part 2

--- plenoirf/summary/scripts/0210_trajectory_estimating_quality.py
+++ plenoirf/summary/scripts/0210_trajectory_estimating_quality.py
@@ -2,22 +2,17 @@
 import sys
 import numpy as np
 import plenoirf as irf
-import confusion_matrix
-import sparse_numeric_table as snt
 import os
 import sebastians_matplotlib_addons as sebplt
 import json_utils
+import sparse_numeric_table as snt
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
 
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["plenoirf_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
-sebplt.matplotlib.rcParams.update(sum_config["plot"]["matplotlib"])
-
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
 os.makedirs(paths["out_dir"], exist_ok=True)
+sebplt.matplotlib.rcParams.update(res.analysis["plot"]["matplotlib"])
+
 
 passing_trigger = json_utils.tree.read(
     os.path.join(paths["analysis_dir"], "0055_passing_trigger")
@@ -34,12 +29,9 @@ weights_thrown2expected = json_utils.tree.read(
         "0040_weights_from_thrown_to_expected_energy_spectrum",
     )
 )
-min_trajectory_quality = sum_config["quality"]["min_trajectory_quality"]
+min_trajectory_quality = res.analysis["quality"]["min_trajectory_quality"]
 
 theta_bin_edges_deg = np.linspace(0.0, 3.0, 15)
-
-PARTICLES = irf_config["config"]["particles"]
-SITES = irf_config["config"]["sites"]
 
 # feature correlation
 # ===================
@@ -158,10 +150,10 @@ def write_correlation_figure(
     sebplt.close(fig)
 
 
-def align_values_with_event_frame(event_frame, idxs, values):
+def align_values_with_event_frame(event_frame, uids, values):
     Q = {}
-    for ii in range(len(idxs)):
-        Q[idxs[ii]] = values[ii]
+    for ii in range(len(uids)):
+        Q[uids[ii]] = values[ii]
 
     aligned_values = np.nan * np.ones(event_frame.shape[0])
     for ii in range(event_frame.shape[0]):
@@ -175,150 +167,144 @@ QP = {}
 QP["quality_cuts"] = np.linspace(0.0, 1.0, 137)
 QP["fraction_passing"] = {}
 QP["fraction_passing_w"] = {}
-for sk in SITES:
-    QP["fraction_passing"][sk] = {}
-    QP["fraction_passing_w"][sk] = {}
 
-
-for sk in SITES:
-    for pk in PARTICLES:
-        event_table = snt.read(
-            path=os.path.join(
-                paths["plenoirf_dir"], "event_table", sk, pk, "event_table.tar"
-            ),
-            structure=irf.table.STRUCTURE,
-        )
-        idx_common = snt.intersection(
-            [
-                passing_trigger[sk][pk]["uid"],
-                passing_quality[sk][pk]["uid"],
-                passing_trajectory[sk][pk]["trajectory_quality"]["uid"],
-            ]
-        )
-        event_table = snt.cut_and_sort_table_on_indices(
-            table=event_table,
-            common_indices=idx_common,
-            level_keys=[
-                "primary",
-                "core",
-                "reconstructed_trajectory",
-                "features",
-            ],
+for pk in res.PARTICLES:
+    with res.open_event_table(particle_key=pk) as arc:
+        event_table = arc.query(
+            levels_and_columns={
+                "primary": "__all__",
+                "instrument_pointing": "__all__",
+                "groundgrid_choice": "__all__",
+                "reconstructed_trajectory": "__all__",
+                "features": "__all__",
+            }
         )
 
-        event_frame = (
-            irf.reconstruction.trajectory_quality.make_rectangular_table(
-                event_table=event_table,
-                plenoscope_pointing=irf_config["config"][
-                    "plenoscope_pointing"
-                ],
-            )
-        )
+    uid_common = snt.logic.intersection(
+        [
+            passing_trigger[pk]["uid"],
+            passing_quality[pk]["uid"],
+            passing_trajectory[pk]["trajectory_quality"]["uid"],
+        ]
+    )
+    event_table = snt.logic.cut_and_sort_table_on_indices(
+        table=event_table,
+        common_indices=uid_common,
+        level_keys=[
+            "primary",
+            "instrument_pointing",
+            "groundgrid_choice",
+            "reconstructed_trajectory",
+            "features",
+        ],
+    )
 
-        quality = align_values_with_event_frame(
-            event_frame=event_frame,
-            idxs=passing_trajectory[sk][pk]["trajectory_quality"]["uid"],
-            values=passing_trajectory[sk][pk]["trajectory_quality"]["quality"],
-        )
+    event_frame = irf.reconstruction.trajectory_quality.make_rectangular_table(
+        event_table=event_table,
+        pointing_model=res.config["pointing"]["model"],
+    )
 
+    quality = align_values_with_event_frame(
+        event_frame=event_frame,
+        uids=passing_trajectory[pk]["trajectory_quality"]["uid"],
+        values=passing_trajectory[pk]["trajectory_quality"]["quality"],
+    )
+
+    write_correlation_figure(
+        path=os.path.join(
+            paths["out_dir"],
+            "{:s}_{:s}_vs_quality.jpg".format(pk, the),
+        ),
+        x=quality,
+        y=np.rad2deg(event_frame["trajectory/" + the + "_rad"]),
+        x_bin_edges=np.linspace(0, 1, 15),
+        y_bin_edges=theta_bin_edges_deg,
+        x_label="quality / 1",
+        y_label=the + r" / $1^{\circ}$",
+        min_exposure_x=100,
+        logx=False,
+        logy=False,
+        log_exposure_counter=False,
+        x_cut=min_trajectory_quality,
+    )
+
+    if pk == "gamma":
         write_correlation_figure(
             path=os.path.join(
                 paths["out_dir"],
-                "{:s}_{:s}_{:s}_vs_quality.jpg".format(sk, pk, the),
+                "{:s}_energy_vs_quality.jpg".format(pk, the),
             ),
             x=quality,
-            y=np.rad2deg(event_frame["trajectory/" + the + "_rad"]),
+            y=event_frame["primary/energy_GeV"],
             x_bin_edges=np.linspace(0, 1, 15),
-            y_bin_edges=theta_bin_edges_deg,
+            y_bin_edges=np.geomspace(1, 1000, 15),
             x_label="quality / 1",
-            y_label=the + r" / $1^{\circ}$",
+            y_label="energy / GeV",
             min_exposure_x=100,
             logx=False,
-            logy=False,
+            logy=True,
             log_exposure_counter=False,
             x_cut=min_trajectory_quality,
         )
 
-        if pk == "gamma":
+    if pk == "gamma":
+        for fk in feature_correlations:
             write_correlation_figure(
                 path=os.path.join(
                     paths["out_dir"],
-                    "{:s}_{:s}_energy_vs_quality.jpg".format(sk, pk, the),
+                    "{:s}_{:s}_vs_{:s}.jpg".format(
+                        pk, the, str.replace(fk["key"], "/", "-")
+                    ),
                 ),
-                x=quality,
-                y=event_frame["primary/energy_GeV"],
-                x_bin_edges=np.linspace(0, 1, 15),
-                y_bin_edges=np.geomspace(1, 1000, 15),
-                x_label="quality / 1",
-                y_label="energy / GeV",
+                x=event_frame[fk["key"]],
+                y=np.rad2deg(event_frame["trajectory/" + the + "_rad"]),
+                x_bin_edges=fk["bin_edges"],
+                y_bin_edges=theta_bin_edges_deg,
+                x_label=fk["label"],
+                y_label=the + r" / $1^{\circ}$",
                 min_exposure_x=100,
-                logx=False,
-                logy=True,
+                logx=fk["log"],
+                logy=False,
                 log_exposure_counter=False,
-                x_cut=min_trajectory_quality,
             )
 
-        if pk == "gamma":
-            for fk in feature_correlations:
-                write_correlation_figure(
-                    path=os.path.join(
-                        paths["out_dir"],
-                        "{:s}_{:s}_{:s}_vs_{:s}.jpg".format(
-                            sk, pk, the, str.replace(fk["key"], "/", "-")
-                        ),
-                    ),
-                    x=event_frame[fk["key"]],
-                    y=np.rad2deg(event_frame["trajectory/" + the + "_rad"]),
-                    x_bin_edges=fk["bin_edges"],
-                    y_bin_edges=theta_bin_edges_deg,
-                    x_label=fk["label"],
-                    y_label=the + r" / $1^{\circ}$",
-                    min_exposure_x=100,
-                    logx=fk["log"],
-                    logy=False,
-                    log_exposure_counter=False,
-                )
+    # plot losses
+    # ===========
 
-        # plot losses
-        # ===========
+    reweight_spectrum = np.interp(
+        x=event_frame["primary/energy_GeV"],
+        xp=weights_thrown2expected[pk]["weights_vs_energy"]["energy_GeV"],
+        fp=weights_thrown2expected[pk]["weights_vs_energy"]["mean"],
+    )
 
-        reweight_spectrum = np.interp(
-            x=event_frame["primary/energy_GeV"],
-            xp=weights_thrown2expected[sk][pk]["weights_vs_energy"][
-                "energy_GeV"
-            ],
-            fp=weights_thrown2expected[sk][pk]["weights_vs_energy"]["mean"],
-        )
+    fraction_passing = []
+    fraction_passing_w = []
+    for quality_cut in QP["quality_cuts"]:
+        mask = quality >= quality_cut
+        num_passing_cut = np.sum(mask)
+        num_total = quality.shape[0]
+        fraction_passing.append(num_passing_cut / num_total)
 
-        fraction_passing = []
-        fraction_passing_w = []
-        for quality_cut in QP["quality_cuts"]:
-            mask = quality >= quality_cut
-            num_passing_cut = np.sum(mask)
-            num_total = quality.shape[0]
-            fraction_passing.append(num_passing_cut / num_total)
+        num_passing_cut_w = np.sum(reweight_spectrum[mask])
+        num_total_w = np.sum(reweight_spectrum)
+        fraction_passing_w.append(num_passing_cut_w / num_total_w)
 
-            num_passing_cut_w = np.sum(reweight_spectrum[mask])
-            num_total_w = np.sum(reweight_spectrum)
-            fraction_passing_w.append(num_passing_cut_w / num_total_w)
-
-        QP["fraction_passing"][sk][pk] = np.array(fraction_passing)
-        QP["fraction_passing_w"][sk][pk] = np.array(fraction_passing_w)
+    QP["fraction_passing"][pk] = np.array(fraction_passing)
+    QP["fraction_passing_w"][pk] = np.array(fraction_passing_w)
 
 
-for sk in SITES:
-    fig = sebplt.figure(sebplt.FIGURE_1_1)
-    ax = sebplt.add_axes(fig=fig, span=[0.16, 0.11, 0.8, 0.8])
-    for pk in PARTICLES:
-        ax.plot(
-            QP["quality_cuts"],
-            QP["fraction_passing_w"][sk][pk],
-            color=sum_config["plot"]["particle_colors"][pk],
-        )
-    ax.plot([min_trajectory_quality, min_trajectory_quality], [0.0, 1.0], "k:")
-    ax.set_xlim([0, 1])
-    ax.set_ylim([0, 1])
-    ax.set_xlabel("trajectory-quality-cut / 1")
-    ax.set_ylabel("passing cut / 1")
-    fig.savefig(os.path.join(paths["out_dir"], "{:s}_passing.jpg".format(sk)))
-    sebplt.close(fig)
+fig = sebplt.figure(sebplt.FIGURE_1_1)
+ax = sebplt.add_axes(fig=fig, span=[0.16, 0.11, 0.8, 0.8])
+for pk in PARTICLES:
+    ax.plot(
+        QP["quality_cuts"],
+        QP["fraction_passing_w"][pk],
+        color=sum_config["plot"]["particle_colors"][pk],
+    )
+ax.plot([min_trajectory_quality, min_trajectory_quality], [0.0, 1.0], "k:")
+ax.set_xlim([0, 1])
+ax.set_ylim([0, 1])
+ax.set_xlabel("trajectory-quality-cut / 1")
+ax.set_ylabel("passing cut / 1")
+fig.savefig(os.path.join(paths["out_dir"], "passing.jpg"))
+sebplt.close(fig)
