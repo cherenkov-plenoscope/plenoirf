e9a823f1a5252e24bb158dbb6bd73a61be4385f7
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-03-21 14:14:17 +0100

    get running again. but result is rubbish.

--- plenoirf/summary/scripts/0120_trigger_rates_for_night_sky_background.py
+++ plenoirf/summary/scripts/0120_trigger_rates_for_night_sky_background.py
@@ -6,100 +6,75 @@ import sparse_numeric_table as snt
 import os
 import json_utils
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
-
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["plenoirf_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
-
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
 os.makedirs(paths["out_dir"], exist_ok=True)
 
-PARTICLES = irf_config["config"]["particles"]
-SITES = irf_config["config"]["sites"]
-
-MAX_CHERENKOV_IN_NSB_PE = sum_config["night_sky_background"][
+MAX_CHERENKOV_IN_NSB_PE = res.analysis["night_sky_background"][
     "max_num_true_cherenkov_photons"
 ]
 TIME_SLICE_DURATION = 0.5e-9
 NUM_TIME_SLICES_IN_LIGHTFIELDSEQUENCE = 100
 NUM_TIME_SLICES_PER_EVENT = (
     NUM_TIME_SLICES_IN_LIGHTFIELDSEQUENCE
-    - irf_config["config"]["sum_trigger"]["integration_time_slices"]
+    - res.config["sum_trigger"]["integration_time_slices"]
 )
 EXPOSURE_TIME_PER_EVENT = NUM_TIME_SLICES_PER_EVENT * TIME_SLICE_DURATION
-TRIGGER = sum_config["trigger"]
+TRIGGER = res.analysis["trigger"][res.site_key]
 
-nsb = {}
-for sk in SITES:
-    trigger_thresholds = TRIGGER[sk]["ratescan_thresholds_pe"]
-    num_trigger_thresholds = len(trigger_thresholds)
-    trigger_modus = TRIGGER[sk]["modus"]
+trigger_thresholds = TRIGGER["ratescan_thresholds_pe"]
+num_trigger_thresholds = len(trigger_thresholds)
+trigger_modus = TRIGGER["modus"]
+nsb = {
+    "num_exposures": 0,
+    "num_triggers_vs_threshold": np.zeros(num_trigger_thresholds, dtype=int),
+}
+for pk in res.PARTICLES:
+    with res.open_event_table(particle_key=pk) as arc:
+        airshower_table = arc.query(levels_and_columns={"trigger": "__all__"})
 
-    nsb[sk] = {
-        "num_exposures": 0,
-        "num_triggers_vs_threshold": np.zeros(
-            num_trigger_thresholds, dtype=np.int
+    # The true num of Cherenkov-photons in the light-field-sequence must be
+    # below a critical threshold.
+    uid_nsb = airshower_table["trigger"]["uid"][
+        airshower_table["trigger"]["num_cherenkov_pe"]
+        <= MAX_CHERENKOV_IN_NSB_PE
+    ]
+    nsb_table = snt.logic.cut_level_on_indices(
+        level=airshower_table["trigger"],
+        indices=uid_nsb,
+        index_key="uid",
+    )
+
+    for tt, threshold in enumerate(trigger_thresholds):
+        uid_trigger = irf.analysis.light_field_trigger_modi.make_indices(
+            trigger_table=nsb_table,
+            threshold=threshold,
+            modus=trigger_modus,
+        )
+        nsb["num_exposures"] += len(uid_nsb)
+        nsb["num_triggers_vs_threshold"][tt] += len(uid_trigger)
+
+
+num_exposures = nsb["num_exposures"]
+num_triggers_vs_threshold = nsb["num_triggers_vs_threshold"]
+
+mean = num_triggers_vs_threshold / (num_exposures * EXPOSURE_TIME_PER_EVENT)
+relative_uncertainty = irf.utils._divide_silent(
+    numerator=np.sqrt(num_triggers_vs_threshold),
+    denominator=num_triggers_vs_threshold,
+    default=np.nan,
+)
+
+json_utils.write(
+    os.path.join(paths["out_dir"], "night_sky_background_rates.json"),
+    {
+        "comment": (
+            "Trigger rate for night-sky-background"
+            "VS trigger-ratescan-thresholds"
         ),
-    }
-    for pk in PARTICLES:
-        airshower_table = snt.read(
-            path=os.path.join(
-                paths["plenoirf_dir"],
-                "event_table",
-                sk,
-                pk,
-                "event_table.tar",
-            ),
-            structure=irf.table.STRUCTURE,
-        )
-
-        # The true num of Cherenkov-photons in the light-field-sequence must be
-        # below a critical threshold.
-        idx_nsb = airshower_table["trigger"]["uid"][
-            airshower_table["trigger"]["num_cherenkov_pe"]
-            <= MAX_CHERENKOV_IN_NSB_PE
-        ]
-        nsb_table = snt.cut_level_on_indices(
-            level=airshower_table["trigger"],
-            indices=idx_nsb,
-        )
-
-        for tt, threshold in enumerate(trigger_thresholds):
-            idx_trigger = irf.analysis.light_field_trigger_modi.make_indices(
-                trigger_table=nsb_table,
-                threshold=threshold,
-                modus=trigger_modus,
-            )
-            nsb[sk]["num_exposures"] += len(idx_nsb)
-            nsb[sk]["num_triggers_vs_threshold"][tt] += len(idx_trigger)
-
-for sk in SITES:
-    num_exposures = nsb[sk]["num_exposures"]
-    num_triggers_vs_threshold = nsb[sk]["num_triggers_vs_threshold"]
-
-    mean = num_triggers_vs_threshold / (
-        num_exposures * EXPOSURE_TIME_PER_EVENT
-    )
-    relative_uncertainty = irf.utils._divide_silent(
-        numerator=np.sqrt(num_triggers_vs_threshold),
-        denominator=num_triggers_vs_threshold,
-        default=np.nan,
-    )
-
-    site_dir = os.path.join(paths["out_dir"], sk)
-    os.makedirs(site_dir, exist_ok=True)
-    json_utils.write(
-        os.path.join(site_dir, "night_sky_background_rates.json"),
-        {
-            "comment": (
-                "Trigger rate for night-sky-background"
-                "VS trigger-ratescan-thresholds"
-            ),
-            "trigger": sum_config["trigger"],
-            "unit": "s$^{-1}$",
-            "mean": mean,
-            "relative_uncertainty": relative_uncertainty,
-        },
-    )
+        "trigger": TRIGGER,
+        "unit": "s$^{-1}$",
+        "mean": mean,
+        "relative_uncertainty": relative_uncertainty,
+    },
+)
--- plenoirf/summary/scripts/0130_trigger_ratescan_plot.py
+++ plenoirf/summary/scripts/0130_trigger_ratescan_plot.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python
 import sys
-import numpy as np
 import plenoirf as irf
+import sparse_numeric_table as snt
 import os
+import numpy as np
 import sebastians_matplotlib_addons as sebplt
 import json_utils
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
 
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["plenoirf_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
-sebplt.matplotlib.rcParams.update(sum_config["plot"]["matplotlib"])
-
+paths = irf.summary.paths_from_argv(sys.argv)
+res = irf.summary.Resources.from_argv(sys.argv)
 os.makedirs(paths["out_dir"], exist_ok=True)
+sebplt.matplotlib.rcParams.update(res.analysis["plot"]["matplotlib"])
 
-SITES = irf_config["config"]["sites"]
-PARTICLES = irf_config["config"]["particles"]
-TRIGGER = sum_config["trigger"]
+TRIGGER = res.analysis["trigger"][res.site_key]
+
 cosmic_rates = json_utils.tree.read(
     os.path.join(
         paths["analysis_dir"], "0105_trigger_rates_for_cosmic_particles"
@@ -31,58 +26,44 @@ nsb_rates = json_utils.tree.read(
     )
 )
 
-particle_colors = sum_config["plot"]["particle_colors"]
-
 trigger_rates = {}
-for sk in SITES:
-    trigger_rates[sk] = {}
-    trigger_rates[sk]["night_sky_background"] = np.array(
-        nsb_rates[sk]["night_sky_background_rates"]["mean"]
-    )
-    for pk in PARTICLES:
-        trigger_rates[sk][pk] = np.array(
-            cosmic_rates[sk][pk]["integral_rate"]["mean"]
-        )
+trigger_rates["night_sky_background"] = np.array(
+    nsb_rates["night_sky_background_rates"]["mean"]
+)
+for pk in res.PARTICLES:
+    trigger_rates[pk] = np.array(cosmic_rates[pk]["integral_rate"]["mean"])
 
-COSMIC_RAYS = list(PARTICLES.keys())
-COSMIC_RAYS.remove("gamma")
 
-for sk in SITES:
-    trigger_thresholds = np.array(TRIGGER[sk]["ratescan_thresholds_pe"])
-    analysis_trigger_threshold = TRIGGER[sk]["threshold_pe"]
+trigger_thresholds = np.array(TRIGGER["ratescan_thresholds_pe"])
+analysis_trigger_threshold = TRIGGER["threshold_pe"]
 
-    tr = trigger_rates[sk]
+tr = trigger_rates
 
-    fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
-    ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
+fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
+ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
+ax.plot(
+    trigger_thresholds,
+    tr["night_sky_background"] + tr["electron"] + tr["proton"] + tr["helium"],
+    "k",
+    label="night-sky + cosmic-rays",
+)
+ax.plot(
+    trigger_thresholds, tr["night_sky_background"], "k:", label="night-sky"
+)
+
+for ck in res.COSMIC_RAYS:
     ax.plot(
         trigger_thresholds,
-        tr["night_sky_background"]
-        + tr["electron"]
-        + tr["proton"]
-        + tr["helium"],
-        "k",
-        label="night-sky + cosmic-rays",
-    )
-    ax.plot(
-        trigger_thresholds, tr["night_sky_background"], "k:", label="night-sky"
+        tr[ck],
+        color=res.PARTICLE_COLORS[ck],
+        label=ck,
     )
 
-    for ck in COSMIC_RAYS:
-        ax.plot(
-            trigger_thresholds,
-            tr[ck],
-            color=particle_colors[ck],
-            label=ck,
-        )
-
-    ax.semilogy()
-    ax.set_xlabel("trigger-threshold / photo-electrons")
-    ax.set_ylabel("trigger-rate / s$^{-1}$")
-    ax.legend(loc="best", fontsize=8)
-    ax.axvline(
-        x=analysis_trigger_threshold, color="k", linestyle="-", alpha=0.25
-    )
-    ax.set_ylim([1e0, 1e7])
-    fig.savefig(os.path.join(paths["out_dir"], "{:s}_ratescan.jpg".format(sk)))
-    sebplt.close(fig)
+ax.semilogy()
+ax.set_xlabel("trigger-threshold / photo-electrons")
+ax.set_ylabel("trigger-rate / s$^{-1}$")
+ax.legend(loc="best", fontsize=8)
+ax.axvline(x=analysis_trigger_threshold, color="k", linestyle="-", alpha=0.25)
+ax.set_ylim([1e0, 1e7])
+fig.savefig(os.path.join(paths["out_dir"], "ratescan.jpg"))
+sebplt.close(fig)
