30bb7673d778f4f778e0746b028261a4eff39466
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-07-03 17:25:21 +0200

    fix 0131 zenith bins

--- plenoirf/summary/scripts/0131_trigger_rates_total.py
+++ plenoirf/summary/scripts/0131_trigger_rates_total.py
@@ -18,6 +18,7 @@ nsb_rates = json_utils.tree.read(
         "0120_trigger_rates_for_night_sky_background",
     )
 )
+zenith_bin = res.zenith_binning("once")
 
 TRIGGER = res.analysis["trigger"][res.site_key]
 
@@ -37,14 +38,19 @@ trigger_rates["night_sky_background"] = nsb_rates[
     "night_sky_background_rates"
 ]["mean"]
 
-for pk in res.PARTICLES:
-    trigger_rates[pk] = cosmic_rates[pk]["integral_rate"]["mean"]
+for zd in range(zenith_bin["num"]):
+    zk = f"zd{zd:d}"
+    trigger_rates[zk] = {}
+    for pk in res.PARTICLES:
+        trigger_rates[zk][pk] = np.array(
+            cosmic_rates[zk][pk]["integral_rate"]["mean"]
+        )
 
 json_utils.write(
     opj(res.paths["out_dir"], "trigger_rates_by_origin.json"),
     {
         "comment": (
-            "Trigger-rates by origin VS. trigger-threshold. "
+            "Trigger-rates by origin VS. zenith-bin VS. trigger-threshold. "
             "Including the analysis_trigger_threshold."
         ),
         "analysis_trigger_threshold_idx": analysis_trigger_threshold_idx,
