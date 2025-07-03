2b8e9094b0e1ab902f6e728a864f510f3f703810
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-07-03 17:20:43 +0200

    fix plot ratescan

--- plenoirf/summary/scripts/0130_trigger_ratescan_plot.py
+++ plenoirf/summary/scripts/0130_trigger_ratescan_plot.py
@@ -23,47 +23,61 @@ nsb_rates = json_utils.tree.read(
         "0120_trigger_rates_for_night_sky_background",
     )
 )
+zenith_bin = res.zenith_binning("once")
 
 trigger_rates = {}
 trigger_rates["night_sky_background"] = np.array(
     nsb_rates["night_sky_background_rates"]["mean"]
 )
-for pk in res.PARTICLES:
-    trigger_rates[pk] = np.array(cosmic_rates[pk]["integral_rate"]["mean"])
-
+for zd in range(zenith_bin["num"]):
+    zk = f"zd{zd:d}"
+    trigger_rates[zk] = {}
+    for pk in res.PARTICLES:
+        trigger_rates[zk][pk] = np.array(
+            cosmic_rates[zk][pk]["integral_rate"]["mean"]
+        )
 
 trigger_thresholds = np.array(TRIGGER["ratescan_thresholds_pe"])
 analysis_trigger_threshold = TRIGGER["threshold_pe"]
 
 tr = trigger_rates
 
-fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
-ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
-ax.plot(
-    trigger_thresholds,
-    tr["night_sky_background"] + tr["electron"] + tr["proton"] + tr["helium"],
-    "k",
-    label="night-sky + cosmic-rays",
-)
-ax.plot(
-    trigger_thresholds, tr["night_sky_background"], "k:", label="night-sky"
-)
 
-for ck in res.COSMIC_RAYS:
+for zd in range(zenith_bin["num"]):
+    zk = f"zd{zd:d}"
+
+    fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
+    ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
     ax.plot(
         trigger_thresholds,
-        tr[ck],
-        color=res.PARTICLE_COLORS[ck],
-        label=ck,
+        tr["night_sky_background"]
+        + tr[zk]["electron"]
+        + tr[zk]["proton"]
+        + tr[zk]["helium"],
+        "k",
+        label="night-sky + cosmic-rays",
+    )
+    ax.plot(
+        trigger_thresholds, tr["night_sky_background"], "k:", label="night-sky"
     )
 
-ax.semilogy()
-ax.set_xlabel("trigger-threshold / photo-electrons")
-ax.set_ylabel("trigger-rate / s$^{-1}$")
-ax.legend(loc="best", fontsize=8)
-ax.axvline(x=analysis_trigger_threshold, color="k", linestyle="-", alpha=0.25)
-ax.set_ylim([1e0, 1e7])
-fig.savefig(opj(res.paths["out_dir"], "ratescan.jpg"))
-sebplt.close(fig)
+    for ck in res.COSMIC_RAYS:
+        ax.plot(
+            trigger_thresholds,
+            tr[zk][ck],
+            color=res.PARTICLE_COLORS[ck],
+            label=ck,
+        )
+
+    ax.semilogy()
+    ax.set_xlabel("trigger-threshold / photo-electrons")
+    ax.set_ylabel("trigger-rate / s$^{-1}$")
+    ax.legend(loc="best", fontsize=8)
+    ax.axvline(
+        x=analysis_trigger_threshold, color="k", linestyle="-", alpha=0.25
+    )
+    ax.set_ylim([1e0, 1e7])
+    fig.savefig(opj(res.paths["out_dir"], f"zd{zd:d}_ratescan.jpg"))
+    sebplt.close(fig)
 
 res.stop()
