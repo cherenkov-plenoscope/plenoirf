9b7da576c6bcd4f036fbc884603dbaefadc6810b
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-08-06 14:24:08 +0200

    by zenith bin

--- plenoirf/summary/scripts/0300_onregion_trigger_acceptance.py
+++ plenoirf/summary/scripts/0300_onregion_trigger_acceptance.py
@@ -24,6 +24,9 @@ passing_quality = json_utils.tree.read(
 passing_trajectory_quality = json_utils.tree.read(
     opj(res.paths["analysis_dir"], "0059_passing_trajectory_quality")
 )
+zenith_assignment = json_utils.tree.read(
+    opj(res.paths["analysis_dir"], "0019_zenith_bin_assignment")
+)
 
 MAX_SOURCE_ANGLE_DEG = res.analysis["gamma_ray_source_direction"][
     "max_angle_relative_to_pointing_deg"
@@ -39,6 +42,7 @@ pointing_azimuth_deg = None
 pointing_zenith_deg = None
 
 energy_bin = res.energy_binning(key="trigger_acceptance_onregion")
+zenith_bin = res.zenith_binning("once")
 
 
 def cut_candidates_for_detection(
@@ -46,11 +50,18 @@ def cut_candidates_for_detection(
     uid_trajectory_quality,
     uid_trigger,
     uid_quality,
+    uid_zenith_assignment,
 ):
     uid_self = event_table["primary"]["uid"]
 
     uid_candidates = snt.logic.intersection(
-        [uid_self, uid_trigger, uid_quality, uid_trajectory_quality]
+        [
+            uid_self,
+            uid_trigger,
+            uid_quality,
+            uid_trajectory_quality,
+            uid_zenith_assignment,
+        ]
     )
 
     return snt.logic.cut_and_sort_table_on_indices(
@@ -74,205 +85,221 @@ def make_wighted_mask_wrt_primary_table(
     return mask
 
 
-for ok in ONREGION_TYPES:
+for zd in range(zenith_bin["num"]):
+    zk = f"zd{zd:d}"
+    for ok in ONREGION_TYPES:
+        for pk in res.PARTICLES:
+            os.makedirs(opj(res.paths["out_dir"], zk, ok, pk), exist_ok=True)
+
+
+for zd in range(zenith_bin["num"]):
+    zk = f"zd{zd:d}"
     for pk in res.PARTICLES:
-        os.makedirs(opj(res.paths["out_dir"], ok, pk), exist_ok=True)
 
+        with res.open_event_table(particle_key=pk) as arc:
+            diffuse_thrown = arc.query(
+                levels_and_columns={
+                    "primary": "__all__",
+                    "instrument_pointing": "__all__",
+                    "groundgrid_choice": "__all__",
+                    "reconstructed_trajectory": "__all__",
+                    "features": "__all__",
+                    "groundgrid": "__all__",
+                }
+            )
 
-for pk in res.PARTICLES:
-
-    with res.open_event_table(particle_key=pk) as arc:
-        diffuse_thrown = arc.query(
-            levels_and_columns={
-                "primary": "__all__",
-                "instrument_pointing": "__all__",
-                "groundgrid_choice": "__all__",
-                "reconstructed_trajectory": "__all__",
-                "features": "__all__",
-                "groundgrid": "__all__",
-            }
-        )
-
-    uid_possible_onregion = (
-        irf.analysis.cuts.cut_primary_direction_within_angle(
+        uid_possible_onregion = irf.analysis.cuts.cut_primary_direction_within_angle(
             event_table=diffuse_thrown,
             max_angle_between_primary_and_pointing_rad=MAX_SOURCE_ANGLE_RAD,
         )
-    )
 
-    # point source
-    # -------------
+        # point source
+        # -------------
 
-    # thrown
-    point_thrown = snt.logic.cut_table_on_indices(
-        table=diffuse_thrown,
-        common_indices=uid_possible_onregion,
-    )
-
-    # detected
-    point_candidate = cut_candidates_for_detection(
-        event_table=point_thrown,
-        uid_trajectory_quality=passing_trajectory_quality[pk]["uid"],
-        uid_trigger=passing_trigger[pk]["uid"],
-        uid_quality=passing_quality[pk]["uid"],
-    )
-
-    poicanarr = irf.reconstruction.trajectory_quality.make_rectangular_table(
-        event_table=point_candidate,
-        instrument_pointing_model=res.config["pointing"]["model"],
-    )
-
-    for ok in ONREGION_TYPES:
-        onregion_config = copy.deepcopy(ONREGION_TYPES[ok])
-        idx_dict_source_in_onregion = {}
-        for ii in range(poicanarr["uid"].shape[0]):
-            _onregion = irf.reconstruction.onregion.estimate_onregion(
-                reco_cx=poicanarr["reconstructed_trajectory/cx_rad"][ii],
-                reco_cy=poicanarr["reconstructed_trajectory/cy_rad"][ii],
-                reco_main_axis_azimuth=poicanarr[
-                    "reconstructed_trajectory/fuzzy_main_axis_azimuth_rad"
-                ][ii],
-                reco_num_photons=poicanarr["features/num_photons"][ii],
-                reco_core_radius=np.hypot(
-                    poicanarr["reconstructed_trajectory/x_m"][ii],
-                    poicanarr["reconstructed_trajectory/y_m"][ii],
-                ),
-                config=onregion_config,
-            )
-
-            hit = irf.reconstruction.onregion.is_direction_inside(
-                cx=poicanarr["true_trajectory/cx_rad"][ii],
-                cy=poicanarr["true_trajectory/cy_rad"][ii],
-                onregion=_onregion,
-            )
-
-            idx_dict_source_in_onregion[poicanarr["uid"][ii]] = hit
-
-        mask_detected = make_wighted_mask_wrt_primary_table(
-            primary_table=point_thrown["primary"],
-            idx_dict_of_weights=idx_dict_source_in_onregion,
+        # thrown
+        point_thrown = snt.logic.cut_table_on_indices(
+            table=diffuse_thrown,
+            common_indices=uid_possible_onregion,
         )
 
-        (
-            Qeff,
-            Qeff_au,
-        ) = atmospheric_cherenkov_response.analysis.effective_quantity_for_grid(
-            energy_bin_edges_GeV=energy_bin["edges"],
-            energy_GeV=point_thrown["primary"]["energy_GeV"],
-            mask_detected=mask_detected,
-            quantity_scatter=point_thrown["groundgrid"]["area_thrown_m2"],
-            num_grid_cells_above_lose_threshold=point_thrown["groundgrid"][
-                "num_bins_above_threshold"
-            ],
-            total_num_grid_cells=point_thrown["groundgrid"]["num_bins_thrown"],
+        # detected
+        point_candidate = cut_candidates_for_detection(
+            event_table=point_thrown,
+            uid_trajectory_quality=passing_trajectory_quality[pk]["uid"],
+            uid_trigger=passing_trigger[pk]["uid"],
+            uid_quality=passing_quality[pk]["uid"],
+            uid_zenith_assignment=zenith_assignment[zk][pk],
         )
 
-        json_utils.write(
-            opj(res.paths["out_dir"], ok, pk, "point.json"),
-            {
-                "comment": (
-                    "Effective area "
-                    "for a point source, reconstructed in onregion. "
-                    "VS energy"
-                ),
-                "unit": "m$^{2}$",
-                "mean": Qeff,
-                "absolute_uncertainty": Qeff_au,
-            },
+        poicanarr = (
+            irf.reconstruction.trajectory_quality.make_rectangular_table(
+                event_table=point_candidate,
+                instrument_pointing_model=res.config["pointing"]["model"],
+            )
         )
 
-    # diffuse source
-    # --------------
+        for ok in ONREGION_TYPES:
+            print(f"{zk:s}, {pk:s}, {ok:s}, point")
 
-    # thrown
-    diffuse_thrown = diffuse_thrown
-
-    # detected
-    diffuse_candidate = cut_candidates_for_detection(
-        event_table=diffuse_thrown,
-        uid_trajectory_quality=passing_trajectory_quality[pk]["uid"],
-        uid_trigger=passing_trigger[pk]["uid"],
-        uid_quality=passing_quality[pk]["uid"],
-    )
-
-    difcanarr = irf.reconstruction.trajectory_quality.make_rectangular_table(
-        event_table=diffuse_candidate,
-        instrument_pointing_model=res.config["pointing"]["model"],
-    )
-
-    for ok in ONREGION_TYPES:
-        onregion_config = copy.deepcopy(ONREGION_TYPES[ok])
-
-        idx_dict_probability_for_source_in_onregion = {}
-        for ii in range(difcanarr["uid"].shape[0]):
-            _onregion = irf.reconstruction.onregion.estimate_onregion(
-                reco_cx=difcanarr["reconstructed_trajectory/cx_rad"][ii],
-                reco_cy=difcanarr["reconstructed_trajectory/cy_rad"][ii],
-                reco_main_axis_azimuth=difcanarr[
-                    "reconstructed_trajectory/fuzzy_main_axis_azimuth_rad"
-                ][ii],
-                reco_num_photons=difcanarr["features/num_photons"][ii],
-                reco_core_radius=np.hypot(
-                    difcanarr["reconstructed_trajectory/x_m"][ii],
-                    difcanarr["reconstructed_trajectory/y_m"][ii],
-                ),
-                config=onregion_config,
-            )
-
-            onregion_polygon = irf.reconstruction.onregion.make_polygon(
-                onregion=_onregion, num_steps=37
-            )
-
-            overlap_srad = (
-                irf.reconstruction.onregion.intersecting_area_of_polygons(
-                    a=onregion_polygon, b=POSSIBLE_ONREGION_POLYGON
+            onregion_config = copy.deepcopy(ONREGION_TYPES[ok])
+            idx_dict_source_in_onregion = {}
+            for ii in range(poicanarr["uid"].shape[0]):
+                _onregion = irf.reconstruction.onregion.estimate_onregion(
+                    reco_cx=poicanarr["reconstructed_trajectory/cx_rad"][ii],
+                    reco_cy=poicanarr["reconstructed_trajectory/cy_rad"][ii],
+                    reco_main_axis_azimuth=poicanarr[
+                        "reconstructed_trajectory/fuzzy_main_axis_azimuth_rad"
+                    ][ii],
+                    reco_num_photons=poicanarr["features/num_photons"][ii],
+                    reco_core_radius=np.hypot(
+                        poicanarr["reconstructed_trajectory/x_m"][ii],
+                        poicanarr["reconstructed_trajectory/y_m"][ii],
+                    ),
+                    config=onregion_config,
                 )
+
+                hit = irf.reconstruction.onregion.is_direction_inside(
+                    cx=poicanarr["true_trajectory/cx_rad"][ii],
+                    cy=poicanarr["true_trajectory/cy_rad"][ii],
+                    onregion=_onregion,
+                )
+
+                idx_dict_source_in_onregion[poicanarr["uid"][ii]] = hit
+
+            mask_detected = make_wighted_mask_wrt_primary_table(
+                primary_table=point_thrown["primary"],
+                idx_dict_of_weights=idx_dict_source_in_onregion,
             )
 
-            probability_to_contain_random_source = (
-                overlap_srad / SOLID_ANGLE_TO_CONTAIN_SOURCE_SR
+            (
+                Qeff,
+                Qeff_au,
+            ) = atmospheric_cherenkov_response.analysis.effective_quantity_for_grid(
+                energy_bin_edges_GeV=energy_bin["edges"],
+                energy_GeV=point_thrown["primary"]["energy_GeV"],
+                mask_detected=mask_detected,
+                quantity_scatter=point_thrown["groundgrid"]["area_thrown_m2"],
+                num_grid_cells_above_lose_threshold=point_thrown["groundgrid"][
+                    "num_bins_above_threshold"
+                ],
+                total_num_grid_cells=point_thrown["groundgrid"][
+                    "num_bins_thrown"
+                ],
             )
 
-            idx_dict_probability_for_source_in_onregion[
-                difcanarr["uid"][ii]
-            ] = probability_to_contain_random_source
+            json_utils.write(
+                opj(res.paths["out_dir"], zk, ok, pk, "point.json"),
+                {
+                    "comment": (
+                        "Effective area "
+                        "for a point source, reconstructed in onregion. "
+                        "VS energy"
+                    ),
+                    "zenith_key": zk,
+                    "unit": "m$^{2}$",
+                    "mean": Qeff,
+                    "absolute_uncertainty": Qeff_au,
+                },
+            )
 
-        mask_probability_for_source_in_onregion = make_wighted_mask_wrt_primary_table(
-            primary_table=diffuse_thrown["primary"],
-            idx_dict_of_weights=idx_dict_probability_for_source_in_onregion,
+        # diffuse source
+        # --------------
+
+        # thrown
+        diffuse_thrown = diffuse_thrown
+
+        # detected
+        diffuse_candidate = cut_candidates_for_detection(
+            event_table=diffuse_thrown,
+            uid_trajectory_quality=passing_trajectory_quality[pk]["uid"],
+            uid_trigger=passing_trigger[pk]["uid"],
+            uid_quality=passing_quality[pk]["uid"],
+            uid_zenith_assignment=zenith_assignment[zk][pk],
         )
 
-        (
-            Qeff,
-            Qeff_au,
-        ) = atmospheric_cherenkov_response.analysis.effective_quantity_for_grid(
-            energy_bin_edges_GeV=energy_bin["edges"],
-            energy_GeV=diffuse_thrown["primary"]["energy_GeV"],
-            mask_detected=mask_probability_for_source_in_onregion,
-            quantity_scatter=(
-                diffuse_thrown["groundgrid"]["area_thrown_m2"]
-                * diffuse_thrown["primary"]["solid_angle_thrown_sr"]
-            ),
-            num_grid_cells_above_lose_threshold=diffuse_thrown["groundgrid"][
-                "num_bins_above_threshold"
-            ],
-            total_num_grid_cells=diffuse_thrown["groundgrid"][
-                "num_bins_thrown"
-            ],
+        difcanarr = (
+            irf.reconstruction.trajectory_quality.make_rectangular_table(
+                event_table=diffuse_candidate,
+                instrument_pointing_model=res.config["pointing"]["model"],
+            )
         )
 
-        json_utils.write(
-            opj(res.paths["out_dir"], ok, pk, "diffuse.json"),
-            {
-                "comment": (
-                    "Effective acceptance (area x solid angle) "
-                    "for a diffuse source, reconstructed in onregion. "
-                    "VS energy"
+        for ok in ONREGION_TYPES:
+            print(f"{zk:s}, {pk:s}, {ok:s}, diffuse")
+
+            onregion_config = copy.deepcopy(ONREGION_TYPES[ok])
+
+            idx_dict_probability_for_source_in_onregion = {}
+            for ii in range(difcanarr["uid"].shape[0]):
+                _onregion = irf.reconstruction.onregion.estimate_onregion(
+                    reco_cx=difcanarr["reconstructed_trajectory/cx_rad"][ii],
+                    reco_cy=difcanarr["reconstructed_trajectory/cy_rad"][ii],
+                    reco_main_axis_azimuth=difcanarr[
+                        "reconstructed_trajectory/fuzzy_main_axis_azimuth_rad"
+                    ][ii],
+                    reco_num_photons=difcanarr["features/num_photons"][ii],
+                    reco_core_radius=np.hypot(
+                        difcanarr["reconstructed_trajectory/x_m"][ii],
+                        difcanarr["reconstructed_trajectory/y_m"][ii],
+                    ),
+                    config=onregion_config,
+                )
+
+                onregion_polygon = irf.reconstruction.onregion.make_polygon(
+                    onregion=_onregion, num_steps=37
+                )
+
+                overlap_srad = (
+                    irf.reconstruction.onregion.intersecting_area_of_polygons(
+                        a=onregion_polygon, b=POSSIBLE_ONREGION_POLYGON
+                    )
+                )
+
+                probability_to_contain_random_source = (
+                    overlap_srad / SOLID_ANGLE_TO_CONTAIN_SOURCE_SR
+                )
+
+                idx_dict_probability_for_source_in_onregion[
+                    difcanarr["uid"][ii]
+                ] = probability_to_contain_random_source
+
+            mask_probability_for_source_in_onregion = make_wighted_mask_wrt_primary_table(
+                primary_table=diffuse_thrown["primary"],
+                idx_dict_of_weights=idx_dict_probability_for_source_in_onregion,
+            )
+
+            (
+                Qeff,
+                Qeff_au,
+            ) = atmospheric_cherenkov_response.analysis.effective_quantity_for_grid(
+                energy_bin_edges_GeV=energy_bin["edges"],
+                energy_GeV=diffuse_thrown["primary"]["energy_GeV"],
+                mask_detected=mask_probability_for_source_in_onregion,
+                quantity_scatter=(
+                    diffuse_thrown["groundgrid"]["area_thrown_m2"]
+                    * diffuse_thrown["primary"]["solid_angle_thrown_sr"]
                 ),
-                "unit": "m$^{2}$ sr",
-                "mean": Qeff,
-                "absolute_uncertainty": Qeff_au,
-            },
-        )
+                num_grid_cells_above_lose_threshold=diffuse_thrown[
+                    "groundgrid"
+                ]["num_bins_above_threshold"],
+                total_num_grid_cells=diffuse_thrown["groundgrid"][
+                    "num_bins_thrown"
+                ],
+            )
+
+            json_utils.write(
+                opj(res.paths["out_dir"], zk, ok, pk, "diffuse.json"),
+                {
+                    "comment": (
+                        "Effective acceptance (area x solid angle) "
+                        "for a diffuse source, reconstructed in onregion. "
+                        "VS energy"
+                    ),
+                    "zenith_key": zk,
+                    "unit": "m$^{2}$ sr",
+                    "mean": Qeff,
+                    "absolute_uncertainty": Qeff_au,
+                },
+            )
 
 res.stop()
--- plenoirf/summary/scripts/0301_onregion_trigger_acceptance_plot.py
+++ plenoirf/summary/scripts/0301_onregion_trigger_acceptance_plot.py
@@ -29,6 +29,8 @@ G = json_utils.tree.read(
 
 A_energy_bin = res.energy_binning(key="trigger_acceptance")
 G_energy_bin = res.energy_binning(key="trigger_acceptance_onregion")
+zenith_bin = res.zenith_binning("once")
+
 
 ONREGION_TYPES = res.analysis["on_off_measuremnent"]["onregion_types"]
 
@@ -37,98 +39,48 @@ idx_trigger_threshold = np.where(
 )[0][0]
 assert trigger["threshold_pe"] in trigger["ratescan_thresholds_pe"]
 
-for ok in ONREGION_TYPES:
-    for gk in irf.summary.figure.SOURCES:
-        fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
-        ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
+for zd in range(zenith_bin["num"]):
+    zk = f"zd{zd:d}"
 
-        text_y = 0
-        for pk in res.PARTICLES:
-            Q = G[ok][pk][gk]["mean"]
-            Q_au = G[ok][pk][gk]["absolute_uncertainty"]
-
-            sebplt.ax_add_histogram(
-                ax=ax,
-                bin_edges=G_energy_bin["edges"],
-                bincounts=Q,
-                linestyle="-",
-                linecolor=res.PARTICLE_COLORS[pk],
-                bincounts_upper=Q + Q_au,
-                bincounts_lower=Q - Q_au,
-                face_color=res.PARTICLE_COLORS[pk],
-                face_alpha=0.25,
-            )
-
-            ax.text(
-                0.9,
-                0.1 + text_y,
-                pk,
-                color=res.PARTICLE_COLORS[pk],
-                transform=ax.transAxes,
-            )
-            text_y += 0.06
-
-        ax.set_xlabel("energy / GeV")
-        ax.set_ylabel(
-            "{:s} / {:s}".format(
-                irf.summary.figure.SOURCES[gk]["label"],
-                irf.summary.figure.SOURCES[gk]["unit"],
-            )
-        )
-        ax.set_ylim(
-            irf.summary.figure.SOURCES[gk]["limits"]["passed_all_cuts"]
-        )
-        ax.loglog()
-        ax.set_xlim(G_energy_bin["limits"])
-        fig.savefig(opj(res.paths["out_dir"], f"{ok:s}_{pk:s}.jpg"))
-        sebplt.close(fig)
-
-
-for pk in res.PARTICLES:
-    for gk in irf.summary.figure.SOURCES:
-        acc_trg = A[pk][gk]["mean"][idx_trigger_threshold]
-
-        acc_trg_au = A[pk][gk]["absolute_uncertainty"][idx_trigger_threshold]
-
-        for ok in ONREGION_TYPES:
-            acc_trg_onregion = G[ok][pk][gk]["mean"]
-            acc_trg_onregion_au = G[ok][pk][gk]["absolute_uncertainty"]
-
+    for ok in ONREGION_TYPES:
+        for gk in irf.summary.figure.SOURCES:
             fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
             ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
 
-            sebplt.ax_add_histogram(
-                ax=ax,
-                bin_edges=A_energy_bin["edges"],
-                bincounts=acc_trg,
-                linestyle="-",
-                linecolor="gray",
-                bincounts_upper=acc_trg + acc_trg_au,
-                bincounts_lower=acc_trg - acc_trg_au,
-                face_color=res.PARTICLE_COLORS[pk],
-                face_alpha=0.05,
-            )
-            sebplt.ax_add_histogram(
-                ax=ax,
-                bin_edges=G_energy_bin["edges"],
-                bincounts=acc_trg_onregion,
-                linestyle="-",
-                linecolor=res.PARTICLE_COLORS[pk],
-                bincounts_upper=acc_trg_onregion + acc_trg_onregion_au,
-                bincounts_lower=acc_trg_onregion - acc_trg_onregion_au,
-                face_color=res.PARTICLE_COLORS[pk],
-                face_alpha=0.25,
+            sebplt.add_axes_zenith_range_indicator(
+                fig=fig,
+                span=irf.summary.figure.AX_SPAN_ZENITH_INDICATOR,
+                zenith_bin_edges_rad=zenith_bin["edges"],
+                zenith_bin=zd,
+                fontsize=6,
             )
 
-            ax.text(
-                s="onregion-radius at 100p.e.: {:.3f}".format(
-                    ONREGION_TYPES[ok]["opening_angle_deg"]
+            text_y = 0
+            for pk in res.PARTICLES:
+                Q = G[zk][ok][pk][gk]["mean"]
+                Q_au = G[zk][ok][pk][gk]["absolute_uncertainty"]
+
+                sebplt.ax_add_histogram(
+                    ax=ax,
+                    bin_edges=G_energy_bin["edges"],
+                    bincounts=Q,
+                    linestyle="-",
+                    linecolor=res.PARTICLE_COLORS[pk],
+                    bincounts_upper=Q + Q_au,
+                    bincounts_lower=Q - Q_au,
+                    face_color=res.PARTICLE_COLORS[pk],
+                    face_alpha=0.25,
                 )
-                + r"$^{\circ}$",
-                x=0.1,
-                y=0.1,
-                transform=ax.transAxes,
-            )
+
+                ax.text(
+                    0.9,
+                    0.1 + text_y,
+                    pk,
+                    color=res.PARTICLE_COLORS[pk],
+                    transform=ax.transAxes,
+                )
+                text_y += 0.06
+
             ax.set_xlabel("energy / GeV")
             ax.set_ylabel(
                 "{:s} / {:s}".format(
@@ -137,11 +89,93 @@ for pk in res.PARTICLES:
                 )
             )
             ax.set_ylim(
-                irf.summary.figure.SOURCES[gk]["limits"]["passed_trigger"]
+                irf.summary.figure.SOURCES[gk]["limits"]["passed_all_cuts"]
             )
             ax.loglog()
-            ax.set_xlim(A_energy_bin["limits"])
-            fig.savefig(opj(res.paths["out_dir"], f"{ok:s}_{pk:s}_{gk:s}.jpg"))
+            ax.set_xlim(G_energy_bin["limits"])
+            fig.savefig(opj(res.paths["out_dir"], f"{zk:s}_{ok:s}_{gk:s}.jpg"))
             sebplt.close(fig)
+
+
+compare_trigger_level_dir = opj(res.paths["out_dir"], "compare_trigger")
+os.makedirs(compare_trigger_level_dir)
+
+for zd in range(zenith_bin["num"]):
+    zk = f"zd{zd:d}"
+
+    for pk in res.PARTICLES:
+        for gk in irf.summary.figure.SOURCES:
+            acc_trg = A[zk][pk][gk]["mean"][idx_trigger_threshold]
+
+            acc_trg_au = A[zk][pk][gk]["absolute_uncertainty"][
+                idx_trigger_threshold
+            ]
+
+            for ok in ONREGION_TYPES:
+                acc_trg_onregion = G[zk][ok][pk][gk]["mean"]
+                acc_trg_onregion_au = G[zk][ok][pk][gk]["absolute_uncertainty"]
+
+                fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
+                ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
+
+                sebplt.add_axes_zenith_range_indicator(
+                    fig=fig,
+                    span=irf.summary.figure.AX_SPAN_ZENITH_INDICATOR,
+                    zenith_bin_edges_rad=zenith_bin["edges"],
+                    zenith_bin=zd,
+                    fontsize=6,
+                )
+
+                sebplt.ax_add_histogram(
+                    ax=ax,
+                    bin_edges=A_energy_bin["edges"],
+                    bincounts=acc_trg,
+                    linestyle="-",
+                    linecolor="gray",
+                    bincounts_upper=acc_trg + acc_trg_au,
+                    bincounts_lower=acc_trg - acc_trg_au,
+                    face_color=res.PARTICLE_COLORS[pk],
+                    face_alpha=0.05,
+                )
+                sebplt.ax_add_histogram(
+                    ax=ax,
+                    bin_edges=G_energy_bin["edges"],
+                    bincounts=acc_trg_onregion,
+                    linestyle="-",
+                    linecolor=res.PARTICLE_COLORS[pk],
+                    bincounts_upper=acc_trg_onregion + acc_trg_onregion_au,
+                    bincounts_lower=acc_trg_onregion - acc_trg_onregion_au,
+                    face_color=res.PARTICLE_COLORS[pk],
+                    face_alpha=0.25,
+                )
+
+                ax.text(
+                    s="onregion-radius at 100p.e.: {:.3f}".format(
+                        ONREGION_TYPES[ok]["opening_angle_deg"]
+                    )
+                    + r"$^{\circ}$",
+                    x=0.1,
+                    y=0.1,
+                    transform=ax.transAxes,
+                )
+                ax.set_xlabel("energy / GeV")
+                ax.set_ylabel(
+                    "{:s} / {:s}".format(
+                        irf.summary.figure.SOURCES[gk]["label"],
+                        irf.summary.figure.SOURCES[gk]["unit"],
+                    )
+                )
+                ax.set_ylim(
+                    irf.summary.figure.SOURCES[gk]["limits"]["passed_trigger"]
+                )
+                ax.loglog()
+                ax.set_xlim(A_energy_bin["limits"])
+                fig.savefig(
+                    opj(
+                        compare_trigger_level_dir,
+                        f"{zk:s}_{ok:s}_{pk:s}_{gk:s}.jpg",
+                    )
+                )
+                sebplt.close(fig)
 
 res.stop()
