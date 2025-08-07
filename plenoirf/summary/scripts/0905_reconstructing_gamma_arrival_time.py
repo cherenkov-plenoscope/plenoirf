862b1d28788396d76fbc44c5fd6ec57b34941b98
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-08-07 17:02:25 +0200

    update time of arrival

--- plenoirf/summary/scripts/0905_reconstructing_gamma_arrival_time.py
+++ plenoirf/summary/scripts/0905_reconstructing_gamma_arrival_time.py
@@ -13,33 +13,24 @@ import sebastians_matplotlib_addons as sebplt
 import json_utils
 
 
-argv = irf.summary.argv_since_py(sys.argv)
-pa = irf.summary.paths_from_argv(argv)
-
-irf_config = irf.summary.read_instrument_response_config(
-    run_dir=paths["plenoirf_dir"]
-)
-sum_config = irf.summary.read_summary_config(summary_dir=paths["analysis_dir"])
-sebplt.matplotlib.rcParams.update(sum_config["plot"]["matplotlib"])
-
-os.makedirs(paths["out_dir"], exist_ok=True)
+res = irf.summary.ScriptResources.from_argv(sys.argv)
+res.start()
 
 passing_trigger = json_utils.tree.read(
-    opj(paths["analysis_dir"], "0055_passing_trigger")
+    opj(res.paths["analysis_dir"], "0055_passing_trigger")
 )
 passing_quality = json_utils.tree.read(
-    opj(paths["analysis_dir"], "0056_passing_basic_quality")
+    opj(res.paths["analysis_dir"], "0056_passing_basic_quality")
 )
 passing_trajectory_quality = json_utils.tree.read(
-    opj(paths["analysis_dir"], "0059_passing_trajectory_quality")
+    opj(res.paths["analysis_dir"], "0059_passing_trajectory_quality")
 )
-energy_bin = json_utils.read(
-    opj(paths["analysis_dir"], "0005_common_binning", "energy.json")
-)["point_spread_function"]
+zenith_assignment = json_utils.tree.read(
+    opj(res.paths["analysis_dir"], "0019_zenith_bin_assignment")
+)
+energy_bin = res.energy_binning(key="point_spread_function")
+zenith_bin = res.zenith_binning("once")
 
-
-SITES = irf_config["config"]["sites"]
-PARTICLES = irf_config["config"]["particles"]
 SPEED_OF_LIGHT_IN_VACUUM = 299792458
 SPEED_OF_GAMMA_RAY_IN_ATMOSPHERE = SPEED_OF_LIGHT_IN_VACUUM  # good enough
 SPEED_OF_CHERENKOVLIGHT_IN_ATMOSPHERE = SPEED_OF_LIGHT_IN_VACUUM / 1.0003
@@ -52,50 +43,47 @@ def normed(v):
 
 
 tds = {}
-for sk in SITES:
-    tds[sk] = {}
-    for pk in ["gamma"]:
-        tds[sk][pk] = {}
-        event_table = snt.read(
-            path=opj(
-                paths["plenoirf_dir"],
-                "event_table",
-                sk,
-                pk,
-                "event_table.tar",
-            ),
-            structure=irf.table.STRUCTURE,
-        )
+for zd in range(zenith_bin["num"]):
+    zk = f"zd{zd:d}"
 
-        valid_idx = snt.intersection(
+    tds[zk] = {}
+    for pk in ["gamma"]:
+        tds[zk][pk] = {}
+
+        with res.open_event_table(particle_key=pk) as arc:
+            event_table = arc.query(
+                levels_and_columns={
+                    "primary": "__all__",
+                    "groundgrid_choice": "__all__",
+                    "groundgrid": "__all__",
+                    "instrument": "__all__",
+                    "features": "__all__",
+                    "reconstructed_trajectory": "__all__",
+                }
+            )
+
+        uid_common = snt.logic.intersection(
             [
-                passing_trigger[sk][pk]["uid"],
-                passing_quality[sk][pk]["uid"],
-                passing_trajectory_quality[sk][pk]["uid"],
+                zenith_assignment[zk][pk],
+                passing_trigger[pk]["uid"],
+                passing_quality[pk]["uid"],
+                passing_trajectory_quality[pk]["uid"],
             ]
         )
 
-        event_table = snt.cut_table_on_indices(
+        event_table = snt.logic.cut_table_on_indices(
             table=event_table,
-            common_indices=valid_idx,
-            level_keys=[
-                "primary",
-                "core",
-                "grid",
-                "instrument",
-                "features",
-                "reconstructed_trajectory",
-            ],
+            common_indices=uid_common,
         )
 
-        et = snt.make_rectangular_DataFrame(event_table).to_records()
+        et = snt.logic.make_rectangular_DataFrame(event_table).to_records()
 
-        tds[sk][pk]["true_wrt_corsika_wrt_collection_plane_s"] = []
-        tds[sk][pk]["reco_simple_wrt_corsika_wrt_collection_plane_s"] = []
-        tds[sk][pk]["reco_better_wrt_corsika_wrt_collection_plane_s"] = []
-        tds[sk][pk]["tDelta_simple"] = []
-        tds[sk][pk]["tDelta_better"] = []
-        tds[sk][pk]["energy_GeV"] = []
+        tds[zk][pk]["true_wrt_corsika_wrt_collection_plane_s"] = []
+        tds[zk][pk]["reco_simple_wrt_corsika_wrt_collection_plane_s"] = []
+        tds[zk][pk]["reco_better_wrt_corsika_wrt_collection_plane_s"] = []
+        tds[zk][pk]["tDelta_simple"] = []
+        tds[zk][pk]["tDelta_better"] = []
+        tds[zk][pk]["energy_GeV"] = []
 
         for i in range(len(et)):
             """
@@ -104,7 +92,7 @@ for sk in SITES:
                 continue
             """
 
-            tds[sk][pk]["energy_GeV"].append(et["primary/energy_GeV"][i])
+            tds[zk][pk]["energy_GeV"].append(et["primary/energy_GeV"][i])
             # true arrival-time
             # -----------------
             particle_starting_direction = np.array(
@@ -118,8 +106,10 @@ for sk in SITES:
 
             particle_starting_position = np.array(
                 [
-                    et["primary/starting_x_m"][i] - et["core/core_x_m"][i],
-                    et["primary/starting_y_m"][i] - et["core/core_y_m"][i],
+                    et["primary/starting_x_m"][i]
+                    - et["groundgrid_choice/core_x_m"][i],
+                    et["primary/starting_y_m"][i]
+                    - et["groundgrid_choice/core_y_m"][i],
                     et["primary/starting_height_asl_m"][i],
                 ]
             )
@@ -128,9 +118,7 @@ for sk in SITES:
                 [
                     0,
                     0,
-                    irf_config["config"]["sites"][sk][
-                        "observation_level_asl_m"
-                    ],
+                    res.SITE["observation_level_asl_m"],
                 ]
             )
 
@@ -151,7 +139,7 @@ for sk in SITES:
             t_true = (
                 distance_to_collection_plane / SPEED_OF_GAMMA_RAY_IN_ATMOSPHERE
             )
-            tds[sk][pk]["true_wrt_corsika_wrt_collection_plane_s"].append(
+            tds[zk][pk]["true_wrt_corsika_wrt_collection_plane_s"].append(
                 t_true
             )
 
@@ -161,10 +149,10 @@ for sk in SITES:
             # simple: assume time of detection is the gamma-ray's arriving time
             # -----------------------------------------------------------------
             t_simple = et["instrument/start_time_of_exposure_s"][i]
-            tds[sk][pk][
+            tds[zk][pk][
                 "reco_simple_wrt_corsika_wrt_collection_plane_s"
             ].append(t_simple)
-            tds[sk][pk]["tDelta_simple"].append(t_simple - t_true)
+            tds[zk][pk]["tDelta_simple"].append(t_simple - t_true)
 
             # better:
             # -------
@@ -175,18 +163,18 @@ for sk in SITES:
                 - 1 / SPEED_OF_CHERENKOVLIGHT_IN_ATMOSPHERE
             )
             t_better = et["instrument/start_time_of_exposure_s"][i] + tCorr
-            tds[sk][pk][
+            tds[zk][pk][
                 "reco_better_wrt_corsika_wrt_collection_plane_s"
             ].append(t_better)
-            tds[sk][pk]["tDelta_better"].append(t_better - t_true)
+            tds[zk][pk]["tDelta_better"].append(t_better - t_true)
 
-        tDelta = tds[sk][pk]["tDelta_better"] - np.median(
-            tds[sk][pk]["tDelta_better"]
+        tDelta = tds[zk][pk]["tDelta_better"] - np.median(
+            tds[zk][pk]["tDelta_better"]
         )
         tDelta_rel = tDelta - np.median(tDelta)
 
         E_MAX = 5.0
-        e_mask = np.array(tds[sk][pk]["energy_GeV"]) <= E_MAX
+        e_mask = np.array(tds[zk][pk]["energy_GeV"]) <= E_MAX
         tDelta_10 = tDelta[e_mask]
         tDelta_rel_10 = tDelta_10 - np.median(tDelta_10)
 
@@ -195,6 +183,13 @@ for sk in SITES:
         # plot all energies
         fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
         ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
+        sebplt.add_axes_zenith_range_indicator(
+            fig=fig,
+            span=irf.summary.figure.AX_SPAN_ZENITH_INDICATOR,
+            zenith_bin_edges_rad=zenith_bin["edges"],
+            zenith_bin=zd,
+            fontsize=6,
+        )
         bincounts = np.histogram(a=tDelta_rel, bins=TIME_BIN_EDGES)[0]
         numall = np.sum(bincounts)
         bincounts = bincounts / numall
@@ -261,8 +256,8 @@ for sk in SITES:
         ax.set_ylabel("intensity")
         fig.savefig(
             opj(
-                paths["out_dir"],
-                "{:s}_{:s}_arrival_time_spread.jpg".format(sk, pk),
+                res.paths["out_dir"],
+                f"{zk:s}_{pk:s}_arrival_time_spread.jpg",
             )
         )
         sebplt.close(fig)
