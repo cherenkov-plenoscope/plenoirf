56c5e6d340a0c8bf681d967e0588082f2e813a6f
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-08-20 19:48:21 +0200

    train and test splitting

--- plenoirf/bootstripping.py
+++ plenoirf/bootstripping.py
@@ -18,3 +18,13 @@ def draw(samples, bootstrip, num_bootstrips):
         num_bootstrips=num_bootstrips,
     )
     return samples[arr]
+
+
+def train_test_split(x, bootstrip, num_bootstrips):
+    x = np.asarray(x)
+    x_test = draw(
+        samples=x, bootstrip=bootstrip, num_bootstrips=num_bootstrips
+    )
+    _x_train = set.difference(set(x), set(x_test))
+    x_train = np.array(list(_x_train), dtype=x.dtype)
+    return x_train, x_test
