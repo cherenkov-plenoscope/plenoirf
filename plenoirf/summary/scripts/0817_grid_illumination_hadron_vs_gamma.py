acb87f5e1b4b9d5eda1ea2c77883bcbb9cea2eb6
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-09-03 23:28:42 +0200

    fix and reduce memory load

--- plenoirf/summary/scripts/0817_grid_illumination_hadron_vs_gamma.py
+++ plenoirf/summary/scripts/0817_grid_illumination_hadron_vs_gamma.py
@@ -23,35 +23,46 @@ grid_threshold_num_photons = res.config["ground_grid"]["threshold_num_photons"]
 
 PORTAL_TO_CTA_MST_RATIO = 35
 
-VETO_MIRROR_RATIO = 100
+VETO_MIRROR_RATIO = PORTAL_TO_CTA_MST_RATIO
 
 VETO_TELESCOPE_TRIGGER_THRESHOLD_NUM_PHOTONS = (
     VETO_MIRROR_RATIO * grid_threshold_num_photons
 )
 
 gD = 2 * veto_radius_grid_cells + 1
-plenoscope_diameter = (
-    grid_geometry["bin_width"]
-    / irf_config["config"]["grid"]["bin_width_overhead"]
+plenoscope_diameter_m = (
+    2
+    * res.instrument["light_field_sensor_geometry"][
+        "expected_imaging_system_aperture_radius"
+    ]
 )
 VETO_STR = "outer array {:d} x {:d} telescopes\n".format(gD, gD)
 VETO_STR += "spacing {:.1f}m, mirror diameter {:.1f}m.".format(
-    grid_geometry["bin_width"],
-    plenoscope_diameter / np.sqrt(VETO_MIRROR_RATIO),
+    grid_geometry["bin_width_m"],
+    plenoscope_diameter_m / np.sqrt(VETO_MIRROR_RATIO),
 )
 
 MAX_NUM_PARTICLES = 250
 AX_SPAN = list(irf.summary.figure.AX_SPAN)
 AX_SPAN[3] = AX_SPAN[3] * 0.85
 
+
+def grid_histogram_to_dict(grid_histogram2d):
+    out = {}
+    for item in grid_histogram2d:
+        out[item["x_bin"], item["y_bin"]] = item["weight_photons"]
+    return out
+
+
 pv = {}
 for pk in res.PARTICLES:
+
     ggi_path = opj(
         res.response_path(particle_key=pk), "ground_grid_intensity.zip"
     )
 
     uid_ggi = []
-    ggi = irf.ground_grid.intensity.Reader(ggi_path)
+    ggi = irf.ground_grid.histogram2d.Reader(ggi_path)
     for uid in ggi:
         uid_ggi.append(uid)
 
@@ -60,7 +71,12 @@ for pk in res.PARTICLES:
     )
 
     with res.open_event_table(particle_key=pk) as arc:
-        event_table = arc.query(levels_and_columns={"primary": "__all__"})
+        event_table = arc.query(
+            levels_and_columns={
+                "primary": ("uid", "energy_GeV"),
+                "groundgrid_choice": ("uid", "bin_idx_x", "bin_idx_y"),
+            }
+        )
 
     detected_events = snt.logic.cut_table_on_indices(
         table=event_table,
@@ -72,23 +88,13 @@ for pk in res.PARTICLES:
     pv[pk]["num_passed"] = np.zeros(energy_bin["num"])
 
     particle_counter = 0
-    for shower_idx in idx_passed_trigger_and_in_debug_output:
+    for uid_shower in uid_trigger_ggi:
         if particle_counter >= MAX_NUM_PARTICLES:
             break
 
-        shower_table = snt.cut_table_on_indices(
+        shower_table = snt.logic.cut_and_sort_table_on_indices(
             table=detected_events,
-            common_indices=[shower_idx],
-            level_keys=[
-                "primary",
-                "grid",
-                "core",
-                "cherenkovsize",
-                "cherenkovpool",
-                "cherenkovsizepart",
-                "cherenkovpoolpart",
-                "trigger",
-            ],
+            common_indices=[uid_shower],
         )
         energy_GeV = shower_table["primary"]["energy_GeV"][0]
 
@@ -102,11 +108,9 @@ for pk in res.PARTICLES:
             < energy_bin["edges"][energy_bin_idx]
         )
 
-        grid_intensity = irf.grid.bytes_to_histogram(
-            detected_grid_histograms[shower_idx]
-        )
-        grid_bin_idx_x = shower_table["core"]["bin_idx_x"][0]
-        grid_bin_idx_y = shower_table["core"]["bin_idx_y"][0]
+        grid_intensity = grid_histogram_to_dict(ggi[uid_shower])
+        grid_bin_idx_x = shower_table["groundgrid_choice"]["bin_idx_x"][0]
+        grid_bin_idx_y = shower_table["groundgrid_choice"]["bin_idx_y"][0]
 
         gR = veto_radius_grid_cells
         grid_bin_idx_veto = []
@@ -114,19 +118,23 @@ for pk in res.PARTICLES:
             for iiy in np.arange(
                 grid_bin_idx_y - gR, grid_bin_idx_y + gR + 1, 1
             ):
-                if iix >= 0 and iix < grid_geometry["num_bins_diameter"]:
-                    if iiy >= 0 and iiy < grid_geometry["num_bins_diameter"]:
+                if iix >= 0 and iix < grid_geometry["num_bins_each_axis"]:
+                    if iiy >= 0 and iiy < grid_geometry["num_bins_each_axis"]:
                         grid_bin_idx_veto.append((iix, iiy))
 
         num_veto_trials = len(grid_bin_idx_veto)
         num_veto_triggers = 0
 
         for bin_idx in grid_bin_idx_veto:
-            if (
-                grid_intensity[bin_idx]
-                >= VETO_TELESCOPE_TRIGGER_THRESHOLD_NUM_PHOTONS
-            ):
-                num_veto_triggers += 1
+
+            if bin_idx in grid_intensity:
+                if (
+                    grid_intensity[bin_idx]
+                    >= VETO_TELESCOPE_TRIGGER_THRESHOLD_NUM_PHOTONS
+                ):
+                    num_veto_triggers += 1
+            else:
+                print("no bin_idx", bin_idx)
 
         pv[pk]["num_thrown"][energy_bin_idx] += 1
         if num_veto_triggers > 0:
