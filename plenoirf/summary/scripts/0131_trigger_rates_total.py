3793533850a1828e4f1be337038255d30ccff484
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-09-10 16:33:38 +0200

    adopt from 0130

--- plenoirf/summary/scripts/0131_trigger_rates_total.py
+++ plenoirf/summary/scripts/0131_trigger_rates_total.py
@@ -1,53 +1,56 @@
 #!/usr/bin/python
 import sys
-import numpy as np
 import plenoirf as irf
+import sparse_numeric_table as snt
 import os
 from os.path import join as opj
+import numpy as np
+import sebastians_matplotlib_addons as sebplt
 import json_utils
+import propagate_uncertainties as pru
+
 
 res = irf.summary.ScriptResources.from_argv(sys.argv)
-res.start()
+res.start(sebplt=sebplt)
+
+trigger = res.trigger
 
 cosmic_rates = json_utils.tree.read(
     opj(res.paths["analysis_dir"], "0105_trigger_rates_for_cosmic_particles")
 )
-nsb_rates = json_utils.tree.read(
+nsb = json_utils.tree.read(
     opj(
         res.paths["analysis_dir"],
         "0120_trigger_rates_for_night_sky_background",
     )
-)
+)["night_sky_background_rates"]
 zenith_bin = res.zenith_binning("once")
-trigger = res.trigger
 
 trigger_rates = {}
 
-assert trigger["threshold_pe"] in trigger["ratescan_thresholds_pe"]
-analysis_trigger_threshold_idx = (
-    irf.utils.find_closest_index_in_array_for_value(
-        arr=trigger["ratescan_thresholds_pe"], val=trigger["threshold_pe"]
-    )
-)
-
-trigger_rates = {}
-trigger_rates["night_sky_background"] = nsb_rates[
-    "night_sky_background_rates"
-]["mean"]
+num_trigger_thresholds = len(trigger["ratescan_thresholds_pe"])
 
 for zd in range(zenith_bin["num"]):
     zk = f"zd{zd:d}"
     trigger_rates[zk] = {}
+    trigger_rates[zk]["night_sky_background"] = {}
+    trigger_rates[zk]["night_sky_background"]["rate"] = nsb[zk]["rate"]
+    trigger_rates[zk]["night_sky_background"]["rate_au"] = nsb[zk]["rate_au"]
+
     for pk in res.PARTICLES:
-        trigger_rates[zk][pk] = np.array(
-            cosmic_rates[zk][pk]["integral_rate"]["mean"]
-        )
+        trigger_rates[zk][pk] = {}
+        trigger_rates[zk][pk]["rate"] = cosmic_rates[zk][pk]["integral_rate"][
+            "mean"
+        ]
+        trigger_rates[zk][pk]["rate_au"] = cosmic_rates[zk][pk][
+            "integral_rate"
+        ]["absolute_uncertainty"]
 
 json_utils.write(
     opj(res.paths["out_dir"], "trigger_rates_by_origin.json"),
     {
         "comment": (
-            "Trigger-rates by origin VS. zenith-bin VS. trigger-threshold. "
+            "Trigger rates by origin VS. zenith-bin VS. trigger threshold. "
             "Including the analysis_trigger_threshold."
         ),
         "analysis_trigger_threshold_idx": analysis_trigger_threshold_idx,
