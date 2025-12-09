f5502fb899110c2cf94c9a1b89437e34b6f06f6f
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-12-09 13:00:15 +0100

    rm obsolete

--- plenoirf/utils.py
+++ plenoirf/utils.py
@@ -1,15 +1,11 @@
 import numpy as np
 import propagate_uncertainties as pru
-import binning_utils
-import datetime
 import io
 import os
-import scipy.interpolate
 import json_utils
 import warnings
 import gzip
 import rename_after_writing as rnw
-import warnings
 
 
 def gzip_file(path, ext=".gz", block_size=2**23):
@@ -63,13 +59,6 @@ def _has_gzip_signature_bytes(path):
             return False
 
 
-def contains_same_bytes(path_a, path_b):
-    with open(path_a, "rb") as fa, open(path_b, "rb") as fb:
-        a_bytes = fa.read()
-        b_bytes = fb.read()
-        return a_bytes == b_bytes
-
-
 def _divide_silent(numerator, denominator, default):
     valid = denominator != 0
     division = np.ones(shape=numerator.shape) * default
@@ -84,34 +73,6 @@ _1d = _1h * 24
 _1w = _1d * 7
 _1m = _1d * 30
 _1y = 365 * _1d
-
-
-def make_civil_times_points_in_quasi_logspace():
-    """
-    time-points from 1s to 100y in the civil steps of:
-    s, m, h, d, week, Month, year, decade
-    """
-
-    times = []
-    for _secs in np.arange(1, _10s, 1):
-        times.append(_secs)
-    for _10secs in np.arange(_10s, _1M, _10s):
-        times.append(_10secs)
-    for _mins in np.arange(_1M, _1h, _1M):
-        times.append(_mins)
-    for _hours in np.arange(_1h, _1d, _1h):
-        times.append(_hours)
-    for _days in np.arange(_1d, _1w, _1d):
-        times.append(_days)
-    for _weeks in np.arange(_1w, 4 * _1w, _1w):
-        times.append(_weeks)
-    for _months in np.arange(_1m, 12 * _1m, _1m):
-        times.append(_months)
-    for _years in np.arange(_1y, 10 * _1y, _1y):
-        times.append(_years)
-    for _decades in np.arange(10 * _1y, 100 * _1y, 10 * _1y):
-        times.append(_decades)
-    return times
 
 
 def make_civil_time_str(time_s, format_seconds="{:f}"):
@@ -202,38 +163,6 @@ def filter_particles_with_electric_charge(particles):
     return out
 
 
-def copy_square_selection_from_2D_array(img, ix, iy, r, fill=np.nan):
-    """
-    Returns a square selection from the input-image.
-
-    Parameters
-    ----------
-    img : np.array 2D
-        Input image to copy from.
-    ix : int
-        X central bin for selection in input-image
-    iy : int
-        Y central bin for selection in input-image
-    r : int
-        Radius of selection. Width of selection is (2*r + 1).
-    fill : float/int
-        If selection is outside of input-image, this value is written to
-        output.
-    """
-    assert r >= 0
-    out = fill * np.ones(shape=(2 * r + 1, 2 * r + 1), dtype=img.dtype)
-    x_start = ix - r
-    x_stop = ix + r + 1
-    y_start = iy - r
-    y_stop = iy + r + 1
-    for ox, ix in enumerate(np.arange(x_start, x_stop)):
-        for oy, iy in enumerate(np.arange(y_start, y_stop)):
-            if ix >= 0 and ix < img.shape[0]:
-                if iy >= 0 and iy < img.shape[1]:
-                    out[ox, oy] = img[ix, iy]
-    return out
-
-
 def fill_nans_from_end(arr, val):
     for i in np.arange(len(arr) - 1, -1, -1):
         if np.isnan(arr[i]):
@@ -250,14 +179,6 @@ def fill_nans_from_start(arr, val):
         else:
             break
     return arr
-
-
-def gradient_in_bin_edges(x, bin_edges):
-    assert len(bin_edges) - 1 == len(x)
-    dx = np.nan * np.ones(len(x) - 1)
-    for i in range(len(x) - 1):
-        dx[i] = (x[i + 1] - x[i]) / bin_edges[i]
-    return dx
 
 
 def read_json_but_forgive(path, default={}):
@@ -481,19 +402,3 @@ def parse_metric_prefix(s):
         out = int(s)
 
     return out
-
-
-def get_index_of_closest_match(x, y):
-    """
-    Returns the index in 'x' which has its value closest to value 'y'.
-
-    Parameters
-    ----------
-    x : array like
-    y : scalar
-
-    Returns
-    -------
-    index : int
-    """
-    return int(np.argmin(np.abs(x - y)))
