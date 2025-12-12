b7d2b388cf594e98ab2f220f85aff6b83b3a31e2
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-12-12 15:54:54 +0100

    plot trigger config

--- plenoirf/summary/scripts/0053_trigger_config_plot.py
+++ plenoirf/summary/scripts/0053_trigger_config_plot.py
@@ -38,28 +38,26 @@ fig.savefig(
 sebplt.close(fig)
 
 
-
-res.analysis["trigger"]["modus"]["accepting"]["threshold_accepting_over_rejecting"]
-
 fig = sebplt.figure(irf.summary.figure.FIGURE_STYLE)
 ax = sebplt.add_axes(fig=fig, span=irf.summary.figure.AX_SPAN)
-x_zd_rad = np.linspace(0, np.deg2rad(45), 1337)
+accepting_response_pe = np.geomspace(1e2, 1e6, 1337)
 ax.plot(
-    np.rad2deg(x_zd_rad),
-    irf.light_field_trigger.get_trigger_threshold_corrected_for_pointing_zenith(
-        pointing_zenith_rad=x_zd_rad,
+    accepting_response_pe,
+    irf.light_field_trigger.get_accepting_over_rejecting(
         trigger=trigger,
-        nominal_threshold_pe=trigger["threshold_pe"],
+        accepting_response_pe=accepting_response_pe,
     ),
     color="black",
 )
-ax.set_xlabel(r"instrument pointing zenith / (1$^{\circ}$)")
-ax.set_ylabel("trigger threshold / photo electrons")
-ax.set_xlim([0, 50])
+ax.set_xlabel("accepting focus response / photo electrons")
+ax.set_ylabel("accepting over rejecting ratio / 1")
+ax.set_xlim([min(accepting_response_pe), max(accepting_response_pe)])
+ax.semilogx()
+ax.axvline(x=trigger["threshold_pe"], color="gray", linestyle="--")
 fig.savefig(
     opj(
         res.paths["out_dir"],
-        "threshold_vs_zenith.jpg",
+        "accepting_over_rejecting_ratio_vs_accepting_response.jpg",
     )
 )
 sebplt.close(fig)
