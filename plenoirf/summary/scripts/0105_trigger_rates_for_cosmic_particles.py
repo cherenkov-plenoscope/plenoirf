cc202d62eb2dca48a2e73f0f9cb7d6b856dcb415
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2025-12-19 21:31:58 +0100

    adopt both trigger modi in 0105 and 0106

--- plenoirf/summary/scripts/0105_trigger_rates_for_cosmic_particles.py
+++ plenoirf/summary/scripts/0105_trigger_rates_for_cosmic_particles.py
@@ -17,6 +17,10 @@ energy_bin = res.energy_binning(key="10_bins_per_decade")
 zenith_bin = res.zenith_binning("3_bins_per_45deg")
 fine_energy_bin = res.energy_binning(key="60_bins_per_decade")
 
+TRIGGER_MODI = json_utils.read(
+    opj(res.paths["analysis_dir"], "0055_passing_trigger", "trigger_modi.json")
+)
+
 acceptance = json_utils.tree.Tree(
     opj(
         res.paths["analysis_dir"],
@@ -71,101 +75,48 @@ trigger_thresholds = np.array(
 analysis_trigger_threshold = res.analysis["trigger"]["threshold_pe"]
 num_trigger_thresholds = len(trigger_thresholds)
 
+
+# make out dirs
+for zd in range(zenith_bin["num"]):
+    zk = f"zd{zd:d}"
+    for pk in res.PARTICLES:
+        for tk in TRIGGER_MODI:
+            os.makedirs(opj(res.paths["out_dir"], zk, pk, tk), exist_ok=True)
+
+
 for zd in range(zenith_bin["num"]):
     zk = f"zd{zd:d}"
 
-    # gamma-ray
-    # ---------
-    gamma_dir = opj(res.paths["out_dir"], zk, "gamma")
-    os.makedirs(gamma_dir, exist_ok=True)
+    for tk in TRIGGER_MODI:
 
-    _A = acceptance[zk]["gamma"]["point"]["mean"]
-    _A_au = acceptance[zk]["gamma"]["point"]["absolute_uncertainty"]
+        # gamma-ray
+        # ---------
+        gamma_dir = opj(res.paths["out_dir"], zk, "gamma", tk)
 
-    R = np.zeros(num_trigger_thresholds)
-    R_au = np.zeros(R.shape)
-    dRdE = np.zeros(shape=(num_trigger_thresholds, fine_energy_bin["num"]))
-    dRdE_au = np.zeros(shape=dRdE.shape)
-    for tt in range(num_trigger_thresholds):
-        A = np.interp(
-            x=fine_energy_bin["centers"],
-            xp=energy_bin["centers"],
-            fp=_A[tt, :],
-        )
-        A_au = np.interp(
-            x=fine_energy_bin["centers"],
-            xp=energy_bin["centers"],
-            fp=_A_au[tt, :],
-        )
-
-        dRdE[tt, :], dRdE_au[tt, :] = pru.multiply(
-            x=gamma_dKdE,
-            x_au=gamma_dKdE_au,
-            y=A,
-            y_au=A_au,
-        )
-
-        R[tt], R_au[tt] = irf.utils.integrate_rate_where_known(
-            dRdE=dRdE[tt, :],
-            dRdE_au=dRdE_au[tt, :],
-            E_edges=fine_energy_bin["edges"],
-        )
-
-    json_utils.write(
-        opj(gamma_dir, "differential_rate.json"),
-        {
-            "comment": comment_differential + ", " + gamma_source["name"],
-            "unit": "s$^{-1} (GeV)$^{-1}$",
-            "mean": dRdE,
-            "absolute_uncertainty": dRdE_au,
-        },
-    )
-    json_utils.write(
-        opj(gamma_dir, "integral_rate.json"),
-        {
-            "comment": comment_integral + ", " + gamma_source["name"],
-            "unit": "s$^{-1}$",
-            "mean": R,
-            "absolute_uncertainty": R_au,
-        },
-    )
-
-    # cosmic-rays
-    # -----------
-    for ck in res.COSMIC_RAYS:
-        ck_dir = opj(res.paths["out_dir"], zk, ck)
-        os.makedirs(ck_dir, exist_ok=True)
-
-        _Q = acceptance[zk][ck]["diffuse"]["mean"]
-        _Q_au = acceptance[zk][ck]["diffuse"]["absolute_uncertainty"]
+        _A = acceptance[zk]["gamma"][tk]["point"]["mean"]
+        _A_au = acceptance[zk]["gamma"][tk]["point"]["absolute_uncertainty"]
 
         R = np.zeros(num_trigger_thresholds)
         R_au = np.zeros(R.shape)
         dRdE = np.zeros(shape=(num_trigger_thresholds, fine_energy_bin["num"]))
         dRdE_au = np.zeros(shape=dRdE.shape)
-
-        cosmic_dFdE = airshower_fluxes[ck]["differential_flux"]["values"]
-        cosmic_dFdE_au = airshower_fluxes[ck]["differential_flux"][
-            "absolute_uncertainty"
-        ]
-
         for tt in range(num_trigger_thresholds):
-            Q = np.interp(
+            A = np.interp(
                 x=fine_energy_bin["centers"],
                 xp=energy_bin["centers"],
-                fp=_Q[tt, :],
+                fp=_A[tt, :],
             )
-            Q_au = np.interp(
+            A_au = np.interp(
                 x=fine_energy_bin["centers"],
                 xp=energy_bin["centers"],
-                fp=_Q_au[tt, :],
+                fp=_A_au[tt, :],
             )
 
             dRdE[tt, :], dRdE_au[tt, :] = pru.multiply(
-                x=cosmic_dFdE,
-                x_au=cosmic_dFdE_au,
-                y=Q,
-                y_au=Q_au,
+                x=gamma_dKdE,
+                x_au=gamma_dKdE_au,
+                y=A,
+                y_au=A_au,
             )
 
             R[tt], R_au[tt] = irf.utils.integrate_rate_where_known(
@@ -175,23 +126,87 @@ for zd in range(zenith_bin["num"]):
             )
 
         json_utils.write(
-            opj(ck_dir, "differential_rate.json"),
+            opj(gamma_dir, "differential_rate.json"),
             {
-                "comment": comment_differential,
+                "comment": comment_differential + ", " + gamma_source["name"],
                 "unit": "s$^{-1} (GeV)$^{-1}$",
                 "mean": dRdE,
                 "absolute_uncertainty": dRdE_au,
             },
         )
         json_utils.write(
-            opj(ck_dir, "integral_rate.json"),
+            opj(gamma_dir, "integral_rate.json"),
             {
-                "comment": comment_integral,
+                "comment": comment_integral + ", " + gamma_source["name"],
                 "unit": "s$^{-1}$",
                 "mean": R,
                 "absolute_uncertainty": R_au,
             },
         )
+
+        # cosmic-rays
+        # -----------
+        for ck in res.COSMIC_RAYS:
+            ck_dir = opj(res.paths["out_dir"], zk, ck, tk)
+
+            _Q = acceptance[zk][ck][tk]["diffuse"]["mean"]
+            _Q_au = acceptance[zk][ck][tk]["diffuse"]["absolute_uncertainty"]
+
+            R = np.zeros(num_trigger_thresholds)
+            R_au = np.zeros(R.shape)
+            dRdE = np.zeros(
+                shape=(num_trigger_thresholds, fine_energy_bin["num"])
+            )
+            dRdE_au = np.zeros(shape=dRdE.shape)
+
+            cosmic_dFdE = airshower_fluxes[ck]["differential_flux"]["values"]
+            cosmic_dFdE_au = airshower_fluxes[ck]["differential_flux"][
+                "absolute_uncertainty"
+            ]
+
+            for tt in range(num_trigger_thresholds):
+                Q = np.interp(
+                    x=fine_energy_bin["centers"],
+                    xp=energy_bin["centers"],
+                    fp=_Q[tt, :],
+                )
+                Q_au = np.interp(
+                    x=fine_energy_bin["centers"],
+                    xp=energy_bin["centers"],
+                    fp=_Q_au[tt, :],
+                )
+
+                dRdE[tt, :], dRdE_au[tt, :] = pru.multiply(
+                    x=cosmic_dFdE,
+                    x_au=cosmic_dFdE_au,
+                    y=Q,
+                    y_au=Q_au,
+                )
+
+                R[tt], R_au[tt] = irf.utils.integrate_rate_where_known(
+                    dRdE=dRdE[tt, :],
+                    dRdE_au=dRdE_au[tt, :],
+                    E_edges=fine_energy_bin["edges"],
+                )
+
+            json_utils.write(
+                opj(ck_dir, "differential_rate.json"),
+                {
+                    "comment": comment_differential,
+                    "unit": "s$^{-1} (GeV)$^{-1}$",
+                    "mean": dRdE,
+                    "absolute_uncertainty": dRdE_au,
+                },
+            )
+            json_utils.write(
+                opj(ck_dir, "integral_rate.json"),
+                {
+                    "comment": comment_integral,
+                    "unit": "s$^{-1}$",
+                    "mean": R,
+                    "absolute_uncertainty": R_au,
+                },
+            )
 
 
 def find_index_of_analysis_trigger_threshold(
@@ -212,27 +227,30 @@ tti = find_index_of_analysis_trigger_threshold(
 )
 
 inte = {}
-for zd in range(zenith_bin["num"]):
-    zk = f"zd{zd:d}"
+for tk in TRIGGER_MODI:
+    inte[tk] = {}
+    for zd in range(zenith_bin["num"]):
+        zk = f"zd{zd:d}"
 
-    inte[zk] = {}
-    for pk in res.PARTICLES:
-        zk_pk_dir = opj(res.paths["out_dir"], zk, pk)
-        with open(opj(zk_pk_dir, "integral_rate.json"), "rt") as fin:
-            inr = json_utils.loads(fin.read())
-        inte[zk][pk] = {
-            "R": float(inr["mean"][tti]),
-            "R_au": float(inr["absolute_uncertainty"][tti]),
-        }
+        inte[tk][zk] = {}
 
-    R_tot = []
-    R_tot_au = []
-    for pk in res.PARTICLES:
-        R_tot.append(inte[zk][pk]["R"])
-        R_tot_au.append(inte[zk][pk]["R_au"])
+        for pk in res.PARTICLES:
+            zk_pk_tk_dir = opj(res.paths["out_dir"], zk, pk, tk)
+            with open(opj(zk_pk_tk_dir, "integral_rate.json"), "rt") as fin:
+                inr = json_utils.loads(fin.read())
+            inte[tk][zk][pk] = {
+                "R": float(inr["mean"][tti]),
+                "R_au": float(inr["absolute_uncertainty"][tti]),
+            }
 
-    Rt, Rt_au = pru.sum(x=R_tot, x_au=R_tot_au)
-    inte[zk]["__total__"] = {"R": float(Rt), "R_au": float(Rt_au)}
+        R_tot = []
+        R_tot_au = []
+        for pk in res.PARTICLES:
+            R_tot.append(inte[tk][zk][pk]["R"])
+            R_tot_au.append(inte[tk][zk][pk]["R_au"])
+
+        Rt, Rt_au = pru.sum(x=R_tot, x_au=R_tot_au)
+        inte[tk][zk]["__total__"] = {"R": float(Rt), "R_au": float(Rt_au)}
 
 with rnw.open(
     opj(res.paths["out_dir"], "summary.zenith.particle.json"), "wt"
@@ -241,24 +259,26 @@ with rnw.open(
 
 
 SNR = {}
-for zd in range(zenith_bin["num"]):
-    zk = f"zd{zd:d}"
-    SNR[zk] = {}
+for tk in TRIGGER_MODI:
+    SNR[tk] = {}
+    for zd in range(zenith_bin["num"]):
+        zk = f"zd{zd:d}"
+        SNR[tk][zk] = {}
 
-    Rbg, Rbg_au = pru.add(
-        x=inte[zk]["proton"]["R"],
-        x_au=inte[zk]["proton"]["R_au"],
-        y=inte[zk]["helium"]["R"],
-        y_au=inte[zk]["helium"]["R_au"],
-    )
+        Rbg, Rbg_au = pru.add(
+            x=inte[tk][zk]["proton"]["R"],
+            x_au=inte[tk][zk]["proton"]["R_au"],
+            y=inte[tk][zk]["helium"]["R"],
+            y_au=inte[tk][zk]["helium"]["R_au"],
+        )
 
-    snr, snr_au = pru.divide(
-        x=inte[zk]["gamma"]["R"],
-        x_au=inte[zk]["gamma"]["R_au"],
-        y=Rbg,
-        y_au=Rbg_au,
-    )
-    SNR[zk] = {"snr_u": snr * 1e6, "snr_u_au": snr_au * 1e6}
+        snr, snr_au = pru.divide(
+            x=inte[tk][zk]["gamma"]["R"],
+            x_au=inte[tk][zk]["gamma"]["R_au"],
+            y=Rbg,
+            y_au=Rbg_au,
+        )
+        SNR[tk][zk] = {"snr_u": snr * 1e6, "snr_u_au": snr_au * 1e6}
 
 with rnw.open(opj(res.paths["out_dir"], "snr.json"), "wt") as fout:
     fout.write(json_utils.dumps(SNR, indent=4))
--- plenoirf/summary/scripts/0106_trigger_rates_for_cosmic_particles_plot.py
+++ plenoirf/summary/scripts/0106_trigger_rates_for_cosmic_particles_plot.py
@@ -17,6 +17,10 @@ cosmic_rates = json_utils.tree.Tree(
     opj(res.paths["analysis_dir"], "0105_trigger_rates_for_cosmic_particles")
 )
 
+TRIGGER_MODI = json_utils.read(
+    opj(res.paths["analysis_dir"], "0055_passing_trigger", "trigger_modi.json")
+)
+
 mean_key = "mean"
 unc_key = "absolute_uncertainty"
 
@@ -25,70 +29,80 @@ trigger_thresholds = np.array(
 )
 analysis_trigger_threshold = res.analysis["trigger"]["threshold_pe"]
 
-tt = 0
-for tt, trigger_threshold in enumerate(trigger_thresholds):
-    if trigger_threshold == analysis_trigger_threshold:
-        break
 
-for zd in range(zenith_bin["num"]):
-    zk = f"zd{zd:d}"
+for tk in TRIGGER_MODI:
 
-    sfig, sax = irf.summary.figure.style(key="4:3")
-    fig = sebplt.figure(sfig)
-    ax = sebplt.add_axes(fig=fig, span=sax)
-    sebplt.add_axes_zenith_range_indicator(
-        fig=fig,
-        span=irf.summary.figure.AX_SPAN_ZENITH_INDICATOR,
-        zenith_bin_edges_rad=zenith_bin["edges"],
-        zenith_bin=zd,
-        fontsize=6,
-    )
+    tt = 0
+    for tt, trigger_threshold in enumerate(trigger_thresholds):
+        if trigger_threshold == analysis_trigger_threshold:
+            break
+    if tk == "far_accepting_focus":
+        tt += 18  # roughly same total trigger rate
 
-    text_y = 0.7
-    for pk in res.PARTICLES:
-        dRdE = cosmic_rates[zk][pk]["differential_rate"][mean_key]
-        dRdE_au = cosmic_rates[zk][pk]["differential_rate"][unc_key]
+    os.makedirs(opj(res.paths["out_dir"], tk))
+    for zd in range(zenith_bin["num"]):
+        zk = f"zd{zd:d}"
 
-        ax.plot(
-            fine_energy_bin["centers"],
-            dRdE[tt, :],
-            color=res.PARTICLE_COLORS[pk],
-            label=pk,
+        sfig, sax = irf.summary.figure.style(key="4:3")
+        fig = sebplt.figure(sfig)
+        ax = sebplt.add_axes(fig=fig, span=sax)
+        sebplt.add_axes_zenith_range_indicator(
+            fig=fig,
+            span=irf.summary.figure.AX_SPAN_ZENITH_INDICATOR,
+            zenith_bin_edges_rad=zenith_bin["edges"],
+            zenith_bin=zd,
+            fontsize=6,
         )
-        ax.fill_between(
-            x=fine_energy_bin["centers"],
-            y1=dRdE[tt, :] - dRdE_au[tt, :],
-            y2=dRdE[tt, :] + dRdE_au[tt, :],
-            facecolor=res.PARTICLE_COLORS[pk],
-            alpha=0.2,
-            linewidth=0.0,
-        )
-        ax.text(
-            0.5,
-            0.1 + text_y,
-            pk,
-            color=res.PARTICLE_COLORS[pk],
-            transform=ax.transAxes,
-        )
-        ir = cosmic_rates[zk][pk]["integral_rate"][mean_key][tt]
-        ir_abs_unc = cosmic_rates[zk][pk]["integral_rate"][unc_key][tt]
-        ax.text(
-            0.65,
-            0.1 + text_y,
-            r"{: 8.1f} $\pm${: 6.1f} s$^{{-1}}$".format(ir, ir_abs_unc),
-            color="k",
-            transform=ax.transAxes,
-        )
-        text_y += 0.06
 
-    ax.set_xlabel("energy / GeV")
-    ax.set_ylabel("differential trigger rate /\ns$^{-1}$ (GeV)$^{-1}$")
-    ax.loglog()
-    ax.set_xlim(fine_energy_bin["limits"])
-    ax.set_ylim([1e-3, 1e4])
-    fig.savefig(
-        opj(res.paths["out_dir"], f"differential_trigger_rate_{zk:s}.jpg")
-    )
-    sebplt.close(fig)
+        text_y = 0.7
+        for pk in res.PARTICLES:
+            dRdE = cosmic_rates[zk][pk][tk]["differential_rate"][mean_key]
+            dRdE_au = cosmic_rates[zk][pk][tk]["differential_rate"][unc_key]
+
+            ax.plot(
+                fine_energy_bin["centers"],
+                dRdE[tt, :],
+                color=res.PARTICLE_COLORS[pk],
+                label=pk,
+            )
+            ax.fill_between(
+                x=fine_energy_bin["centers"],
+                y1=dRdE[tt, :] - dRdE_au[tt, :],
+                y2=dRdE[tt, :] + dRdE_au[tt, :],
+                facecolor=res.PARTICLE_COLORS[pk],
+                alpha=0.2,
+                linewidth=0.0,
+            )
+            ax.text(
+                0.5,
+                0.1 + text_y,
+                pk,
+                color=res.PARTICLE_COLORS[pk],
+                transform=ax.transAxes,
+            )
+            ir = cosmic_rates[zk][pk][tk]["integral_rate"][mean_key][tt]
+            ir_abs_unc = cosmic_rates[zk][pk][tk]["integral_rate"][unc_key][tt]
+            ax.text(
+                0.65,
+                0.1 + text_y,
+                r"{: 8.1f} $\pm${: 6.1f} s$^{{-1}}$".format(ir, ir_abs_unc),
+                color="k",
+                transform=ax.transAxes,
+            )
+            text_y += 0.06
+
+        ax.set_xlabel("energy / GeV")
+        ax.set_ylabel("differential trigger rate /\ns$^{-1}$ (GeV)$^{-1}$")
+        ax.loglog()
+        ax.set_xlim(fine_energy_bin["limits"])
+        ax.set_ylim([1e-3, 1e4])
+        fig.savefig(
+            opj(
+                res.paths["out_dir"],
+                tk,
+                f"differential_trigger_rate_{zk:s}.jpg",
+            )
+        )
+        sebplt.close(fig)
 
 res.stop()
